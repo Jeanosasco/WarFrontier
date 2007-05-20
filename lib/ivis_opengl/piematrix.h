Jeanosasco/WarFrontier

commit 0f6db5812d7b5752ac3b2616cddcabe4683d01db

- Remove unused code - Remove duplicated code/macros - VECTOR3D->Vector3i - Create float variants of normal functions to simplify Watermelon's float-PIE patch
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1661 4a71c877-e1ca-e34f-864e-861f7616d084

lib/ivis_opengl/piematrix.h
@@ -45,36 +45,32 @@ typedef struct {SDWORD a, b, c,  d, e, f,  g, h, i,  j, k, l;} SDMATRIX;
 /***************************************************************************/
 
 extern SDMATRIX *psMatrix;
-extern SDWORD	aSinTable[];
+extern SDWORD aSinTable[];
 
 //*************************************************************************
 
 // FIXME DUPLICATE CODE! Already present in trig.c!
-#define SIN(X)	aSinTable[(Uint16)(X) >> 4]
-#define COS(X)	aSinTable[((Uint16)(X) >> 4) + 1024]
+#define SIN(X) aSinTable[(Uint16)(X) >> 4]
+#define COS(X) aSinTable[((Uint16)(X) >> 4) + 1024]
 
 
 //*************************************************************************
 
-#define pie_ROTATE_TRANSLATE(x,y,z,xs,ys,zs)										\
-{																			\
-	xs = (x) * psMatrix->a + (y) * psMatrix->d + (z) * psMatrix->g +	\
-				psMatrix->j;												\
-	ys = (x) * psMatrix->b + (y) * psMatrix->e + (z) * psMatrix->h +	\
-				psMatrix->k;												\
-	zs = (x) * psMatrix->c + (y) * psMatrix->f + (z) * psMatrix->i +	\
-				psMatrix->l;												\
-	xs >>=FP12_SHIFT;												\
-	ys >>=FP12_SHIFT;												\
-	zs >>=FP12_SHIFT;												\
+/*!
+ * Rotate and translate v with the worldmatrix. Store the result in s
+ * \param[in] v Vector to translate
+ * \param[out] s Resulting vector
+ */
+static inline void pie_RotateTranslate3iv(Vector3i * v, Vector3i * s)
+{
+	s->x = ( v->x * psMatrix->a + v->y * psMatrix->d + v->z * psMatrix->g
+			+ psMatrix->j ) / FP12_MULTIPLIER;
+	s->y = ( v->x * psMatrix->b + v->y * psMatrix->e + v->z * psMatrix->h
+			+ psMatrix->k ) / FP12_MULTIPLIER;
+	s->z = ( v->x * psMatrix->c + v->y * psMatrix->f + v->z * psMatrix->i
+			+ psMatrix->l ) / FP12_MULTIPLIER;
 }
 
-//*************************************************************************
-
-
-//*************************************************************************
-
-#define pie_CLOCKWISE(x0,y0,x1,y1,x2,y2) ((((y1)-(y0)) * ((x2)-(x1))) <= (((x1)-(x0)) * ((y2)-(y1))))
 
 //*************************************************************************
 
@@ -86,7 +82,7 @@ extern void pie_MatInit(void);
 
 extern void pie_MatBegin(void);
 extern void pie_MatEnd(void);
-extern void pie_MATTRANS(int x,int y,int z);
+extern void pie_MATTRANS(int x, int y, int z);
 extern void pie_TRANSLATE(int x, int y, int z);
 extern void pie_MatScale( UDWORD percent );
 extern void pie_MatRotX(int x);
@@ -102,9 +98,11 @@ extern void pie_PerspectiveEnd(void);
 
 //*************************************************************************
 
-extern void pie_VectorNormalise(Vector3i *v);
+extern void pie_VectorNormalise3iv(Vector3i *v);
+extern void pie_VectorNormalise3fv(Vector3f *v);
 extern void pie_VectorInverseRotate0(Vector3i *v1, Vector3i *v2);
-extern void pie_SurfaceNormal(Vector3i *p1, Vector3i *p2, Vector3i *p3, Vector3i *v);
+extern void pie_SurfaceNormal3iv(Vector3i *p1, Vector3i *p2, Vector3i *p3, Vector3i *v);
+extern void pie_SurfaceNormal3fv(Vector3f *p1, Vector3f *p2, Vector3f *p3, Vector3f *v);
 extern BOOL pie_Clockwise(iVertex *s);
 extern void pie_SetGeometricOffset(int x, int y);
 

lib/framework/macros.h
@@ -25,5 +25,6 @@
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define ABS(a) (((a) < 0) ? (-(a)) : (a))
 
 #endif // MACROS_H

lib/gamelib/anim.c
@@ -61,16 +61,6 @@ ANIMGLOBALS	g_animGlobals;
 BOOL
 anim_Init( GETSHAPEFUNC pGetShapeFunc )
 {
-	int		iSizeAnim2D = sizeof(ANIM2D),
-			iSizeAnim3D = sizeof(ANIM3D);
-
-	/* ensure ANIM2D and ANIM3D structs same size */
-	if ( iSizeAnim2D != iSizeAnim3D )
-	{
-		debug( LOG_ERROR, "anim_Init: ANIM2D and ANIM3D structs not same size in anim.h!" );
-		abort();
-	}
-
 	/* init globals */
 	g_animGlobals.psAnimList    = NULL;
 	g_animGlobals.uwCurObj      = 0;
@@ -248,8 +238,7 @@ anim_EndScript( void )
 /***************************************************************************/
 
 BOOL
-anim_AddFrameToAnim( int iFrame, VECTOR3D vecPos, VECTOR3D vecRot,
-						VECTOR3D vecScale )
+anim_AddFrameToAnim( int iFrame, Vector3i vecPos, Vector3i vecRot, Vector3i vecScale )
 {
 	ANIM_STATE	*psState;
 	BASEANIM	*psAnim;
@@ -414,7 +403,7 @@ anim_GetShapeFromID( UWORD uwID )
 UWORD
 anim_GetFrame3D( ANIM3D *psAnim, UWORD uwObj, UDWORD udwGameTime,
 			UDWORD udwStartTime, UDWORD udwStartDelay,
-			VECTOR3D *psVecPos, VECTOR3D *psVecRot, VECTOR3D *psVecScale )
+			Vector3i *psVecPos, Vector3i *psVecRot, Vector3i *psVecScale )
 {
 	SDWORD		dwTime;
 	UWORD		uwState, uwFrame;

lib/gamelib/anim.h
@@ -65,12 +65,6 @@ struct BASEANIM;
 	struct BASEANIM		*psNext;
 
 /* ensure ANIM2D/3D structs same size */
-#define ANIM_2D_ELEMENTS						\
-	ANIM_BASE_ELEMENTS							\
-	iTexture		*psFrames;						\
-	UWORD		uwBmapWidth;	/* width of container bitmap */
-
-/* ensure ANIM2D/3D structs same size */
 #define ANIM_3D_ELEMENTS						\
 	ANIM_BASE_ELEMENTS							\
 	iIMDShape	*psFrames;						\
@@ -78,18 +72,12 @@ struct BASEANIM;
 
 /***************************************************************************/
 
-typedef struct VECTOR3D
-{
-	SDWORD	x, y, z;
-}
-VECTOR3D;
-
 typedef struct ANIM_STATE
 {
 	UWORD				uwFrame;		/* frame to play           */
-	VECTOR3D			vecPos;
-	VECTOR3D			vecAngle;
-	VECTOR3D			vecScale;
+	Vector3i			vecPos;
+	Vector3i			vecAngle;
+	Vector3i			vecScale;
 }
 ANIM_STATE;
 
@@ -98,12 +86,6 @@ typedef struct BASEANIM
 	ANIM_BASE_ELEMENTS
 }
 BASEANIM;
-
-typedef struct ANIM2D
-{
-	ANIM_2D_ELEMENTS
-}
-ANIM2D;
 
 typedef struct ANIM3D
 {
@@ -138,15 +120,14 @@ BOOL		anim_Create3D( char szPieFileName[], UWORD uwFrames,
 							UBYTE ubType, UWORD uwID );
 void		anim_BeginScript( void );
 BOOL		anim_EndScript( void );
-BOOL		anim_AddFrameToAnim( int iFrame, VECTOR3D vecPos,
-									VECTOR3D vecRot, VECTOR3D vecScale );
+BOOL		anim_AddFrameToAnim( int iFrame, Vector3i vecPos, Vector3i vecRot, Vector3i vecScale );
 BASEANIM *	anim_GetAnim( UWORD uwAnimID );
 UWORD		anim_GetAnimID( char *szName );
 iIMDShape *	anim_GetShapeFromID( UWORD uwID );
 UWORD		anim_GetFrame3D( ANIM3D *psAnim, UWORD uwObj, UDWORD udwGameTime,
 								UDWORD udwStartTime, UDWORD udwStartDelay,
-								VECTOR3D *psVecPos, VECTOR3D *psVecRot,
-								VECTOR3D *psVecScale );
+								Vector3i *psVecPos, Vector3i *psVecRot,
+								Vector3i *psVecScale );
 void		anim_SetVals( char szFileName[], UWORD uwAnimID );
 
 /***************************************************************************/

lib/gamelib/animobj.c
@@ -269,8 +269,8 @@ animObj_Add( void *pParentObj, int iAnimID,
 /***************************************************************************/
 
 UWORD
-animObj_GetFrame3D( ANIM_OBJECT *psObj, UWORD uwObj, VECTOR3D *psVecPos,
-					VECTOR3D *psVecRot, VECTOR3D *psVecScale )
+animObj_GetFrame3D( ANIM_OBJECT *psObj, UWORD uwObj, Vector3i *psVecPos,
+					Vector3i *psVecRot, Vector3i *psVecScale )
 {
 	ANIM3D		*psAnim;
 

lib/gamelib/animobj.h
@@ -52,8 +52,8 @@ typedef BOOL (* ANIMOBJDIEDTESTFUNC) ( void *psParent );
 /* struct member macros */
 
 #define	COMPONENT_ELEMENTS(pointerType)		\
-	VECTOR3D	position;					\
-	VECTOR3D	orientation;				\
+	Vector3i	position;					\
+	Vector3i	orientation;				\
 	void		*psParent;					\
 	iIMDShape	*psShape;
 
@@ -103,8 +103,8 @@ ANIM_OBJECT *	animObj_GetFirst( void );
 ANIM_OBJECT *	animObj_GetNext( void );
 ANIM_OBJECT *	animObj_Find( void *pParentObj, int iAnimID );
 UWORD			animObj_GetFrame3D( ANIM_OBJECT *psObj, UWORD uwObj,
-									VECTOR3D *psPos, VECTOR3D *psVecRot,
-									VECTOR3D *psVecScale );
+									Vector3i *psPos, Vector3i *psVecRot,
+									Vector3i *psVecScale );
 
 /***************************************************************************/
 

lib/gamelib/audp_parser.y
@@ -26,7 +26,7 @@
 
 static int		g_iCurAnimID = 0;
 static int		g_iDummy;
-static VECTOR3D		vecPos, vecRot, vecScale;
+static Vector3i vecPos, vecRot, vecScale;
 
 int audp_lex();
 void audp_error(char *pMessage,...);

lib/ivis_common/imdload.c
@@ -284,7 +284,7 @@ static BOOL _imd_load_polys( char **ppFileData, iIMDShape *s )
 				p2.y = points[poly->pindex[poly->npnts-1]].y;
 				p2.z = points[poly->pindex[poly->npnts-1]].z;
 
-				pie_SurfaceNormal(&p0,&p1,&p2,&poly->normal);
+				pie_SurfaceNormal3iv(&p0,&p1,&p2,&poly->normal);
 				//iV_DEBUG3("normal %d %d %d\n",poly->normal.x,poly->normal.y,poly->normal.z);
 			} else {
 				poly->normal.x = poly->normal.y = poly->normal.z = 0;
@@ -538,17 +538,17 @@ static BOOL _imd_load_points( char **ppFileData, iIMDShape *s )
 		}
 
 		/* Centered about origin I can do the '-' thing below!! */
-		extremeX = pie_MAX(tempXMax,-tempXMin);
-		extremeZ = pie_MAX(tempZMax,-tempZMin);
+		extremeX = MAX(tempXMax, -tempXMin);
+		extremeZ = MAX(tempZMax, -tempZMin);
 
-		s->visRadius = pie_MAX(extremeX,extremeZ);
+		s->visRadius = MAX(extremeX, extremeZ);
 		// no need to scale an IMD shape (only FSD)
 
-		xmax = pie_MAX(s->xmax,-s->xmin);
-		ymax = pie_MAX(s->ymax,-s->ymin);
-		zmax = pie_MAX(s->zmax,-s->zmin);
+		xmax = MAX(s->xmax, -s->xmin);
+		ymax = MAX(s->ymax, -s->ymin);
+		zmax = MAX(s->zmax, -s->zmin);
 
-		s->radius = pie_MAX(xmax,(pie_MAX(ymax,zmax)));
+		s->radius = MAX(xmax, (MAX(ymax, zmax)));
 
 
 		s->sradius = (SDWORD)((float)sqrt( xmax*xmax + ymax*ymax + zmax*zmax));

commit b4a27734e7d5cbec96c4f283177fe13dada379d3

Fix crash introduced in commit r1661 in function pie_RotateTranslate3iv, where the y and z parameters were not switched, as they should be.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1670 4a71c877-e1ca-e34f-864e-861f7616d084

lib/ivis_opengl/piematrix.h
@@ -63,11 +63,11 @@ extern SDWORD aSinTable[];
  */
 static inline void pie_RotateTranslate3iv(Vector3i * v, Vector3i * s)
 {
-	s->x = ( v->x * psMatrix->a + v->y * psMatrix->d + v->z * psMatrix->g
+	s->x = ( v->x * psMatrix->a + v->z * psMatrix->d + v->y * psMatrix->g
 			+ psMatrix->j ) / FP12_MULTIPLIER;
-	s->y = ( v->x * psMatrix->b + v->y * psMatrix->e + v->z * psMatrix->h
+	s->z = ( v->x * psMatrix->b + v->z * psMatrix->e + v->y * psMatrix->h
 			+ psMatrix->k ) / FP12_MULTIPLIER;
-	s->z = ( v->x * psMatrix->c + v->y * psMatrix->f + v->z * psMatrix->i
+	s->y = ( v->x * psMatrix->c + v->z * psMatrix->f + v->y * psMatrix->i
 			+ psMatrix->l ) / FP12_MULTIPLIER;
 }
 