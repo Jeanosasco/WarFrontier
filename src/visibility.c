Jeanosasco/WarFrontier

commit b0b76d787667bd61d25320589f85f5b499cdcbb0

Cleanup of visibility code. Added new debug category: "sensor".
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2320 4a71c877-e1ca-e34f-864e-861f7616d084

src/visibility.c
@@ -44,59 +44,29 @@
 // accuracy for the height gradient
 #define GRAD_MUL	10000
 
-// decay constant for the power of sensors/ecms
-#define POWER_DECAY		10
-
-// maximum radius of an object
-#define MAX_OBJRADIUS	(TILE_UNITS*2)
-
-// maximum number of objects to test against a ray
-#define MAX_RAYTEST		100
-// List of objects that could intersect a ray
-static BASE_OBJECT		*apsTestObjects[MAX_RAYTEST];
-static SDWORD			numTestObjects = 0;
-
-// maximum number of objects intersecting a ray
-#define MAX_RAYOBJ		20
 // list of objects intersecting a ray and the distance to them
-static BASE_OBJECT		*apsRayObjects[MAX_RAYOBJ];
-static SDWORD			aRayObjDist[MAX_RAYOBJ];
 static SDWORD			numRayObjects = 0;
-//static BOOL				gotRayObjects = FALSE;
+
 // which object is being considered by the callback
 static SDWORD			currObj;
 
 // rate to change visibility level
-#define VIS_LEVEL_INC		(255*2)
+#define VIS_LEVEL_INC		255*2
 #define VIS_LEVEL_DEC		50
+
 // fractional accumulator of how much to change visibility this frame
 static float			visLevelIncAcc, visLevelDecAcc;
+
 // integer amount to change visiblility this turn
 static SDWORD			visLevelInc, visLevelDec;
 
 // percentage of power over which objects start to be visible
+// UNUSED. What for? - Per
 #define VIS_LEVEL_START		100
 #define VIS_LEVEL_RANGE		60
 
-// maximum possible sensor power
-#define MAX_SENSOR_POWER	10000
-
-// the last sensor power to see an object - used by process visibility
-// to avoid having to recalculate it
-//static SDWORD			lastSensorPower;
-
-// whether a side has seen a unit on another side yet
-
-/* Get all the objects that might intersect a ray.
- * Do not include psSource in the list as the ray comes from it
- */
-void visGetTestObjects(SDWORD x1,SDWORD y1, SDWORD x2,SDWORD y2,
-							  BASE_OBJECT *psSource, BASE_OBJECT *psTarget);
-
-/* Get the objects in the apsTestObjects array that intersect a ray */
-void visGetRayObjects(SDWORD x1,SDWORD y1, SDWORD x2,SDWORD y2);
-
-void	setUnderTilesVis(BASE_OBJECT *psObj, UDWORD player);
+// alexl's sensor range.
+BOOL bDisplaySensorRange;
 
 /* Variables for the visibility callback */
 static SDWORD		rayPlayer;				// The player the ray is being cast for
@@ -113,8 +83,8 @@ static SDWORD		wallX,wallY;			// the position of a wall if it is on the LOS
 // initialise the visibility stuff
 BOOL visInitialise(void)
 {
-	visLevelIncAcc = MAKEFRACT(0);
-	visLevelDecAcc = MAKEFRACT(0);
+	visLevelIncAcc = 0;
+	visLevelDecAcc = 0;
 	visLevelInc = 0;
 	visLevelDec = 0;
 
@@ -124,39 +94,12 @@ BOOL visInitialise(void)
 // update the visibility change levels
 void visUpdateLevel(void)
 {
-	visLevelIncAcc += FRACTmul(MAKEFRACT(frameTime),
-							   FRACTCONST(VIS_LEVEL_INC,GAME_TICKS_PER_SEC));
-	visLevelInc = MAKEINT(visLevelIncAcc);
-	visLevelIncAcc -= MAKEFRACT(visLevelInc);
-	visLevelDecAcc += FRACTmul(MAKEFRACT(frameTime),
-							   FRACTCONST(VIS_LEVEL_DEC,GAME_TICKS_PER_SEC));
-	visLevelDec = MAKEINT(visLevelDecAcc);
-	visLevelDecAcc -= MAKEFRACT(visLevelDec);
-}
-
-/* Return the radius a base object covers on the map */
-static SDWORD visObjRadius(BASE_OBJECT *psObject)
-{
-	SDWORD	radius;
-
-	switch (psObject->type)
-	{
-	case OBJ_DROID:
-		radius = psObject->sDisplay.imd->radius;
-		break;
-	case OBJ_STRUCTURE:
-		radius = psObject->sDisplay.imd->radius;
-		break;
-	case OBJ_FEATURE:
-		radius = psObject->sDisplay.imd->radius;
-		break;
-	default:
-		ASSERT( FALSE,"visObjRadius: unknown object type" );
-		radius = 0;
-		break;
-	}
-
-	return radius;
+	visLevelIncAcc += (float)frameTime * (VIS_LEVEL_INC / GAME_TICKS_PER_SEC);
+	visLevelInc = visLevelIncAcc;
+	visLevelIncAcc -= visLevelInc;
+	visLevelDecAcc += (float)frameTime * (VIS_LEVEL_DEC / GAME_TICKS_PER_SEC);
+	visLevelDec = visLevelDecAcc;
+	visLevelDecAcc -= visLevelDec;
 }
 
 static SDWORD visObjHeight(BASE_OBJECT *psObject)
@@ -184,7 +127,6 @@ static SDWORD visObjHeight(BASE_OBJECT *psObject)
 	return height;
 }
 
-
 /* The terrain revealing ray callback */
 static BOOL rayTerrainCallback(SDWORD x, SDWORD y, SDWORD dist)
 {
@@ -195,16 +137,13 @@ static BOOL rayTerrainCallback(SDWORD x, SDWORD y, SDWORD dist)
 			y >= 0 && y < ((SDWORD)mapHeight << TILE_SHIFT),
 			"rayTerrainCallback: coords off map" );
 
-	psTile = mapTile(x >> TILE_SHIFT, y >> TILE_SHIFT);
-
-
+	psTile = mapTile(map_coord(x), map_coord(y));
 
 	/* Not true visibility - done on sensor range */
 
-	if(dist == 0) {	//Complete hack PD.. John what should happen if dist is 0 ???
-
-		debug( LOG_NEVER, "rayTerrainCallback: dist == 0, will divide by zero\n" );
-
+	if (dist == 0)
+	{
+		debug(LOG_ERROR, "rayTerrainCallback: dist is 0, which is not a valid distance");
 		dist = 1;
 	}
 
@@ -244,12 +183,10 @@ static BOOL rayTerrainCallback(SDWORD x, SDWORD y, SDWORD dist)
 	return TRUE;
 }
 
-
 /* The los ray callback */
 static BOOL rayLOSCallback(SDWORD x, SDWORD y, SDWORD dist)
 {
 	SDWORD		newG;		// The new gradient
-//	MAPTILE		*psTile;
 	SDWORD		distSq;
 	SDWORD		tileX,tileY;
 	MAPTILE		*psTile;
@@ -257,11 +194,6 @@ static BOOL rayLOSCallback(SDWORD x, SDWORD y, SDWORD dist)
 	ASSERT( x >= 0 && x < ((SDWORD)mapWidth << TILE_SHIFT) &&
 			y >= 0 && y < ((SDWORD)mapHeight << TILE_SHIFT),
 			"rayLOSCallback: coords off map" );
-
-/*	if(dist == 0) {	//Complete hack PD.. John what should happen if dist is 0 ???
-		DBPRINTF(("rayTerrainCallback: dist == 0, will divide by zero\n"));
-		dist = 1;
-	}*/
 
 	distSq = dist*dist;
 
@@ -271,14 +203,6 @@ static BOOL rayLOSCallback(SDWORD x, SDWORD y, SDWORD dist)
 	}
 	else
 	{
-		// see if the ray hit an object on the last tile
-/*		if (currObj < numRayObjects &&
-			distSq > aRayObjDist[currObj])
-		{
-			lastH += visObjHeight(apsRayObjects[currObj]);
-			currObj += 1;
-		}*/
-
 		// Calculate the current LOS gradient
 		newG = (lastH - startH) * GRAD_MUL / lastD;
 		if (newG >= currG)
@@ -296,14 +220,12 @@ static BOOL rayLOSCallback(SDWORD x, SDWORD y, SDWORD dist)
 	else
 	{
 		// Store the height at this tile for next time round
-	//	psTile = mapTile(x >> TILE_SHIFT, y >> TILE_SHIFT);
-	//	lastH = psTile->height * ELEVATION_SCALE;
-		tileX = x>>TILE_SHIFT;
-		tileY = y>>TILE_SHIFT;
+		tileX = map_coord(x);
+		tileY = map_coord(y);
 
 		if (blockingWall && !((tileX == finalX) && (tileY == finalY)))
 		{
-			psTile = mapTile(x >> TILE_SHIFT, y >> TILE_SHIFT);
+			psTile = mapTile(tileX, tileY);
 			if (TILE_HAS_WALL(psTile) && !TILE_HAS_SMALLSTRUCTURE(psTile))
 			{
 				lastH = 2*UBYTE_MAX * ELEVATION_SCALE;
@@ -338,7 +260,6 @@ BOOL visTilesPending(BASE_OBJECT *psObj)
 	return (((DROID*)psObj)->updateFlags & DUPF_SCANTERRAIN);
 }
 
-
 /* Check which tiles can be seen by an object */
 void visTilesUpdate(BASE_OBJECT *psObj,BOOL SpreadLoad)
 {
@@ -361,8 +282,6 @@ void visTilesUpdate(BASE_OBJECT *psObj,BOOL SpreadLoad)
 			"units and structures" );
 		return;
 	}
-
-
 
 	rayPlayer = psObj->player;
 
@@ -406,8 +325,6 @@ void visTilesUpdate(BASE_OBJECT *psObj,BOOL SpreadLoad)
 		for(ray=0; ray < NUM_RAYS; ray += NUM_RAYS/80)
 		{
 			// initialise the callback variables
-	/*		startH = (mapTile(psObj->x >> TILE_SHIFT,psObj->y >> TILE_SHIFT)->height)
-						* ELEVATION_SCALE;*/
 			startH = psObj->z + visObjHeight(psObj);
 			currG = -UBYTE_MAX * GRAD_MUL;
 
@@ -416,10 +333,6 @@ void visTilesUpdate(BASE_OBJECT *psObj,BOOL SpreadLoad)
 		}
 	}
 }
-
-
-
-
 
 /* Check whether psViewer can see psTarget.
  * psViewer should be an object that has some form of sensor,
@@ -433,7 +346,6 @@ BOOL visibleObject(BASE_OBJECT *psViewer, BASE_OBJECT *psTarget)
 	SDWORD		xdiff,ydiff, rangeSquared;
 	SDWORD		range;
 	UDWORD		senPower, ecmPower;
-//	SDWORD		x1,y1, x2,y2;
 	SDWORD		tarG, top;
 	STRUCTURE	*psStruct;
 
@@ -469,7 +381,6 @@ BOOL visibleObject(BASE_OBJECT *psViewer, BASE_OBJECT *psTarget)
 		{
 			// if a unit is targetted by a counter battery sensor
 			// it is automatically seen
-//			lastSensorPower = senPower;
 			return TRUE;
 		}
 
@@ -557,8 +468,8 @@ BOOL visibleObject(BASE_OBJECT *psViewer, BASE_OBJECT *psTarget)
 	rayStart = TRUE;
 	currObj = 0;
 	ray = NUM_RAYS-1 - calcDirection(psViewer->x,psViewer->y, psTarget->x,psTarget->y);
-	finalX = psTarget->x >> TILE_SHIFT;
-	finalY = psTarget->y >> TILE_SHIFT;
+	finalX = map_coord(psTarget->x);
+	finalY = map_coord(psTarget->y);
 
 	// don't check for any objects intersecting the ray
 	numRayObjects = 0;
@@ -573,7 +484,6 @@ BOOL visibleObject(BASE_OBJECT *psViewer, BASE_OBJECT *psTarget)
 	return tarG >= currG;
 }
 
-
 // Do visibility check, but with walls completely blocking LOS.
 BOOL visibleObjWallBlock(BASE_OBJECT *psViewer, BASE_OBJECT *psTarget)
 {
@@ -585,7 +495,6 @@ BOOL visibleObjWallBlock(BASE_OBJECT *psViewer, BASE_OBJECT *psTarget)
 
 	return result;
 }
-
 
 // Find the wall that is blocking LOS to a target (if any)
 BOOL visGetBlockingWall(BASE_OBJECT *psViewer, BASE_OBJECT *psTarget, STRUCTURE **ppsWall)
@@ -648,14 +557,12 @@ void processVisibility(BASE_OBJECT *psObj)
 		psDroid = (DROID *)psObj;
 		psECMStats = asECMStats + psDroid->asBits[COMP_ECM].nStat;
 		ecmPoints = ecmPower(psECMStats, psDroid->player);
-		//if (psECMStats->power < maxPower)
 		if (ecmPoints < maxPower)
 		{
 			psDroid->ECMMod = maxPower;
 		}
 		else
 		{
-			//psDroid->ECMMod = psECMStats->power;
 			psDroid->ECMMod = ecmPoints;
 			maxPower = psDroid->ECMMod;
 		}
@@ -715,18 +622,18 @@ void processVisibility(BASE_OBJECT *psObj)
 		}
 	}
 
-    //if a player has a SAT_UPLINK structure, they can see everything!
-    for (player=0; player<MAX_PLAYERS; player++)
-    {
-        if (getSatUplinkExists(player))
-        {
-            currVis[player] = TRUE;
+	// if a player has a SAT_UPLINK structure, they can see everything!
+	for (player=0; player<MAX_PLAYERS; player++)
+	{
+		if (getSatUplinkExists(player))
+		{
+			currVis[player] = TRUE;
 			if (psObj->visible[player] == 0)
 			{
 				psObj->visible[player] = 1;
 			}
-        }
-    }
+		}
+	}
 
 	psViewer = gridIterate();
 	while (psViewer != NULL)
@@ -772,12 +679,12 @@ void processVisibility(BASE_OBJECT *psObj)
 		}
 	}
 
-
 	// update the visibility levels
 	for(i=0; i<MAX_PLAYERS; i++)
 	{
 		if (i == psObj->player)
 		{
+			// owner can always see it fully
 			psObj->visible[i] = UBYTE_MAX;
 			continue;
 		}
@@ -919,108 +826,6 @@ MAPTILE		*psTile;
 		}
 	}
 }
-
-
-/* Get all the objects that might intersect a ray.
- * Do not include psSource in the list as the ray comes from it
- */
-void visGetTestObjects(SDWORD x1,SDWORD y1, SDWORD x2,SDWORD y2,
-							  BASE_OBJECT *psSource, BASE_OBJECT *psTarget)
-{
-	SDWORD		player; //, bx1,by1, bx2,by2;
-	SDWORD		radius;
-	BASE_OBJECT	*psObj;
-
-	// find the structures
-	numTestObjects = 0;
-	for (player=0; player < MAX_PLAYERS; player++)
-	{
-		for(psObj = (BASE_OBJECT *)apsStructLists[player]; psObj; psObj = psObj->psNext)
-		{
-			radius = visObjRadius(psObj);
-			if ((((SDWORD)psObj->x + radius) >= x1) && (((SDWORD)psObj->x - radius) <= x2) &&
-				(((SDWORD)psObj->y + radius) >= y1) && (((SDWORD)psObj->y - radius) <= y2) &&
-				psObj != psSource && psObj != psTarget &&
-				numTestObjects < MAX_RAYTEST)
-			{
-				apsTestObjects[numTestObjects++] = psObj;
-			}
-		}
-	}
-
-	// find the features
-	for(psObj = (BASE_OBJECT *)apsFeatureLists[0]; psObj; psObj = psObj->psNext)
-	{
-		radius = visObjRadius(psObj);
-		if ((((SDWORD)psObj->x + radius) >= x1) && (((SDWORD)psObj->x - radius) <= x2) &&
-			(((SDWORD)psObj->y + radius) >= y1) && (((SDWORD)psObj->y - radius) <= y2) &&
-			psObj != psSource && psObj != psTarget &&
-			numTestObjects < MAX_RAYTEST)
-		{
-			apsTestObjects[numTestObjects++] = psObj;
-		}
-	}
-}
-
-
-/* Get the objects in the apsTestObjects array that intersect a ray */
-void visGetRayObjects(SDWORD x1,SDWORD y1, SDWORD x2,SDWORD y2)
-{
-	BASE_OBJECT		*apsObjs[MAX_RAYOBJ];
-	SDWORD			aObjDist[MAX_RAYOBJ];
-	BASE_OBJECT		*psObj;
-	SDWORD			i,j, x,y, dist, xdiff,ydiff, furthest = 0;
-
-	// find the objects that intersect the ray
-	numRayObjects = 0;
-	for(i=0; i<numTestObjects; i++)
-	{
-		psObj = apsTestObjects[i];
-		x = (SDWORD)psObj->x;
-		y = (SDWORD)psObj->y;
-		dist = rayPointDist(x1,y1,x2,y2, x,y);
-		if (dist < visObjRadius(psObj) &&
-			numRayObjects < MAX_RAYOBJ)
-		{
-			// object intersects ray, calc squared distance
-			xdiff = x - x1;
-			ydiff = y - y1;
-			aObjDist[numRayObjects] = xdiff*xdiff + ydiff*ydiff - dist*dist;
-			apsObjs[numRayObjects] = psObj;
-			numRayObjects += 1;
-		}
-	}
-
-	// reorder the objects on distance
-	for(i=0; i<numRayObjects; i++)
-	{
-		dist = SDWORD_MAX;
-#ifdef DEBUG
-		furthest = -1;
-#else
-		furthest = 0;
-#endif
-		for(j=0; j<numRayObjects; j++)
-		{
-			if (aObjDist[j] < dist)
-			{
-				furthest = j;
-				dist = aObjDist[j];
-			}
-		}
-		ASSERT( furthest != -1,
-			"visGetRayObjects: reordering failed" );
-
-		apsRayObjects[i] = apsObjs[furthest];
-		aRayObjDist[i] = aObjDist[furthest];
-		aObjDist[furthest] = SDWORD_MAX;
-	}
-}
-
-////////////////////////////////////////////////////////////////////
-// alexl's sensor range.
-
-BOOL bDisplaySensorRange;
 
 void startSensorDisplay(void)
 {

lib/framework/debug.c
@@ -59,6 +59,7 @@ static const char *code_part_names[] = {
 	"movement",
 	"attack",
 	"fog",
+	"sensor",
 	"last"
 };
 

lib/framework/debug.h
@@ -116,6 +116,7 @@ typedef enum {
   LOG_MOVEMENT,
   LOG_ATTACK,
   LOG_FOG,
+  LOG_SENSOR,
   LOG_LAST /* _must_ be last! */
 } code_part;
 

src/combat.c
@@ -102,13 +102,10 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 	SDWORD			longRange;
 	DROID			*psDroid = NULL;
 	SDWORD			level, cmdLevel;
-	BOOL			bMissVisible;
-	//Watermelon:minOffset
 	int				minOffset = 5;
 	//Watermelon:predicted X,Y offset per sec
 	SDWORD			predictX;
 	SDWORD			predictY;
-	//Watermelon:dist
 	SDWORD			dist;
 
 	CHECK_OBJECT(psAttacker);
@@ -131,11 +128,11 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 	/* Get the stats for the weapon */
 	psStats = asWeaponStats + psWeap->nStat;
 
-    //check valid weapon/prop combination
-    if (!validTarget(psAttacker, psTarget, weapon_slot))
-    {
-        return;
-    }
+	// check valid weapon/prop combination
+	if (!validTarget(psAttacker, psTarget, weapon_slot))
+	{
+		return;
+	}
 
 	/*see if reload-able weapon and out of ammo*/
 	if (psStats->reloadTime && !psWeap->ammo)
@@ -146,7 +143,7 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 		}
 		//reset the ammo level
 		psWeap->ammo = psStats->numRounds;
-    }
+	}
 
 	/* See when the weapon last fired to control it's rate of fire */
 	firePause = weaponFirePause(psStats, psAttacker->player);
@@ -174,7 +171,6 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 		firePause += firePause;
 	}
 
-
 	if (gameTime - psWeap->lastFired <= firePause)
 	{
 		/* Too soon to fire again */
@@ -197,7 +193,8 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 		if(!visibleObjWallBlock(psAttacker, psTarget))
 		{
 			// Can't see the target - can't hit it with direct fire
-			debug(LOG_NEVER, "directLOS failed\n");
+			debug(LOG_SENSOR, "combFire(%u[%s]->%u): Droid has no direct line of sight to target",
+			      psAttacker->id, ((DROID *)psAttacker)->aName, psTarget->id);
 			return;
 		}
 	}
@@ -209,71 +206,38 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 		if (!visibleObjWallBlock(psAttacker, psTarget))
 		{
 			// Can't see the target - can't hit it with direct fire
-			debug(LOG_NEVER, "directLOS failed\n");
+			debug(LOG_SENSOR, "combFire(%u[%s]->%u): Structure has no direct line of sight to target",
+			      psAttacker->id, ((STRUCTURE *)psAttacker)->pStructureType->pName, psTarget->id);
 			return;
 		}
 	}
 	else if ( proj_Direct(psStats) )
 	{
+		// VTOL or tall building
 		if (!visibleObject(psAttacker, psTarget))
 		{
 			// Can't see the target - can't hit it with direct fire
-			debug(LOG_NEVER, "directLOS failed\n");
+			debug(LOG_SENSOR, "combFire(%u[%s]->%u): Tall object has no direct line of sight to target",
+			      psAttacker->id, psStats->pName, psTarget->id);
 			return;
 		}
 	}
 	else
 	{
+		// Indirect fire
 		if (!psTarget->visible[psAttacker->player])
 		{
 			// Can't get an indirect LOS - can't hit it with the weapon
-			debug(LOG_NEVER, "indirectLOS failed\n");
+			debug(LOG_SENSOR, "combFire(%u[%s]->%u): Object has no indirect sight of target",
+			      psAttacker->id, psStats->pName, psTarget->id);
 			return;
 		}
 	}
 
-/*	if ( proj_Direct(psStats) ||
-		 ((psAttacker->type == OBJ_DROID) &&
-		  !proj_Direct(psStats) &&
-		   actionInsideMinRange(psDroid, psDroid->psActionTarget)) )
+	// if the turret doesn't turn, check if the attacker is in alignment with the target
+	if (psAttacker->type == OBJ_DROID && !psStats->rotate)
 	{
-		switch (psAttacker->type)
-		{
-		case OBJ_DROID:
-			if (!visibleObjWallBlock(psAttacker, psTarget))
-			{
-				// Can't see the target - can't hit it with direct fire
-				debug( LOG_ATTACK, "directLOS failed\n" );
-				return;
-			}
-			break;
-		default:
-			if (!visibleObject(psAttacker, psTarget))
-			{
-				// Can't see the target - can't hit it with direct fire
-				debug( LOG_ATTACK, "directLOS failed\n" );
-				return;
-			}
-			break;
-		}
-	}
-	else
-	{
-		if (!psTarget->visible[psAttacker->player])
-		{
-			// Can't get an indirect LOS - can't hit it with the weapon
-			debug( LOG_ATTACK, "indirectLOS failed\n");
-			return;
-		}
-	}*/
-
-	// if the turret doesn't turn, check the attacker is in alignment with the
-	// target
-	if (psAttacker->type == OBJ_DROID &&
-		!psStats->rotate)
-	{
-		targetDir = (SDWORD)calcDirection(psAttacker->x,psAttacker->y,
-										  psTarget->x,psTarget->y);
+		targetDir = calcDirection(psAttacker->x, psAttacker->y, psTarget->x, psTarget->y);
 		dirDiff = labs(targetDir - (SDWORD)psAttacker->direction);
 		if (dirDiff > FIXED_TURRET_DIR)
 		{
@@ -283,6 +247,7 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 
 	// base modifier 100% of original
 	hitMod = 100;
+
 	// base hit increment of zero
 	hitInc = 0;
 
@@ -344,14 +309,14 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 		hitMod = 50*hitMod/100;
 	}
 
-	debug(LOG_NEVER, "%s Hit mod %d : ", psStats->pName, hitMod);
+	debug(LOG_SENSOR, "combFire: %u[%s]->%u: hitMod=%d, hitInc=%d, visibility=%hhu : ", 
+	      psAttacker->id, psStats->pName, psTarget->id, hitMod, hitInc, psTarget->visible[psAttacker->player]);
 
 	/* Now see if the target is in range  - also check not too near*/
 	xDiff = abs(psAttacker->x - psTarget->x);
 	yDiff = abs(psAttacker->y - psTarget->y);
 	distSquared = xDiff*xDiff + yDiff*yDiff;
-	//Watermelon:dist
-	dist = (SDWORD)sqrtf(distSquared);
+	dist = sqrtf(distSquared);
 	longRange = proj_GetLongRange(psStats);
 	if (distSquared <= (psStats->shortRange * psStats->shortRange) &&
 		distSquared >= (psStats->minRange * psStats->minRange))
@@ -369,7 +334,6 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 		/* NEED TO TAKE ACCOUNT OF ECM, BODY SHAPE ETC. */
 		/************************************************/
 		HIT_ROLL(dice);
-		//if (dice <= psStats->shortHit * hitMod /100)
 		if (dice <= (weaponShortHit(psStats,psAttacker->player) * hitMod /100) + hitInc)
 		{
 			/* Kerrrbaaang !!!!! a hit */
@@ -392,12 +356,12 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 				predictX = psTarget->x;
 				predictY = psTarget->y;
 			}
-			debug(LOG_NEVER, "Shot hit (%d)\n", dice);
+			debug(LOG_SENSOR, "combFire: Accurate prediction short range (%d)", dice);
 			if (!proj_SendProjectile(psWeap, psAttacker, psAttacker->player,
 								predictX, predictY, psTarget->z, psTarget, FALSE, FALSE, weapon_slot))
 			{
 				/* Out of memory - we can safely ignore this */
-				debug(LOG_NEVER, "Out of memory");
+				debug(LOG_ERROR, "Out of memory");
 				return;
 			}
 		}
@@ -426,7 +390,6 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 		/* NEED TO TAKE ACCOUNT OF ECM, BODY SHAPE ETC. */
 		/************************************************/
 		HIT_ROLL(dice);
-		//if (dice <= psStats->longHit * hitMod /100)
 		if (dice <= (weaponLongHit(psStats,psAttacker->player) * hitMod /100) + hitInc)
 		{
 			/* Kerrrbaaang !!!!! a hit */
@@ -449,12 +412,12 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 				predictX = psTarget->x;
 				predictY = psTarget->y;
 			}
-			debug(LOG_NEVER, "Shot hit (%d)\n", dice);
+			debug(LOG_SENSOR, "combFire: Accurate prediction long range (%d)", dice);
 			if (!proj_SendProjectile(psWeap, psAttacker, psAttacker->player,
 								predictX, predictY, psTarget->z, psTarget, FALSE, FALSE, weapon_slot))
 			{
 				/* Out of memory - we can safely ignore this */
-				debug(LOG_NEVER, "Out of memory");
+				debug(LOG_ERROR, "Out of memory");
 				return;
 			}
 		}
@@ -466,7 +429,7 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 	else
 	{
 		/* Out of range */
-		debug(LOG_NEVER, "Out of range\n");
+		debug(LOG_NEVER, "combFire(%u[%s]->%u): Out of range", psAttacker->id, psStats->pName, psTarget->id);
 		return;
 	}
 
@@ -474,47 +437,18 @@ void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget, in
 
 missed:
 	/* Deal with a missed shot */
-	debug(LOG_NEVER, "Missed shot (%d)\n", dice);
 
-	/*
-	// Approximate the distance between the attacker and target
-	xDiff = ABSDIF(psAttacker->x,psTarget->x);
-	yDiff = ABSDIF(psAttacker->y,psTarget->y);
-	missDist = (xDiff > yDiff ? xDiff + (yDiff >> 1) : yDiff + (xDiff >> 1));
-
-	// Calculate where the shot will end up
-	missDist = missDist >> 2;
-	if (missDist < MIN_MISSDIST)
-	{
-		missDist = MIN_MISSDIST;
-	}
-	missDir = rand() % BUL_MAXSCATTERDIR;
-	missX = aScatterDir[missDir].x * (rand() % missDist) + psTarget->x;
-	missY = aScatterDir[missDir].y * (rand() % missDist) + psTarget->y;
-	*/
-	//Watermelon:add a more 'accurate' miss
 	missDist = 2 * (100 - (weaponLongHit(psStats,psAttacker->player) * hitMod /100) + hitInc);
 	missDir = rand() % BUL_MAXSCATTERDIR;
 	missX = aScatterDir[missDir].x * missDist + psTarget->x + minOffset;
 	missY = aScatterDir[missDir].y * missDist + psTarget->y + minOffset;
 
-	debug(LOG_NEVER, "Miss Loc: w(%4d,%4d), t(%3d,%3d)\n",
-		missX, missY, missX>>TILE_SHIFT, missY>>TILE_SHIFT);
+	debug(LOG_NEVER, "combFire: Missed shot (%d) ended up at (%4d,%4d)", dice, missX, missY);
 
-	// decide if a miss is visible
-	bMissVisible = FALSE;
-	if (psTarget->player == selectedPlayer)
-	{
-		bMissVisible = TRUE;
-	}
-
-	/* Fire off the bullet to the miss location */
-	if (!proj_SendProjectile( psWeap, psAttacker, psAttacker->player, missX,missY, psTarget->z, NULL, bMissVisible, FALSE, weapon_slot) )
-	{
-		/* Out of memory */
-		debug(LOG_NEVER, "Out of memory");
-		return;
-	}
+	/* Fire off the bullet to the miss location. The miss is only visible if the player owns
+	 * the target. (Why? - Per) */
+	proj_SendProjectile(psWeap, psAttacker, psAttacker->player, missX,missY, psTarget->z, NULL, 
+	                    psTarget->player == selectedPlayer, FALSE, weapon_slot);
 
 	return;
 }

commit 06e89c20b2484843effa9fa5bdb944bd75918846

Fix bug that made enemy units invisible, introduced in r2320.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2323 4a71c877-e1ca-e34f-864e-861f7616d084

src/visibility.c
@@ -51,8 +51,8 @@ static SDWORD			numRayObjects = 0;
 static SDWORD			currObj;
 
 // rate to change visibility level
-#define VIS_LEVEL_INC		255*2
-#define VIS_LEVEL_DEC		50
+const static float VIS_LEVEL_INC = 255 * 2;
+const static float VIS_LEVEL_DEC = 50;
 
 // fractional accumulator of how much to change visibility this frame
 static float			visLevelIncAcc, visLevelDecAcc;