Jeanosasco/WarFrontier

commit e202869ac05114cafa60845797590fdbc7c78f16

Commit patch #878 by Coincidence with some modifications by me. This makes it so that experience gained is now linked to the 'quality' of the attacker relative to the victim.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3015 4a71c877-e1ca-e34f-864e-861f7616d084

src/projectile.c
@@ -244,6 +244,21 @@ proj_GetNext( void )
 
 /***************************************************************************/
 
+/*
+ * Relates the quality of the attacker to the quality of the victim.
+ * Returns 1.0 if both the attacker and victim are of equal value,
+ * else if Q > 1.0 then the victim is worth more than the attacker
+ * else if Q < 1.0 then the attacker is worth less than the attacker.
+ */
+static float QualityFactor(DROID *psAttacker, DROID *psVictim)
+{
+	float quality = calcDroidPower(psVictim) / calcDroidPower(psAttacker)
+	              + calcDroidPoints(psVictim) / calcDroidPoints(psAttacker)
+	              + psVictim->numKills / psAttacker->numKills;
+	
+	return quality / 3;
+}
+
 // update the kills after a target is damaged/destroyed
 static void proj_UpdateKills(PROJECTILE *psObj, SDWORD percentDamage)
 {
@@ -259,7 +274,7 @@ static void proj_UpdateKills(PROJECTILE *psObj, SDWORD percentDamage)
 	}
 
 	// If percentDamage is negative then the target was killed
-	if(bMultiPlayer && percentDamage < 0)
+	if (bMultiPlayer && percentDamage < 0)
 	{
 		sendDestroyExtra(psObj->psDest,psObj->psSource);
 		updateMultiStatsKills(psObj->psDest,psObj->psSource->player);
@@ -268,30 +283,36 @@ static void proj_UpdateKills(PROJECTILE *psObj, SDWORD percentDamage)
 	// Since we are no longer interested if it was killed or not, abs it
 	percentDamage = abs(percentDamage);
 
-	if(psObj->psSource->type == OBJ_DROID)			/* update droid kills */
+	if (psObj->psSource->type == OBJ_DROID)			/* update droid kills */
 	{
-		psDroid = (DROID*)psObj->psSource;
-		psDroid->numKills += percentDamage;
-		cmdDroidUpdateKills(psDroid, percentDamage);
-		if (orderStateObj(psDroid, DORDER_FIRESUPPORT, &psSensor))
+		psDroid = (DROID *) psObj->psSource;
+		
+		// If it is 'droid-on-droid' then modify the experience by the Quality factor
+		if (psObj->psDest != NULL && psObj->psDest->type == OBJ_DROID)
 		{
-            if (psSensor->type == OBJ_DROID)
-            {
-			    ((DROID *)psSensor)->numKills += percentDamage;
-            }
+			// Modify the experience gained by the 'quality factor' of the units
+			percentDamage *= QualityFactor(psDroid, (DROID *) psObj->psDest);
+		}
+
+		psDroid->numKills += percentDamage;		
+		cmdDroidUpdateKills(psDroid, percentDamage);
+		
+		if (orderStateObj(psDroid, DORDER_FIRESUPPORT, &psSensor)
+		 && psSensor->type == OBJ_DROID)
+		{
+		    ((DROID *) psSensor)->numKills += percentDamage;
 		}
 	}
 	else if (psObj->psSource->type == OBJ_STRUCTURE)
 	{
-		// see if there was a command droid designating this target
+		// See if there was a command droid designating this target
 		psDroid = cmdDroidGetDesignator(psObj->psSource->player);
-		if (psDroid != NULL)
+		
+		if (psDroid != NULL
+		 && psDroid->action == DACTION_ATTACK
+		 && psDroid->psActionTarget[0] == psObj->psDest)
 		{
-			if ((psDroid->action == DACTION_ATTACK) &&
-				(psDroid->psActionTarget[0] == psObj->psDest))
-			{
-				psDroid->numKills += percentDamage;
-			}
+			psDroid->numKills += percentDamage;
 		}
 	}
 }

src/droid.c
@@ -3212,6 +3212,26 @@ UDWORD	calcDroidPower(DROID *psDroid)
 	return power;
 }
 
+UDWORD calcDroidPoints(DROID *psDroid)
+{
+	int points, i;
+	
+	points  = getBodyStats(psDroid)->buildPoints;
+	points += getBrainStats(psDroid)->buildPoints;
+	points += getPropulsionStats(psDroid)->buildPoints;
+	points += getSensorStats(psDroid)->buildPoints;
+	points += getECMStats(psDroid)->buildPoints;
+	points += getRepairStats(psDroid)->buildPoints;
+	points += getConstructStats(psDroid)->buildPoints;
+	
+	for (i = 0; i < psDroid->numWeaps; i++)
+	{
+		points += (asWeaponStats + psDroid->asWeaps[i].nStat)->buildPoints;
+	}
+	
+	return points;
+}
+
 //Builds an instance of a Droid - the x/y passed in are in world coords.
 DROID* buildDroid(DROID_TEMPLATE *pTemplate, UDWORD x, UDWORD y, UDWORD player,
 				  BOOL onMission)

src/droid.h
@@ -126,6 +126,9 @@ extern UDWORD calcDroidWeight(DROID_TEMPLATE *psTemplate);
 /* Calculate the power points required to build/maintain a droid */
 extern UDWORD calcDroidPower(DROID *psDroid);
 
+// Calculate the number of points required to build a droid
+extern UDWORD calcDroidPoints(DROID *psDroid);
+
 /* Calculate the body points of a droid from it's template */
 extern UDWORD calcTemplateBody(DROID_TEMPLATE *psTemplate, UBYTE player);
 

commit 071e406fc90424ae357692aca2991a8585091823

Fix a nasty division by zero error introduced in r3015. Also fix balancing in single player as a result of changes in r3015.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3016 4a71c877-e1ca-e34f-864e-861f7616d084

src/projectile.c
@@ -254,7 +254,8 @@ static float QualityFactor(DROID *psAttacker, DROID *psVictim)
 {
 	float quality = calcDroidPower(psVictim) / calcDroidPower(psAttacker)
 	              + calcDroidPoints(psVictim) / calcDroidPoints(psAttacker)
-	              + psVictim->numKills / psAttacker->numKills;
+	              // Make sure psAttacker has some kills to prevent div by 0
+	              + (psAttacker->numKills) ? psVictim->numKills / psAttacker->numKills : 1;
 	
 	return quality / 3;
 }
@@ -288,7 +289,10 @@ static void proj_UpdateKills(PROJECTILE *psObj, SDWORD percentDamage)
 		psDroid = (DROID *) psObj->psSource;
 		
 		// If it is 'droid-on-droid' then modify the experience by the Quality factor
-		if (psObj->psDest != NULL && psObj->psDest->type == OBJ_DROID)
+		// Only do this in MP so to not un-balance the campaign
+		if (psObj->psDest != NULL
+		 && psObj->psDest->type == OBJ_DROID
+		 && bMultiPlayer)
 		{
 			// Modify the experience gained by the 'quality factor' of the units
 			percentDamage *= QualityFactor(psDroid, (DROID *) psObj->psDest);