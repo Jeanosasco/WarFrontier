Jeanosasco/WarFrontier

commit 2bcc7eab09ce6f848b17ceba62bff121e867d302

* Get rid of cheap modulus calculating macro MODFRACT and use function fmodf instead
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3130 4a71c877-e1ca-e34f-864e-861f7616d084

src/warcam.c
@@ -54,10 +54,6 @@
 #include "display3d.h"
 #include "selection.h"
 
-#define MODFRACT(value,mod) \
-	while((value) < 0)	{ (value) += (mod); } \
-	while((value) > (mod)) { (value) -= (mod); }
-
 #define MIN_TRACK_HEIGHT 16
 
 /* Holds all the details of our camera */
@@ -1004,7 +1000,7 @@ static void updateCameraRotationAcceleration( UBYTE update )
 
 		/* Which way are we facing? */
 		worldAngle =  trackingCamera.rotation.y;
-		separation = (float) ((yConcern - worldAngle));
+		separation = yConcern - worldAngle;
 		if(separation < DEG(-180))
 		{
 			separation += DEG(360);
@@ -1050,17 +1046,13 @@ static void updateCameraRotationAcceleration( UBYTE update )
 				trackingCamera.rotation.x+=DEG(360);
 			}
 		worldAngle =  trackingCamera.rotation.x;
-		separation = (float) ((xConcern - worldAngle));
+		separation = fmodf(xConcern - worldAngle, DEG(360));
 
-		MODFRACT(separation,DEG(360));
-
-		if(separation<DEG(-180))
+		// Make sure that rotations larger than 180 degrees are noted
+		// in the range of -180 - 0 degrees.
+		if (separation > DEG(180))
 		{
-			separation+=DEG(360);
-		}
-		else if(separation>DEG(180))
-		{
-			separation-=DEG(360);
+			separation -= DEG(360);
 		}
 
 		/* Make new acceleration */

commit 2f6c14debe21997a714f91ff79d6c3c323509646

Fix the warcam (pressing space) that broke in r3130, and remove an unused variable + add a newline to the end of a file to make the code compile with -Werror again.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3366 4a71c877-e1ca-e34f-864e-861f7616d084

src/scriptfuncs.c
@@ -7464,7 +7464,7 @@ BOOL scrMapRevealedInRange(void)
 /* Returns true if a certain map tile was revealed, ie fog of war was removed */
 BOOL scrMapTileVisible(void)
 {
-	SDWORD		tileX,tileY,wRange,player;
+	SDWORD		tileX,tileY,player;
 
 	if (!stackPopParams(3, VAL_INT, &tileX, VAL_INT, &tileY, VAL_INT, &player))
 	{

src/warcam.c
@@ -1026,13 +1026,17 @@ static void updateCameraRotationAcceleration( UBYTE update )
 				trackingCamera.rotation.x+=DEG(360);
 			}
 		worldAngle =  trackingCamera.rotation.x;
-		separation = fmodf(xConcern - worldAngle, DEG(360));
+		separation = xConcern - worldAngle;
 
-		// Make sure that rotations larger than 180 degrees are noted
-		// in the range of -180 - 0 degrees.
-		if (separation > DEG(180))
+		// Make sure that rotations are noted
+		// in the range of -180 - 180 degrees.
+		while (separation > DEG(180))
 		{
 			separation -= DEG(360);
+		}
+		while (separation < -DEG(180))
+		{
+			separation += DEG(360);
 		}
 
 		/* Make new acceleration */