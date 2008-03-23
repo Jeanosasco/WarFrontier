Jeanosasco/WarFrontier

commit e6395e096eecf0d921443e58afe8c99ca412573f

Generalize the damage function and move some common variables between the various object types into the base object type. This also makes the recently added minimum damage change apply to structures and features as well. See patch #1000.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4013 4a71c877-e1ca-e34f-864e-861f7616d084

src/multisync.c
@@ -709,7 +709,7 @@ static BOOL sendStructureCheck(void)
 		NETbeginEncode(NET_CHECK_STRUCT, NET_ALL_PLAYERS);
 			NETuint8_t(&pS->player);
 			NETuint32_t(&pS->id);
-			NETuint16_t(&pS->body);
+			NETuint32_t(&pS->body);
 			NETuint32_t(&pS->pStructureType->ref);
 			NETuint16_t(&pS->pos.x);
 			NETuint16_t(&pS->pos.y);

src/basedef.h
@@ -53,16 +53,14 @@ typedef enum _object_type
 	SCREEN_DISP_DATA    sDisplay;                   /**< screen coordinate details */ \
 	UBYTE               player;                     /**< Which player the object belongs to */ \
 	UBYTE               group;                      /**< Which group selection is the droid currently in? */ \
-	UBYTE               selected;                   /**< Whether the object is selected */ \
-	                                                /* might want this elsewhere */ \
+	UBYTE               selected;                   /**< Whether the object is selected (might want this elsewhere) */ \
 	UBYTE               cluster;                    /**< Which cluster the object is a member of */ \
 	UBYTE               visible[MAX_PLAYERS];       /**< Whether object is visible to specific player */ \
 	UDWORD              died;                       /**< When an object was destroyed, if 0 still alive */ \
-	/*BOOL            (*damage)(pointerType* psObject, UDWORD damage, UDWORD weaponClass); - Damage function */ \
-	/*UDWORD              emissionInterval;*/           /**< How frequently does it puff out damage smoke? */ \
 	UDWORD              lastEmission;               /**< When did it last puff out smoke? */ \
-\
-	/* Data for fire damage */ \
+	UDWORD              lastHitWeapon;		/**< The weapon that last hit it */ \
+	UDWORD              timeLastHit;		/**< The time the structure was last attacked */ \
+	UDWORD              body;			/**< Hit points with lame name */ \
 	BOOL                inFire;                     /**< TRUE if the object is in a fire */ \
 	UDWORD              burnStart;                  /**< When the object entered the fire */ \
 	UDWORD              burnDamage;                 /**< How much damage has been done since the object entered the fire */ \

src/cluster.c
@@ -670,8 +670,6 @@ void clustObjectAttacked(BASE_OBJECT *psObj)
 {
 	if ((aClusterAttacked[psObj->cluster] + ATTACK_CB_PAUSE) < gameTime)
 	{
-//		DBPRINTF(("CALL_ATTACKED player %d, cluster %d\n",
-//			psObj->player, psObj->cluster));
 		psScrCBTarget = psObj;
 		eventFireCallbackTrigger((TRIGGER_TYPE)CALL_ATTACKED);
 
@@ -689,7 +687,7 @@ void clustObjectAttacked(BASE_OBJECT *psObj)
 				break;
 			default:
 				ASSERT(!"invalid object type", "clustObjectAttacked: invalid object type");
-				break;
+				return;
 		}
 
 		aClusterAttacked[psObj->cluster] = gameTime;

src/combat.c
@@ -34,6 +34,7 @@
 #include "lib/gamelib/gtime.h"
 #include "map.h"
 #include "move.h"
+#include "cluster.h"
 #include "messagedef.h"
 #include "miscimd.h"
 #include "projectile.h"
@@ -44,6 +45,7 @@
 #include "order.h"
 #include "ai.h"
 #include "action.h"
+#include "difficulty.h"
 
 /* minimum miss distance */
 #define MIN_MISSDIST	(TILE_UNITS/6)
@@ -473,4 +475,83 @@ void counterBatteryFire(BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget)
 			}
 		}
 	}
+}
+
+/* Deals damage to an object
+ * \param psObj object to deal damage to
+ * \param damage amount of damage to deal
+ * \param weaponClass the class of the weapon that deals the damage
+ * \param weaponSubClass the subclass of the weapon that deals the damage
+ * \param angle angle of impact (from the damage dealing projectile in relation to this object)
+ * \return > 0 when the dealt damage destroys the object, < 0 when the object survives
+ */
+float objDamage(BASE_OBJECT *psObj, UDWORD damage, UDWORD originalhp, UDWORD weaponClass, UDWORD weaponSubClass, HIT_SIDE impactSide)
+{
+	int	actualDamage, armour, level = 1;
+
+	// If the previous hit was by an EMP cannon and this one is not:
+	// don't reset the weapon class and hit time
+	// (Giel: I guess we need this to determine when the EMP-"shock" is over)
+	if (psObj->lastHitWeapon != WSC_EMP || weaponSubClass == WSC_EMP)
+	{
+		psObj->timeLastHit = gameTime;
+		psObj->lastHitWeapon = weaponSubClass;
+	}
+
+	// EMP cannons do no damage, if we are one return now
+	if (weaponSubClass == WSC_EMP)
+	{
+		return 0;
+	}
+
+	if (psObj->player != selectedPlayer)
+	{
+		// Player inflicting damage on enemy.
+		damage = (UDWORD) modifyForDifficultyLevel(damage,TRUE);
+	}
+	else
+	{
+		// Enemy inflicting damage on player.
+		damage = (UDWORD) modifyForDifficultyLevel(damage,FALSE);
+	}
+
+	armour = psObj->armour[impactSide][weaponClass];
+
+	debug(LOG_ATTACK, "objDamage(%d): body %d armour %d damage: %d", psObj->id, psObj->body, armour, damage);
+
+	if (psObj->type == OBJ_STRUCTURE || psObj->type == OBJ_DROID)
+	{
+		clustObjectAttacked((BASE_OBJECT *)psObj);
+	}
+
+
+	if (psObj->type == OBJ_DROID)
+	{
+		DROID *psDroid = (DROID *)psObj;
+
+		// Retrieve highest, applicable, experience level
+		level = getDroidEffectiveLevel(psDroid);
+	}
+
+	// Reduce damage taken by EXP_REDUCE_DAMAGE % for each experience level
+	actualDamage = (damage * (100 - EXP_REDUCE_DAMAGE * level)) / 100;
+
+	// You always do at least a third of the experience modified damage
+	actualDamage = MAX(actualDamage - armour, actualDamage / 3);
+
+	// And at least MIN_WEAPON_DAMAGE points
+	actualDamage = MAX(actualDamage, MIN_WEAPON_DAMAGE);
+
+	objTrace(LOG_ATTACK, psObj->id, "objDamage: Penetrated %d", actualDamage);
+
+	// If the shell did sufficient damage to destroy the object, deal with it and return
+	if (actualDamage >= psObj->body)
+	{
+		return (float) psObj->body / (float) originalhp * -1.0f;
+	}
+
+	// Substract the dealt damage from the droid's remaining body points
+	psObj->body -= actualDamage;
+
+	return (float) actualDamage / (float) originalhp;
 }

src/combat.h
@@ -54,7 +54,7 @@ extern void combFire(WEAPON *psWeap, BASE_OBJECT *psAttacker, BASE_OBJECT *psTar
 if any support a counter battery sensor*/
 extern void counterBatteryFire(BASE_OBJECT *psAttacker, BASE_OBJECT *psTarget);
 
+extern float objDamage(BASE_OBJECT *psObj, UDWORD damage, UDWORD originalhp, UDWORD weaponClass, 
+                       UDWORD weaponSubClass, HIT_SIDE impactSide);
+
 #endif
-
-
-

src/droid.c
@@ -81,6 +81,7 @@
 #include "edit3d.h"
 #include "scores.h"
 #include "research.h"
+#include "combat.h"
 // FIXME Direct iVis implementation include!
 #include "lib/ivis_opengl/screen.h"
 #include "scriptfuncs.h"			//for ThreatInRange()
@@ -147,86 +148,47 @@ BOOL droidInit(void)
  */
 float droidDamage(DROID *psDroid, UDWORD damage, UDWORD weaponClass, UDWORD weaponSubClass, HIT_SIDE impactSide)
 {
-	UDWORD				actualDamage, armour, level;
-	float				originalBody = psDroid->originalBody;
-	float				body = psDroid->body;
-	SECONDARY_STATE		state;
+	float		relativeDamage;
+	SECONDARY_STATE	state;
 
 	CHECK_DROID(psDroid);
 
-	// If the previous hit was by an EMP cannon and this one is not:
-	// don't reset the weapon class and hit time
-	// (Giel: I guess we need this to determine when the EMP-"shock" is over)
-	if (psDroid->lastHitWeapon != WSC_EMP || weaponSubClass == WSC_EMP)
-	{
-		psDroid->timeLastHit = gameTime;
-		psDroid->lastHitWeapon = weaponSubClass;
-	}
-
-	// EMP cannons do no damage, if we are one return now
-	if (weaponSubClass == WSC_EMP)
-	{
-		return 0;
-	}
-
-	if (psDroid->player != selectedPlayer)
-	{
-		// Player inflicting damage on enemy.
-		damage = (UDWORD) modifyForDifficultyLevel( (SDWORD) damage,TRUE);
-	}
-	else
-	{
-		// Enemy inflicting damage on player.
-		damage = (UDWORD) modifyForDifficultyLevel( (SDWORD) damage,FALSE);
-	}
-
 	// VTOLs on the ground take triple damage
-	if (vtolDroid(psDroid) &&
-		psDroid->sMove.Status == MOVEINACTIVE)
+	if (vtolDroid(psDroid) && psDroid->sMove.Status == MOVEINACTIVE)
 	{
 		damage *= 3;
 	}
 
-	// reset the attack level
-	if (secondaryGetState(psDroid, DSO_ATTACK_LEVEL, &state)
-	    && state == DSS_ALEV_ATTACKED)
+	relativeDamage = objDamage((BASE_OBJECT *)psDroid, damage, psDroid->originalBody, weaponClass, weaponSubClass, impactSide);
+
+	if (relativeDamage > 0.0f)
 	{
-		secondarySetState(psDroid, DSO_ATTACK_LEVEL, DSS_ALEV_ALWAYS);
+		// reset the attack level
+		if (secondaryGetState(psDroid, DSO_ATTACK_LEVEL, &state) && state == DSS_ALEV_ATTACKED)
+		{
+			secondarySetState(psDroid, DSO_ATTACK_LEVEL, DSS_ALEV_ALWAYS);
+		}
+		// Now check for auto return on droid's secondary orders (i.e. return on medium/heavy damage)
+		secondaryCheckDamageLevel(psDroid);
+
+		// Now check for scripted run-away based on health left
+		orderHealthCheck(psDroid);
+
+		CHECK_DROID(psDroid);
+
+		return relativeDamage;
 	}
-
-	armour = psDroid->armour[impactSide][weaponClass];
-
-	debug( LOG_ATTACK, "unitDamage(%d): body %d armour %d damage: %d\n",
-		psDroid->id, psDroid->body, armour, damage);
-
-	clustObjectAttacked((BASE_OBJECT *)psDroid);
-
-
-	// Retrieve highest, applicable, experience level
-	level = getDroidEffectiveLevel(psDroid);
-
-	// Apply droid/commander experience to the droid damage
-	actualDamage = (damage * (100 - EXP_REDUCE_DAMAGE * level)) / 100;
-
-	// Calculate final damage
-	actualDamage = (UDWORD)MAX((SDWORD)(actualDamage - armour), (SDWORD)(actualDamage / 3));
-
-	// Make sure we substract at least MIN_WEAPON_DAMAGE points from the target hp
-	actualDamage = MAX(actualDamage, MIN_WEAPON_DAMAGE);
-
-
-	// If the shell did sufficient damage to destroy the droid, deal with it and return
-	if (actualDamage >= psDroid->body)
+	else if (relativeDamage < 0.0f)
 	{
 		// HACK: Prevent transporters from being destroyed in single player
 		if (!bMultiPlayer && psDroid->droidType == DROID_TRANSPORTER)
 		{
 			psDroid->body = 1;
-			return 0;
+			return 0.0f;
 		}
 
 		// Droid destroyed
-		debug( LOG_ATTACK, "        DESTROYED\n");
+		debug(LOG_ATTACK, "droidDamage(%d): DESTROYED", psDroid->id);
 
 		// Deal with score increase/decrease and messages to the player
 		if( psDroid->player == selectedPlayer)
@@ -254,22 +216,12 @@ float droidDamage(DROID *psDroid, UDWORD damage, UDWORD weaponClass, UDWORD weap
 			destroyDroid(psDroid);
 		}
 
-		return body / originalBody * -1.0f;
+		return relativeDamage * -1.0f;
 	}
-
-	// Substract the dealt damage from the droid's remaining body points
-	psDroid->body -= actualDamage;
-
-	// Now check for auto return on droid's secondary orders (i.e. return on medium/heavy damage)
-	secondaryCheckDamageLevel(psDroid);
-
-	// Now check for scripted run-away based on health left
-	orderHealthCheck(psDroid);
-
-	CHECK_DROID(psDroid);
-
-	// Return the amount of damage done as an SDWORD between 0 and 999
-	return (float) actualDamage / originalBody;
+	else
+	{
+		return 0.0f;	// undamaged
+	}
 }
 
 // Check that psVictimDroid is not referred to by any other object in the game

src/droiddef.h
@@ -152,7 +152,6 @@ typedef struct _droid
 	UDWORD          weight;
 	UDWORD          baseSpeed;                      ///< the base speed dependant on propulsion type
 	UDWORD          originalBody;                   ///< the original body points
-	UDWORD          body;                           ///< the current body points
 	float           experience;
 	UWORD           turretRotation[DROID_MAXWEAPS]; ///< Watermelon:turretRotation info for multiple turrents :)
 	UWORD           turretPitch[DROID_MAXWEAPS];    ///< Watermelon:turrentPitch info for multiple turrents :)
@@ -176,8 +175,6 @@ typedef struct _droid
 	UWORD           orderX, orderY;
 	UWORD           orderX2, orderY2;
 
-	UDWORD          lastHitWeapon;
-	UDWORD          timeLastHit;
 	BOOL            bTargetted;
 
 	BASE_OBJECT*    psTarget;                       ///< Order target

src/feature.c
@@ -50,7 +50,7 @@
 #include "effects.h"
 #include "geometry.h"
 #include "scores.h"
-
+#include "combat.h"
 #include "multiplay.h"
 #include "advvis.h"
 
@@ -254,44 +254,25 @@ void featureStatsShutDown(void)
  */
 float featureDamage(FEATURE *psFeature, UDWORD damage, UDWORD weaponClass, UDWORD weaponSubClass, HIT_SIDE impactSide)
 {
-	// Do at least one point of damage
-	unsigned int actualDamage = 1;
-	float		body = (float) psFeature->body;
-	float		originalBody = (float) psFeature->psStats->body;
+	float		relativeDamage;
 
-	ASSERT( psFeature != NULL,
-		"featureDamage: Invalid feature pointer" );
+	ASSERT(psFeature != NULL, "featureDamage: Invalid feature pointer");
 
-	debug( LOG_ATTACK, "featureDamage(%d): body %d armour %d damage: %d\n",
-		psFeature->id, psFeature->body, psFeature->armour[impactSide][weaponClass], damage);
+	debug(LOG_ATTACK, "featureDamage(%d): body %d armour %d damage: %d",
+	      psFeature->id, psFeature->body, psFeature->armour[impactSide][weaponClass], damage);
 
-	// EMP cannons do not work on Features
-	if (weaponSubClass == WSC_EMP)
-	{
-		return 0;
-	}
-
-	if (damage > psFeature->armour[impactSide][weaponClass])
-	{
-		// Damage has penetrated - reduce body points
-		actualDamage = damage - psFeature->armour[impactSide][weaponClass];
-		debug( LOG_ATTACK, "        penetrated: %d\n", actualDamage);
-	}
+	relativeDamage = objDamage((BASE_OBJECT *)psFeature, damage, psFeature->psStats->body, weaponClass, weaponSubClass, impactSide);
 
 	// If the shell did sufficient damage to destroy the feature
-	if (actualDamage >= psFeature->body)
+	if (relativeDamage < 0.0f)
 	{
 		destroyFeature(psFeature);
-		return body / originalBody * -1.0f;
+		return relativeDamage * -1.0f;
 	}
-
-	// Substract the dealt damage from the feature's remaining body points
-	psFeature->body -= actualDamage;
-
-	// Set last hit-time to <now>
-	psFeature->timeLastHit = gameTime;
-
-	return (float) actualDamage / originalBody;
+	else
+	{
+		return relativeDamage;
+	}
 }
 
 

commit 23f7bb4320e05b4cd92ff487abcc7d850312d757

Fix problem I introduced in r4013 that would make buildings occasionally get 0 hp.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4265 4a71c877-e1ca-e34f-864e-861f7616d084

src/multisync.c
@@ -747,14 +747,14 @@ BOOL recvStructureCheck()
 	int				i, j;
 	float			direction;
 	uint8_t			player, ourCapacity;
-	uint16_t		body;
+	uint32_t		body;
 	uint16_t		x, y, z;
 	uint32_t		ref, type;
 
 	NETbeginDecode(NET_CHECK_STRUCT);
 		NETuint8_t(&player);
 		NETuint32_t(&ref);
-		NETuint16_t(&body);
+		NETuint32_t(&body);
 		NETuint32_t(&type);
 		NETuint16_t(&x);
 		NETuint16_t(&y);