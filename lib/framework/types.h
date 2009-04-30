Jeanosasco/WarFrontier

commit f6785548d8d30a69d8510df36a79c1e4908c45ee

Remove unnecessary SDL includes.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7237 4a71c877-e1ca-e34f-864e-861f7616d084

lib/framework/types.h
@@ -27,8 +27,6 @@
 #include "wzglobal.h"
 
 // Defines C99 types for C99 incompatible compilers (e.g. MSVC)
-#include <SDL_types.h>
-
 #include <limits.h>
 
 #ifdef WZ_C99

lib/framework/math_ext.h
@@ -27,6 +27,7 @@
 
 #include "wzglobal.h"
 #include <math.h>
+#include <stdlib.h>
 
 // Also PERCENT(int,int);	// returns a int value 0->100 of the percentage of the first param over the second
 #define PERCENT(a,b) (((a)*100)/(b))

lib/framework/utf.c
@@ -28,6 +28,7 @@
 
 #include "utf.h"
 #include <assert.h>
+#include <stdlib.h>
 
 #if defined(LIB_COMPILE)
 # define ASSERT(expr, ...) (assert(expr))

lib/ivis_opengl/piedraw.c
@@ -23,7 +23,6 @@
 
 #include "lib/ivis_opengl/GLee.h"
 #include <string.h>
-#include <SDL_video.h>
 
 #include "lib/framework/frame.h"
 #include "lib/ivis_common/ivisdef.h"

src/seqdisp.c
@@ -28,7 +28,6 @@
 #include "lib/framework/frameint.h"
 
 #include <string.h>
-#include <SDL_timer.h>
 #include <physfs.h>
 
 #include "lib/framework/file.h"

src/terrain.c
@@ -30,7 +30,6 @@
 
 #include "lib/ivis_opengl/GLee.h"
 #include <string.h>
-#include <SDL_video.h>
 
 #include "lib/framework/frame.h"
 #include "lib/ivis_common/ivisdef.h"

commit 17008df63efb5f04127cdc737ec1e090796688f6

Fixes compile issue that was broken in r7237.
In this case, 'Remove unnecessary SDL includes.' Removed the #include for C99 types that we use on non C99 compilers.

In case your wondering, using SDL_stdinc.h, since SDL_types.h is Deprecated.

git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7250 4a71c877-e1ca-e34f-864e-861f7616d084

lib/framework/types.h
@@ -25,16 +25,15 @@
 #define __INCLUDED_LIB_FRAMEWORK_TYPES_H__
 
 #include "wzglobal.h"
-
-// Defines C99 types for C99 incompatible compilers (e.g. MSVC)
 #include <limits.h>
-
 #include <ctype.h>
 
 #ifdef WZ_C99
 /* Compilers that have support for C99 have all values below defined in stdint.h */
 # include <stdint.h>
 #else
+// Defines C99 types for C99 incompatible compilers (e.g. MSVC)
+#include <SDL_stdinc.h>
 # define INT8_MIN               (-128)
 # define INT16_MIN              (-32767-1)
 # define INT32_MIN              (-2147483647-1)