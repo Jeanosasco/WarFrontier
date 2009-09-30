Jeanosasco/WarFrontier

commit 7e243a030caa5ed1e1c7fd399d0096803b67c374

Clean up renderStructure() a bit to remove unused or duplicate code. Use OpenGL matrix commands to avoid a lot of manual copying and transformation in pie_Draw3DShape2(), and make the function more readable. Patch reviewed by gerard_
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@8189 4a71c877-e1ca-e34f-864e-861f7616d084

src/display3d.c
@@ -2147,7 +2147,7 @@ void	renderStructure(STRUCTURE *psStructure)
 			if (structureIsBlueprint(psStructure))
 			{
 				pieFlag = pie_TRANSLUCENT;
-				pieFlagData = 120;
+				pieFlagData = BLUEPRINT_OPACITY;
 			}
 			else
 			{
@@ -2169,16 +2169,15 @@ void	renderStructure(STRUCTURE *psStructure)
 		objectShimmy((BASE_OBJECT *)psStructure);
 	}
 
-	//first check if partially built - ANOTHER HACK!
-	if (psStructure->status == SS_BEING_BUILT
-	    || psStructure->status == SS_BEING_DEMOLISHED
-	    || (psStructure->status == SS_BEING_BUILT && psStructure->pStructureType->type == REF_RESOURCE_EXTRACTOR))
+	if (defensive)
 	{
-		if (defensive)
-		{
-			temp = strImd->points;
-			strImd->points = alteredPoints;
-		}
+		temp = strImd->points;
+		strImd->points = alteredPoints;
+	}
+
+	//first check if partially built - ANOTHER HACK!
+	if (psStructure->status == SS_BEING_BUILT || psStructure->status == SS_BEING_DEMOLISHED)
+	{
 		pie_Draw3DShape(strImd, 0, playerFrame, buildingBrightness, WZCOL_BLACK, pie_HEIGHT_SCALED | pie_SHADOW,
 		                (SDWORD)(structHeightScale(psStructure) * pie_RAISE_SCALE));
 		if (defensive)
@@ -2188,11 +2187,6 @@ void	renderStructure(STRUCTURE *psStructure)
 	}
 	else
 	{
-		if (defensive)
-		{
-			temp = strImd->points;
-			strImd->points = alteredPoints;
-		}
 		if (structureIsBlueprint(psStructure))
 		{
 			pieFlag = pie_TRANSLUCENT;
@@ -2257,12 +2251,8 @@ void	renderStructure(STRUCTURE *psStructure)
 					mountImd[0] =  psStructure->pStructureType->pECM->pMountGraphic;
 					flashImd[0] = NULL;
 				}
-			}
-
-			if (weaponImd[0] == NULL)
-			{
 				//check for sensor
-				if (psStructure->pStructureType->pSensor != NULL)
+				else if (psStructure->pStructureType->pSensor != NULL)
 				{
 					weaponImd[0] =  psStructure->pStructureType->pSensor->pIMD;
 					/* No recoil for sensors */

lib/ivis_opengl/piedraw.c
@@ -128,7 +128,6 @@ static unsigned int nb_tshapes = 0;
 
 static void pie_Draw3DShape2(iIMDShape *shape, int frame, PIELIGHT colour, WZ_DECL_UNUSED PIELIGHT specular, int pieFlag, int pieFlagData)
 {
-	Vector3f *pVertices, *pPixels, scrPoints[pie_MAX_VERTICES];
 	iIMDPoly *pPolys;
 	BOOL light = lighting;
 
@@ -179,41 +178,20 @@ static void pie_Draw3DShape2(iIMDShape *shape, int frame, PIELIGHT colour, WZ_DE
 		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
 	}
 
-	if (pieFlag & pie_RAISE)
-	{
-		pieFlagData = (shape->max.y * (pie_RAISE_SCALE - pieFlagData)) / pie_RAISE_SCALE;
-	}
-
 	pie_SetTexturePage(shape->texpage);
 
-	//now draw the shape
-	//rotate and project points from shape->points to scrPoints
-	for (pVertices = shape->points, pPixels = scrPoints;
-			pVertices < shape->points + shape->npoints;
-			pVertices++, pPixels++)
+	if (pieFlag & pie_HEIGHT_SCALED)	// construct
 	{
-		float tempY = pVertices->y;
-		if (pieFlag & pie_RAISE)
-		{
-			tempY = pVertices->y - pieFlagData;
-			if (tempY < 0)
-				tempY = 0;
-
-		}
-		else if ( (pieFlag & pie_HEIGHT_SCALED) && pVertices->y > 0 )
-		{
-			tempY = (pVertices->y * pieFlagData) / pie_RAISE_SCALE;
-		}
-		pPixels->x = pVertices->x;
-		pPixels->y = tempY;
-		pPixels->z = pVertices->z;
+		glScalef(1.0f, (float)pieFlagData / (float)pie_RAISE_SCALE, 1.0f);
+	}
+	if (pieFlag & pie_RAISE)		// collapse
+	{
+		glTranslatef(1.0f, (-shape->max.y * (pie_RAISE_SCALE - pieFlagData)) / pie_RAISE_SCALE, 1.0f);
 	}
 
 	glColor4ubv(colour.vector);	// Only need to set once for entire model
 
-	for (pPolys = shape->polys;
-			pPolys < shape->polys + shape->npolys;
-			pPolys++)
+	for (pPolys = shape->polys; pPolys < shape->polys + shape->npolys; pPolys++)
 	{
 		Vector2f	texCoords[pie_MAX_VERTICES_PER_POLYGON];
 		Vector3f	vertexCoords[pie_MAX_VERTICES_PER_POLYGON];
@@ -224,9 +202,9 @@ static void pie_Draw3DShape2(iIMDShape *shape, int frame, PIELIGHT colour, WZ_DE
 				n < pPolys->npnts;
 				n++, index++)
 		{
-			vertexCoords[n].x = scrPoints[*index].x;
-			vertexCoords[n].y = scrPoints[*index].y;
-			vertexCoords[n].z = scrPoints[*index].z;
+			vertexCoords[n].x = shape->points[*index].x;
+			vertexCoords[n].y = shape->points[*index].y;
+			vertexCoords[n].z = shape->points[*index].z;
 			texCoords[n].x = pPolys->texCoord[n].x;
 			texCoords[n].y = pPolys->texCoord[n].y;
 		}

commit 2285ce0a37356729888cedb955af73e7a2b00e7f

Fix drawing of radar dishes that broke in r8189.
Reported in http://forums.wz2100.net/viewtopic.php?f=4&t=3859.

git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@8195 4a71c877-e1ca-e34f-864e-861f7616d084

src/display3d.c
@@ -2252,7 +2252,7 @@ void	renderStructure(STRUCTURE *psStructure)
 					flashImd[0] = NULL;
 				}
 				//check for sensor
-				else if (psStructure->pStructureType->pSensor != NULL)
+				if (psStructure->pStructureType->pSensor != NULL)
 				{
 					weaponImd[0] =  psStructure->pStructureType->pSensor->pIMD;
 					/* No recoil for sensors */