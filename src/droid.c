Jeanosasco/WarFrontier

commit 7820967fab80a1085ab4e8cd7f36a3d86c07d326

When a droid has completed construction of a building, make sure we clear the orders of every droid that has been helping, to ensure they do not get lagging behind when AI gives new orders the (possibly) next frame.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@9749 4a71c877-e1ca-e34f-864e-861f7616d084

src/structure.c
@@ -1200,15 +1200,26 @@ void structureBuild(STRUCTURE *psStruct, DROID *psDroid, int buildPoints)
 		     in order to be able to start a new built task, doubled in actionUpdateDroid() */
 		if (psDroid)
 		{
-			psDroid->action = DACTION_NONE;
-			psDroid->order = DORDER_NONE;
-			setDroidTarget(psDroid, NULL);
-			setDroidActionTarget(psDroid, NULL, 0);
-			psDroid->psTarStats = NULL;
+			DROID	*psIter;
+
+			// Clear all orders for helping hands. Needed for AI script which runs next frame.
+			for (psIter = apsDroidLists[psDroid->player]; psIter; psIter = psIter->psNext)
+			{
+				if ((psIter->order == DORDER_BUILD || psIter->order == DORDER_HELPBUILD || psIter->order == DORDER_LINEBUILD)
+				    && psIter->psTarget == (BASE_OBJECT *)psStruct)
+				{
+					objTrace(psIter->id, "Construction complete");
+					psIter->action = DACTION_NONE;
+					psIter->order = DORDER_NONE;
+					setDroidTarget(psIter, NULL);
+					setDroidActionTarget(psIter, NULL, 0);
+					psIter->psTarStats = NULL;
+				}
+			}
 
 			/* Notify scripts we just finished building a structure, pass builder and what was built */
 			psScrCBNewStruct	= psStruct;
-			psScrCBNewStructTruck= psDroid;
+			psScrCBNewStructTruck	= psDroid;
 			eventFireCallbackTrigger((TRIGGER_TYPE)CALL_STRUCTBUILT);
 
 			audio_StopObjTrack( psDroid, ID_SOUND_CONSTRUCTION_LOOP );

commit 43170da26feaf3481aba785930146a14566e32fb

Fix line build that was broken by r9749.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@9754 4a71c877-e1ca-e34f-864e-861f7616d084

src/droid.c
@@ -119,6 +119,7 @@ void	droidUpdateRecoil( DROID *psDroid );
 
 static void cancelBuild(DROID *psDroid)
 {
+	objTrace(psDroid->id, "Droid build order cancelled");
 	psDroid->action = DACTION_NONE;
 	psDroid->order = DORDER_NONE;
 	setDroidTarget(psDroid, NULL);
@@ -1089,8 +1090,17 @@ BOOL droidUpdateBuild(DROID *psDroid)
 	{
 		// Update the interface
 		intBuildFinished(psDroid);
-
-		cancelBuild(psDroid);
+		if ((map_coord(psDroid->orderX) == map_coord(psDroid->orderX2) && map_coord(psDroid->orderY) == map_coord(psDroid->orderY2))
+		    || psDroid->order != DORDER_LINEBUILD)
+		{
+			cancelBuild(psDroid);
+		}
+		else
+		{
+			psDroid->action = DACTION_NONE;	// make us continue line build
+			setDroidTarget(psDroid, NULL);
+			setDroidActionTarget(psDroid, NULL, 0);
+		}
 		return false;
 	}
 

src/order.c
@@ -1709,6 +1709,7 @@ void orderDroidBase(DROID *psDroid, DROID_ORDER_DATA *psOrder)
 		psDroid->psTarStats = psOrder->psStats;
 		ASSERT((!psDroid->psTarStats || ((STRUCTURE_STATS *)psDroid->psTarStats)->type != REF_DEMOLISH), "Cannot build demolition");
 		actionDroidLoc(psDroid, DACTION_BUILD, psOrder->x,psOrder->y);
+		objTrace(psDroid->id, "Starting new construction effort of %s", psOrder->psStats->pName);
 		break;
 	case DORDER_BUILDMODULE:
 		//build a module onto the structure
@@ -1724,6 +1725,7 @@ void orderDroidBase(DROID *psDroid, DROID_ORDER_DATA *psOrder)
 		ASSERT(psDroid->psTarStats != NULL, "orderUnitBase: should have found a module stats");
 		ASSERT((!psDroid->psTarStats || ((STRUCTURE_STATS *)psDroid->psTarStats)->type != REF_DEMOLISH), "Cannot build demolition");
 		actionDroidLoc(psDroid, DACTION_BUILD, psOrder->psObj->pos.x,psOrder->psObj->pos.y);
+		objTrace(psDroid->id, "Starting new upgrade of %s", psOrder->psStats->pName);
 		break;
 	case DORDER_LINEBUILD:
 		// build a line of structures
@@ -1742,6 +1744,7 @@ void orderDroidBase(DROID *psDroid, DROID_ORDER_DATA *psOrder)
 		psDroid->psTarStats = psOrder->psStats;
 		ASSERT((!psDroid->psTarStats || ((STRUCTURE_STATS *)psDroid->psTarStats)->type != REF_DEMOLISH), "Cannot build demolition");
 		actionDroidLoc(psDroid, DACTION_BUILD, psOrder->x,psOrder->y);
+		objTrace(psDroid->id, "Starting new line construction of %s", psOrder->psStats->pName);
 		break;
 	case DORDER_HELPBUILD:
 		// help to build a structure that is starting to be built
@@ -1756,6 +1759,7 @@ void orderDroidBase(DROID *psDroid, DROID_ORDER_DATA *psOrder)
 		psDroid->psTarStats = (BASE_STATS *)((STRUCTURE *)psOrder->psObj)->pStructureType;
 		ASSERT((!psDroid->psTarStats || ((STRUCTURE_STATS *)psDroid->psTarStats)->type != REF_DEMOLISH), "Cannot build demolition");
 		actionDroidLoc(psDroid, DACTION_BUILD, psDroid->orderX, psDroid->orderY);
+		objTrace(psDroid->id, "Helping construction of %s", psOrder->psStats->pName);
 		break;
 	case DORDER_DEMOLISH:
 		if (!(psDroid->droidType == DROID_CONSTRUCT || psDroid->droidType == DROID_CYBORG_CONSTRUCT))

src/scriptai.c
@@ -240,14 +240,11 @@ BOOL scrIdleGroup(void)
 	{
 		return false;
 	}
-
-	ASSERT( psGroup != NULL,
-		"scrIdleGroup: invalid group pointer" );
+	ASSERT_OR_RETURN(false, psGroup, "Invalid group pointer");
 
 	for(psDroid = psGroup->psList;psDroid; psDroid = psDroid->psGrpNext)
 	{
-		if(  psDroid->order == DORDER_NONE
-		  || (psDroid->order == DORDER_GUARD && psDroid->psTarget == NULL))
+		if (psDroid->order == DORDER_NONE || (psDroid->order == DORDER_GUARD && psDroid->psTarget == NULL))
 		{
 			count++;
 		}

src/structure.c
@@ -1206,9 +1206,12 @@ void structureBuild(STRUCTURE *psStruct, DROID *psDroid, int buildPoints)
 			for (psIter = apsDroidLists[psDroid->player]; psIter; psIter = psIter->psNext)
 			{
 				if ((psIter->order == DORDER_BUILD || psIter->order == DORDER_HELPBUILD || psIter->order == DORDER_LINEBUILD)
-				    && psIter->psTarget == (BASE_OBJECT *)psStruct)
+				    && psIter->psTarget == (BASE_OBJECT *)psStruct
+				    && (psIter->order != DORDER_LINEBUILD || (map_coord(psIter->orderX) == map_coord(psIter->orderX2)
+				                                              && map_coord(psIter->orderY) == map_coord(psIter->orderY2))))
 				{
-					objTrace(psIter->id, "Construction complete");
+					objTrace(psIter->id, "Construction order %s complete (%d, %d -> %d, %d)", getDroidOrderName(psDroid->order),
+					         (int)psIter->orderX, (int)psIter->orderY, (int)psIter->orderX2, (int)psIter->orderY2);
 					psIter->action = DACTION_NONE;
 					psIter->order = DORDER_NONE;
 					setDroidTarget(psIter, NULL);