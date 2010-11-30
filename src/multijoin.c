Warzone2100/warzone2100

commit 2ad6ff08556a86b99ca2ea8692176591b11f4fa3

Fix desynch when player leaves, due to clearing buildings at different gameTimes.

src/multijoin.c
@@ -198,6 +198,26 @@ static void resetMultiVisibility(UDWORD player)
 	return;
 }
 
+static void sendPlayerLeft(uint32_t playerIndex)
+{
+	NETbeginEncode(NETgameQueue(selectedPlayer), GAME_PLAYER_LEFT);
+		NETuint32_t(&playerIndex);
+	NETend();
+}
+
+void recvPlayerLeft(NETQUEUE queue)
+{
+	uint32_t playerIndex = 0;
+
+	NETbeginDecode(queue, GAME_PLAYER_LEFT);
+		NETuint32_t(&playerIndex);
+	NETend();
+
+	turnOffMultiMsg(true);
+	clearPlayer(playerIndex, false);  // don't do it quietly
+	turnOffMultiMsg(false);
+}
+
 // ////////////////////////////////////////////////////////////////////////////
 // A remote player has left the game
 BOOL MultiPlayerLeave(UDWORD playerIndex)
@@ -215,11 +235,11 @@ BOOL MultiPlayerLeave(UDWORD playerIndex)
 
 	ssprintf(buf, _("%s has Left the Game"), getPlayerName(playerIndex));
 
-	turnOffMultiMsg(true);
-	clearPlayer(playerIndex, false);		// don't do it quietly
+	if (NetPlay.isHost)
+	{
+		sendPlayerLeft(playerIndex);
+	}
 	game.skDiff[playerIndex] = DIFF_SLIDER_STOPS / 2;
-
-	turnOffMultiMsg(false);
 
 	addConsoleMessage(buf, DEFAULT_JUSTIFY, SYSTEM_MESSAGE);
 

src/multiplay.c
@@ -844,6 +844,8 @@ BOOL recvMessage(void)
 		case NET_PLAYER_STATS:
 			recvMultiStats(queue);
 			break;
+		case GAME_PLAYER_LEFT:
+			recvPlayerLeft(queue);
 		default:
 			processedMessage2 = false;
 			break;

lib/netplay/netplay.c
@@ -3202,6 +3202,7 @@ const char *messageTypeToString(unsigned messageType_)
 		case GAME_STRUCTUREINFO:            return "GAME_STRUCTUREINFO";
 		case GAME_LASSAT:                   return "GAME_LASSAT";
 		case GAME_GAME_TIME:                return "GAME_GAME_TIME";
+		case GAME_PLAYER_LEFT:              return "GAME_PLAYER_LEFT";
 		case GAME_DROIDDEST:                return "GAME_DROIDDEST";
 		case GAME_CHECK_DROID:              return "GAME_CHECK_DROID";
 		case GAME_CHECK_STRUCT:             return "GAME_CHECK_STRUCT";

lib/netplay/netplay.h
@@ -110,6 +110,7 @@ typedef enum
 	GAME_STRUCTUREINFO,             ///< Structure state.
 	GAME_LASSAT,                    ///< lassat firing.
 	GAME_GAME_TIME,                 ///< Game time. Used for synchronising, so that all messages are executed at the same gameTime on all clients.
+	GAME_PLAYER_LEFT,               ///< Player has left or dropped.
 	// The following messages (not including GAME_MAX_TYPE) are currently redundant, and should probably at some point not be
 	// sent, except (some of them) when using cheats in debug mode.
 	GAME_DROIDDEST,                 ///< issue a droid destruction, will be sent by all players at the same time, and have no effect, if synchronised.

src/multijoin.h
@@ -32,6 +32,7 @@ extern "C"
 #endif //__cplusplus
 
 extern BOOL intDisplayMultiJoiningStatus(UBYTE joinCount);
+void recvPlayerLeft(NETQUEUE queue);
 extern BOOL MultiPlayerLeave			(UDWORD playerIndex);						// A player has left the game.
 extern BOOL MultiPlayerJoin				(UDWORD playerIndex);						// A Player has joined the game.
 extern void setupNewPlayer				(UDWORD player);		// stuff to do when player joins.

Jeanosasco/WarFrontier

commit b23a7b6896e0416fa653bf1d0ed5cf336f21f748

Fix host crash when someone leaves pre-game lobby screen.
Broke in 2ad6ff08556a86b99ca2ea8692176591b11f4fa3.

src/multijoin.c
@@ -235,7 +235,11 @@ BOOL MultiPlayerLeave(UDWORD playerIndex)
 
 	ssprintf(buf, _("%s has Left the Game"), getPlayerName(playerIndex));
 
-	if (NetPlay.isHost)
+	if (ingame.localJoiningInProgress)
+	{
+		clearPlayer(playerIndex, false);
+	}
+	else if (NetPlay.isHost)  // If hosting, and game has started (not in pre-game lobby screen, that is).
 	{
 		sendPlayerLeft(playerIndex);
 	}

src/multiplay.c
@@ -846,6 +846,7 @@ BOOL recvMessage(void)
 			break;
 		case GAME_PLAYER_LEFT:
 			recvPlayerLeft(queue);
+			break;
 		default:
 			processedMessage2 = false;
 			break;