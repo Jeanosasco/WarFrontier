Warzone2100/warzone2100

commit af5f329fa2315e567e45a648603fbefd8d51277e

Check sender/receiver.

lib/netplay/netplay.cpp
@@ -1378,6 +1378,11 @@ static bool NETprocessSystemMessage(NETQUEUE playerQueue, uint8_t type)
 				debug(LOG_ERROR, "Incomplete NET_SEND_TO_PLAYER.");
 				break;
 			}
+			if (sender > MAX_PLAYERS || receiver > MAX_PLAYERS)
+			{
+				debug(LOG_ERROR, "Bad NET_SEND_TO_PLAYER.");
+				break;
+			}
 			if ((receiver == selectedPlayer || receiver == NET_ALL_PLAYERS) && playerQueue.index == NetPlay.hostPlayer)
 			{
 				// Message was sent to us via the host.

Jeanosasco/WarFrontier

commit 52ba43bafb047f434e481d29a4e8579677ca3347

Fix broken multiplayer.
Broken in af5f329fa2315e567e45a648603fbefd8d51277e. D'oh.

Fixes ticket:3281.

lib/netplay/netplay.cpp
@@ -1378,7 +1378,7 @@ static bool NETprocessSystemMessage(NETQUEUE playerQueue, uint8_t type)
 				debug(LOG_ERROR, "Incomplete NET_SEND_TO_PLAYER.");
 				break;
 			}
-			if (sender > MAX_PLAYERS || receiver > MAX_PLAYERS)
+			if (sender > MAX_PLAYERS || (receiver > MAX_PLAYERS && receiver != NET_ALL_PLAYERS))
 			{
 				debug(LOG_ERROR, "Bad NET_SEND_TO_PLAYER.");
 				break;