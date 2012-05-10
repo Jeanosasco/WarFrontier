Warzone2100/warzone2100

commit a6fa286dcf463d4c6d74e8a874e70f671f3d5018

Implement stored templates. Button graphics is a placeholder for now. Loosely based on original patch by EvilGuru. Closes ticket:1946.

src/template.cpp
@@ -26,6 +26,7 @@
 #include "template.h"
 
 #include "lib/framework/frame.h"
+#include "lib/framework/wzconfig.h"
 #include "lib/framework/math_ext.h"
 #include "lib/framework/strres.h"
 #include "lib/netplay/netplay.h"
@@ -56,6 +57,93 @@ static const StringToEnum<DROID_TYPE> map_DROID_TYPE[] =
 	{"ZNULLDROID",          DROID_ANY               },
 	{"DROID",               DROID_DEFAULT           },
 };
+
+bool researchedTemplate(DROID_TEMPLATE *psCurr, int player)
+{
+	if (apCompLists[player][COMP_BODY][psCurr->asParts[COMP_BODY]] != AVAILABLE
+	    || (psCurr->asParts[COMP_BRAIN] > 0 && apCompLists[player][COMP_BRAIN][psCurr->asParts[COMP_BRAIN]] != AVAILABLE)
+	    || apCompLists[player][COMP_PROPULSION][psCurr->asParts[COMP_PROPULSION]] != AVAILABLE
+	    || (psCurr->asParts[COMP_SENSOR] > 0 && apCompLists[player][COMP_SENSOR][psCurr->asParts[COMP_SENSOR]] != AVAILABLE)
+	    || (psCurr->asParts[COMP_ECM] > 0 && apCompLists[player][COMP_ECM][psCurr->asParts[COMP_ECM]] != AVAILABLE)
+	    || (psCurr->asParts[COMP_REPAIRUNIT] > 0 && apCompLists[player][COMP_REPAIRUNIT][psCurr->asParts[COMP_REPAIRUNIT]] != AVAILABLE)
+	    || (psCurr->asParts[COMP_CONSTRUCT] > 0 && apCompLists[player][COMP_CONSTRUCT][psCurr->asParts[COMP_CONSTRUCT]] != AVAILABLE)
+	    || (psCurr->numWeaps > 0 && apCompLists[player][COMP_WEAPON][psCurr->asWeaps[0]] != AVAILABLE)
+	    || (psCurr->numWeaps > 1 && apCompLists[player][COMP_WEAPON][psCurr->asWeaps[1]] != AVAILABLE))
+	{
+		return false;
+	}
+	return true;
+}
+
+bool initTemplates()
+{
+	WzConfig ini("templates.ini");
+	if (ini.status() != QSettings::NoError)
+	{
+		debug(LOG_ERROR, "Could not open templates.ini");
+		return false;
+	}
+	QStringList list = ini.childGroups();
+	for (int i = 0; i < list.size(); ++i)
+	{
+		ini.beginGroup(list[i]);
+		DROID_TEMPLATE design;
+		design.pName = NULL;
+		design.droidType = (DROID_TYPE)ini.value("droidType").toInt();
+		design.multiPlayerID = generateNewObjectId();
+		design.asParts[COMP_BODY] = getCompFromName(COMP_BODY, ini.value("body", QString("ZNULLBODY")).toString().toUtf8().constData());
+		design.asParts[COMP_BRAIN] = getCompFromName(COMP_BRAIN, ini.value("brain", QString("ZNULLBRAIN")).toString().toUtf8().constData());
+		design.asParts[COMP_PROPULSION] = getCompFromName(COMP_PROPULSION, ini.value("propulsion", QString("ZNULLPROP")).toString().toUtf8().constData());
+		design.asParts[COMP_REPAIRUNIT] = getCompFromName(COMP_REPAIRUNIT, ini.value("repair", QString("ZNULLREPAIR")).toString().toUtf8().constData());
+		design.asParts[COMP_ECM] = getCompFromName(COMP_ECM, ini.value("ecm", QString("ZNULLECM")).toString().toUtf8().constData());
+		design.asParts[COMP_SENSOR] = getCompFromName(COMP_SENSOR, ini.value("sensor", QString("ZNULLSENSOR")).toString().toUtf8().constData());
+		design.asParts[COMP_CONSTRUCT] = getCompFromName(COMP_CONSTRUCT, ini.value("construct", QString("ZNULLCONSTRUCT")).toString().toUtf8().constData());
+		design.asWeaps[0] = getCompFromName(COMP_WEAPON, ini.value("weapon/1", QString("ZNULLWEAPON")).toString().toUtf8().constData());
+		design.asWeaps[1] = getCompFromName(COMP_WEAPON, ini.value("weapon/2", QString("ZNULLWEAPON")).toString().toUtf8().constData());
+		design.asWeaps[2] = getCompFromName(COMP_WEAPON, ini.value("weapon/3", QString("ZNULLWEAPON")).toString().toUtf8().constData());
+		design.numWeaps = ini.value("weapons").toInt();
+		design.prefab = false;		// not AI template
+		design.stored = true;
+		bool valid = intValidTemplate(&design, ini.value("name").toString().toUtf8().constData());
+		ASSERT_OR_RETURN(false, valid, "Invalid template %d", i);
+		addTemplateToList(&design, &apsDroidTemplates[selectedPlayer]);
+		localTemplates.push_back(design);
+		ini.endGroup();
+	}
+	return true;
+}
+
+bool shutdownTemplates()
+{
+	// Write stored templates (back) to file
+	WzConfig ini("templates.ini");
+	if (ini.status() != QSettings::NoError)
+	{
+		debug(LOG_ERROR, "Could not open templates.ini");
+		return false;
+	}
+	for (DROID_TEMPLATE *psCurr = apsDroidTemplates[selectedPlayer]; psCurr != NULL; psCurr = psCurr->psNext)
+	{
+		if (!psCurr->stored) continue; // not stored
+		ini.beginGroup("template_" + QString::number(psCurr->multiPlayerID));
+		ini.setValue("name", psCurr->aName);
+		ini.setValue("droidType", psCurr->droidType);
+		ini.setValue("body", (asBodyStats + psCurr->asParts[COMP_BODY])->pName);
+		ini.setValue("propulsion", (asPropulsionStats + psCurr->asParts[COMP_PROPULSION])->pName);
+		ini.setValue("brain", (asBrainStats + psCurr->asParts[COMP_BRAIN])->pName);
+		ini.setValue("repair", (asRepairStats + psCurr->asParts[COMP_REPAIRUNIT])->pName);
+		ini.setValue("ecm", (asECMStats + psCurr->asParts[COMP_ECM])->pName);
+		ini.setValue("sensor", (asSensorStats + psCurr->asParts[COMP_SENSOR])->pName);
+		ini.setValue("construct", (asConstructStats + psCurr->asParts[COMP_CONSTRUCT])->pName);
+		ini.setValue("weapons", psCurr->numWeaps);
+		for (int j = 0; j < psCurr->numWeaps; j++)
+		{
+			ini.setValue("weapon/" + QString::number(j + 1), (asWeaponStats + psCurr->asWeaps[j])->pName);
+		}
+		ini.endGroup();
+	}
+	return true;
+}
 
 static void initTemplatePoints(DROID_TEMPLATE *pDroidDesign)
 {
@@ -98,6 +186,7 @@ DROID_TEMPLATE::DROID_TEMPLATE()  // This constructor replaces a memset in scrAs
 	, multiPlayerID(0)
 	, psNext(NULL)
 	, prefab(false)
+	, stored(false)
 {
 	aName[0] = '\0';
 	std::fill_n(asParts, DROID_MAXCOMP, 0);
@@ -117,6 +206,7 @@ DROID_TEMPLATE::DROID_TEMPLATE(LineView line)
 	, multiPlayerID(line.u32(1))
 	, psNext(NULL)
 	, prefab(false)
+	, stored(false)
 	// Ignored columns: 6 - but used later to decide whether the template is for human players.
 {
 	std::string name = line.s(0);
@@ -507,6 +597,7 @@ at any one time. Pass back the number available.
 */
 void fillTemplateList(std::vector<DROID_TEMPLATE *> &pList, STRUCTURE *psFactory)
 {
+	const int player = psFactory->player;
 	pList.clear();
 
 	DROID_TEMPLATE	*psCurr;
@@ -522,15 +613,14 @@ void fillTemplateList(std::vector<DROID_TEMPLATE *> &pList, STRUCTURE *psFactory
 			//can only have (MAX_CMDDROIDS) in the world at any one time
 			if (psCurr->droidType == DROID_COMMAND)
 			{
-				if (checkProductionForCommand(psFactory->player) +
-					checkCommandExist(psFactory->player) >= (MAX_CMDDROIDS))
+				if (checkProductionForCommand(player) + checkCommandExist(player) >= (MAX_CMDDROIDS))
 				{
 					continue;
 				}
 			}
 		}
 
-		if (!validTemplateForFactory(psCurr, psFactory))
+		if (!validTemplateForFactory(psCurr, psFactory) || !researchedTemplate(psCurr, player))
 		{
 			continue;
 		}
@@ -538,21 +628,6 @@ void fillTemplateList(std::vector<DROID_TEMPLATE *> &pList, STRUCTURE *psFactory
 		//check the factory can cope with this sized body
 		if (!((asBodyStats + psCurr->asParts[COMP_BODY])->size > iCapacity) )
 		{
-			//cyborg templates are available when the body has been research
-			//-same for Transporter in multiPlayer
-			if ( psCurr->droidType == DROID_CYBORG ||
-				 psCurr->droidType == DROID_CYBORG_SUPER ||
-				 psCurr->droidType == DROID_CYBORG_CONSTRUCT ||
-				 psCurr->droidType == DROID_CYBORG_REPAIR ||
-				 psCurr->droidType == DROID_TRANSPORTER)
-			{
-				if ( apCompLists[psFactory->player][COMP_BODY]
-					 [psCurr->asParts[COMP_BODY]] != AVAILABLE )
-				{
-					//ignore if not research yet
-					continue;
-				}
-			}
 			pList.push_back(psCurr);
 		}
 	}

src/design.cpp
@@ -362,6 +362,13 @@ static bool saveTemplate();
 
 static void desCreateDefaultTemplate( void );
 
+/**
+ * Updates the status of the stored template toggle button.
+ *
+ * @param isStored If the template is stored or not.
+ */
+static void updateStoreButton(bool isStored);
+
 /* The current name of the design */
 static char			aCurrName[WIDG_MAXSTR];
 
@@ -452,6 +459,7 @@ static bool _intAddDesign( bool bShowCentreScreen )
 	/* Initialise the current design */
 	sCurrDesign = sDefaultDesignTemplate;
 	sCurrDesign.pName = NULL;
+	sCurrDesign.stored = false;
 	sstrcpy(aCurrName, _("New Vehicle"));
 	sstrcpy(sCurrDesign.aName, aCurrName);
 
@@ -607,6 +615,24 @@ static bool _intAddDesign( bool bShowCentreScreen )
 	sButInit.pTip = _("Delete Design");
 	sButInit.pDisplay = intDisplayButtonHilight;
 	sButInit.UserData = PACKDWORD_TRI(0,IMAGE_DES_BINH, IMAGE_DES_BIN);
+	if (!widgAddButton(psWScreen, &sButInit))
+	{
+		return false;
+	}
+
+	// Add the store template button
+	sButInit.formID = IDDES_PARTFORM;
+	sButInit.id = IDDES_STOREBUTTON;
+	sButInit.style = WBUT_PLAIN;
+	sButInit.width = iV_GetImageWidth(IntImages, IMAGE_DES_BIN);
+	sButInit.height = iV_GetImageHeight(IntImages, IMAGE_DES_BIN);
+	sButInit.x = DES_PARTSEPARATIONX;
+	sButInit.y = DES_PARTFORMHEIGHT - 2*sButInit.height - 2*DES_PARTSEPARATIONY;
+	sButInit.pTip = _("Store Design");
+	sButInit.FontID = font_regular;
+	sButInit.pDisplay = intDisplayButtonHilight;
+	sButInit.UserData = PACKDWORD_TRI(0, IMAGE_DES_BINH, IMAGE_DES_BIN);
+
 	if (!widgAddButton(psWScreen, &sButInit))
 	{
 		return false;
@@ -866,7 +892,8 @@ void desSetupDesignTemplates(void)
 		    psTempl->droidType != DROID_CYBORG_SUPER       &&
 		    psTempl->droidType != DROID_CYBORG_CONSTRUCT   &&
 		    psTempl->droidType != DROID_CYBORG_REPAIR      &&
-		    psTempl->droidType != DROID_PERSON)
+		    psTempl->droidType != DROID_PERSON             &&
+		    researchedTemplate(psTempl, selectedPlayer))
 		{
 			apsTemplateList.push_back(psTempl);
 		}
@@ -3338,6 +3365,7 @@ static void desCreateDefaultTemplate( void )
 	/* set current design to default */
 	sCurrDesign = sDefaultDesignTemplate;
 	sCurrDesign.pName = NULL;
+	sCurrDesign.stored = false;
 
 	/* reset stats */
 	intSetDesignStats(&sCurrDesign);
@@ -3450,6 +3478,8 @@ void intProcessDesign(UDWORD id)
 			intSetButtonFlash( IDDES_PROPBUTTON,   true );
 			intSetButtonFlash( IDDES_WPABUTTON,   true );
 			intSetButtonFlash( IDDES_WPBBUTTON,   true );
+
+			widgHide(psWScreen, IDDES_STOREBUTTON);
 		}
 		else
 		{
@@ -3496,6 +3526,9 @@ void intProcessDesign(UDWORD id)
 				{
 					intSetButtonFlash( IDDES_WPBBUTTON,   true );
 				}
+
+				widgReveal(psWScreen, IDDES_STOREBUTTON);
+				updateStoreButton(sCurrDesign.stored);
 			}
 		}
 
@@ -4008,6 +4041,10 @@ void intProcessDesign(UDWORD id)
 			}
 			break;
 		}
+		case IDDES_STOREBUTTON:
+			sCurrDesign.stored = !sCurrDesign.stored;	// Invert the current status
+			saveTemplate();
+			break;
 		case IDDES_SYSTEMBUTTON:
 			// Add the correct component form
 			switch (droidTemplateType(&sCurrDesign))
@@ -4409,8 +4446,11 @@ static bool saveTemplate(void)
 {
 	if (!intValidTemplate(&sCurrDesign, aCurrName))
 	{
+		widgHide(psWScreen, IDDES_STOREBUTTON);
 		return false;
 	}
+	widgReveal(psWScreen, IDDES_STOREBUTTON);
+	updateStoreButton(sCurrDesign.stored);	// Change the buttons icon
 
 	/* if first (New Design) button selected find empty template
 	 * else find current button template
@@ -4436,6 +4476,7 @@ static bool saveTemplate(void)
 		psTempl = templateFromButtonId(droidTemplID);
 		if (psTempl == NULL)
 		{
+			debug(LOG_ERROR, "Template not found for button");
 			return false;
 		}
 
@@ -4636,3 +4677,18 @@ void reverseTemplateList(DROID_TEMPLATE **ppsList)
 	*ppsList = psPrev;
 }
 
+void updateStoreButton(bool isStored)
+{
+	UDWORD imageset;
+
+	if (isStored)
+	{
+		imageset = PACKDWORD_TRI(0, IMAGE_DES_TURRETH, IMAGE_DES_TURRET);
+	}
+	else
+	{
+		imageset = PACKDWORD_TRI(0, IMAGE_DES_BINH, IMAGE_DES_BIN);
+	}
+
+	widgSetUserData2(psWScreen, IDDES_STOREBUTTON, imageset);
+}

src/design.h
@@ -59,6 +59,8 @@
 #define IDDES_WEAPONS_A		5028		// The weapon TURRET_A button for the Component form (right)
 #define IDDES_WEAPONS_B		5029		// The weapon TURRET_B button for the Component form (right)
 
+#define IDDES_STOREBUTTON	5905		// Stored template button
+
 /* Design screen bar graph IDs */
 #define IDDES_BODYARMOUR_K	5100		// The body armour bar graph for kinetic weapons
 #define IDDES_BODYPOWER		5101		// The body power plant graph

src/droid.cpp
@@ -82,7 +82,6 @@
 #include "research.h"
 #include "combat.h"
 #include "scriptfuncs.h"			//for ThreatInRange()
-//#include "design.h"					//for GetDefaultTemplateName
 #include "template.h"
 
 #define DEFAULT_RECOIL_TIME	(GAME_TICKS_PER_SEC/4)

src/droiddef.h
@@ -125,6 +125,7 @@ struct DROID_TEMPLATE : public BASE_STATS
 	UDWORD          multiPlayerID;              ///< multiplayer unique descriptor(cant use id's for templates). Used for save games as well now - AB 29/10/98
 	DROID_TEMPLATE* psNext;                     ///< Pointer to next template
 	bool		prefab;                     ///< Not player designed, not saved, never delete or change
+	bool		stored;                     ///< Stored template
 };
 
 struct PACKAGED_CHECK;

src/init.cpp
@@ -95,6 +95,7 @@
 #include "terrain.h"
 #include "ingameop.h"
 #include "qtscript.h"
+#include "template.h"
 
 static void	initMiscVars(void);
 
@@ -1097,6 +1098,9 @@ bool stageThreeInitialise(void)
 	preProcessVisibility();
 	closeLoadingScreen();			// reset the loading screen.
 
+	// Load any stored templates; these need to be available ASAP
+	initTemplates();
+
 	if (!fpathInitialise())
 	{
 		return false;
@@ -1182,6 +1186,8 @@ bool stageThreeShutDown(void)
 	challengesUp = false;
 	challengeActive = false;
 	isInGamePopupUp = false;
+
+	shutdownTemplates();
 
 	// make sure any button tips are gone.
 	widgReset();

src/multiplay.cpp
@@ -1349,6 +1349,7 @@ static void NETtemplate(DROID_TEMPLATE *pTempl)
 	NETuint32_t(&pTempl->powerPoints);
 	NETuint32_t(&pTempl->storeCount);
 	NETuint32_t(&pTempl->numWeaps);
+	NETbool(&pTempl->stored);	// other players don't need to know, but we need to keep the knowledge in the loop somehow...
 
 	for (int i = 0; i < DROID_MAXWEAPS; ++i)
 	{
@@ -1394,12 +1395,12 @@ bool recvTemplate(NETQUEUE queue)
 	{
 		t.psNext = psTempl->psNext;
 		*psTempl = t;
-		debug(LOG_SYNC, "Updating MP template %d", (int)t.multiPlayerID);
+		debug(LOG_SYNC, "Updating MP template %d (stored=%s)", (int)t.multiPlayerID, t.stored ? "yes" : "no");
 	}
 	else
 	{
 		addTemplateBack(player, &t);  // Add to back of list, to avoid game state templates being in wrong order, which matters when saving games.
-		debug(LOG_SYNC, "Creating MP template %d", (int)t.multiPlayerID);
+		debug(LOG_SYNC, "Creating MP template %d (stored=%s)", (int)t.multiPlayerID, t.stored ? "yes" : "no");
 	}
 
 	return true;

src/template.h
@@ -7,6 +7,9 @@
 extern DROID_TEMPLATE			*apsDroidTemplates[MAX_PLAYERS];
 extern DROID_TEMPLATE			*apsStaticTemplates;			// for AIs and scripts
 
+bool initTemplates();
+bool shutdownTemplates();
+
 /** Initialise the template build and power points */
 void initTemplatePoints(void);
 
@@ -19,17 +22,20 @@ bool templateIsIDF(DROID_TEMPLATE *psTemplate);
 void fillTemplateList(std::vector<DROID_TEMPLATE *> &pList, STRUCTURE *psFactory);
 
 /* gets a template from its aName (when pName is unknown) */
-extern DROID_TEMPLATE	*GetHumanDroidTemplate(const char *aName);
-extern DROID_TEMPLATE	*GetAIDroidTemplate(const char *aName);
+DROID_TEMPLATE *GetHumanDroidTemplate(const char *aName);
+DROID_TEMPLATE *GetAIDroidTemplate(const char *aName);
 /* gets a template from its name - relies on the name being unique */
-extern DROID_TEMPLATE * getTemplateFromUniqueName(const char *pName, unsigned int player);
+DROID_TEMPLATE *getTemplateFromUniqueName(const char *pName, unsigned int player);
 /* gets a template from its name - relies on the name being unique */
-extern DROID_TEMPLATE* getTemplateFromTranslatedNameNoPlayer(char const *pName);
+DROID_TEMPLATE *getTemplateFromTranslatedNameNoPlayer(char const *pName);
 /*getTemplateFromMultiPlayerID gets template for unique ID  searching all lists */
-extern DROID_TEMPLATE* getTemplateFromMultiPlayerID(UDWORD multiPlayerID);
+DROID_TEMPLATE *getTemplateFromMultiPlayerID(UDWORD multiPlayerID);
 
 /*return the name to display for the interface - we don't know if this is
 a string ID or something the user types in*/
-extern const char* getTemplateName(const DROID_TEMPLATE *psTemplate);
+const char* getTemplateName(const DROID_TEMPLATE *psTemplate);
+
+/// Have we researched the components of this template?
+bool researchedTemplate(DROID_TEMPLATE *psCurr, int player);
 
 #endif // TEMPLATE_H

Jeanosasco/WarFrontier

commit 4c3f58a3a3e1c637008167211566fd9eb8b85b27

Fix tutorial that was broken in a6fa286dcf463d4c6d74e8a874e70f671f3d5018 and e862a993cb4ad1939e70dfc9a5b09e8b6519b5a7
Fixes ticket:3058

src/scriptfuncs.cpp
@@ -6339,11 +6339,9 @@ bool scrIsVtol(void)
 	return true;
 }
 
-
-
-
-// do the setting up of the template list for the tutorial.
-// This function looks like it searches for a template called "ViperLtMGWheels", and deletes it. Why?
+// Fix the tutorial's template list(s).
+// DO NOT MODIFY THIS WITHOUT KNOWING WHAT YOU ARE DOING.  You will break the tutorial!
+// In short, we want to design a ViperLtMGWheels, but it is already available to make, so we must delete it.
 bool scrTutorialTemplates(void)
 {
 	DROID_TEMPLATE	*psCurr, *psPrev;
@@ -6351,11 +6349,9 @@ bool scrTutorialTemplates(void)
 	// find ViperLtMGWheels
 	char const *pName = getDroidResourceName("ViperLtMGWheels");
 
-	for (psCurr = apsDroidTemplates[selectedPlayer],psPrev = NULL;
-			psCurr != NULL;
-			psCurr = psCurr->psNext)
+	for (psCurr = apsDroidTemplates[selectedPlayer], psPrev = NULL; psCurr != NULL;	psCurr = psCurr->psNext)
 	{
-		if (strcmp(pName,psCurr->aName)==0)
+		if (strcmp(pName, psCurr->aName)==0)
 		{
 			if (psPrev)
 			{
@@ -6365,15 +6361,24 @@ bool scrTutorialTemplates(void)
 			{
 				apsDroidTemplates[selectedPlayer] = psCurr->psNext;
 			}
-			//quit looking cos found
 			break;
 		}
 		psPrev = psCurr;
 	}
 
-	// Delete the template.
+	// Delete the template in *both* lists!
 	if(psCurr)
 	{
+		for (std::list<DROID_TEMPLATE>::iterator i = localTemplates.begin(); i != localTemplates.end(); ++i)
+		{
+			DROID_TEMPLATE *dropTemplate = &*i;
+			if (psCurr->multiPlayerID == dropTemplate->multiPlayerID)
+			{
+				free(dropTemplate->pName);
+				localTemplates.erase(i);
+				break;
+			}
+		}
 		delete psCurr;
 	}
 	else

src/template.cpp
@@ -41,7 +41,7 @@
 
 /* default droid design template */
 extern DROID_TEMPLATE	sDefaultDesignTemplate;
-
+extern bool		bInTutorial;
 // Template storage
 DROID_TEMPLATE		*apsDroidTemplates[MAX_PLAYERS];
 
@@ -77,7 +77,8 @@ bool researchedTemplate(DROID_TEMPLATE *psCurr, int player)
 		break; // now proceed to normal droids...
 	}
 	// Note the ugly special case for commanders - their weapon is unavailable
-	if (apCompLists[player][COMP_BODY][psCurr->asParts[COMP_BODY]] != AVAILABLE
+	// NOTE: besides being one ugly & hard to debug if statement, this breaks the tutorial level, so skip this whole thing during tutorial.
+	if (!bInTutorial && (apCompLists[player][COMP_BODY][psCurr->asParts[COMP_BODY]] != AVAILABLE
 	    || (psCurr->asParts[COMP_BRAIN] > 0 && apCompLists[player][COMP_BRAIN][psCurr->asParts[COMP_BRAIN]] != AVAILABLE)
 	    || apCompLists[player][COMP_PROPULSION][psCurr->asParts[COMP_PROPULSION]] != AVAILABLE
 	    || (psCurr->asParts[COMP_SENSOR] > 0 && apCompLists[player][COMP_SENSOR][psCurr->asParts[COMP_SENSOR]] != AVAILABLE)
@@ -85,7 +86,7 @@ bool researchedTemplate(DROID_TEMPLATE *psCurr, int player)
 	    || (psCurr->asParts[COMP_REPAIRUNIT] > 0 && apCompLists[player][COMP_REPAIRUNIT][psCurr->asParts[COMP_REPAIRUNIT]] != AVAILABLE)
 	    || (psCurr->asParts[COMP_CONSTRUCT] > 0 && apCompLists[player][COMP_CONSTRUCT][psCurr->asParts[COMP_CONSTRUCT]] != AVAILABLE)
 	    || (psCurr->asParts[COMP_BRAIN] == 0 && psCurr->numWeaps > 0 && apCompLists[player][COMP_WEAPON][psCurr->asWeaps[0]] != AVAILABLE)
-	    || (psCurr->numWeaps > 1 && apCompLists[player][COMP_WEAPON][psCurr->asWeaps[1]] != AVAILABLE))
+	    || (psCurr->numWeaps > 1 && apCompLists[player][COMP_WEAPON][psCurr->asWeaps[1]] != AVAILABLE)))
 	{
 		return false;
 	}