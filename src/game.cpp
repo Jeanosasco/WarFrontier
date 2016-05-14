Warzone2100/warzone2100

commit dddf05cbe4bb31b87b8fb69c4b4ad4e634edc151

teams: Fix unable to start game when all players are on the same team, but teams mode is alliances off.
Fixes ticket:4343.

src/game.cpp
@@ -1902,7 +1902,7 @@ bool loadGame(const char *pGameToLoad, bool keepObjects, bool freeMem, bool User
 		{
 			for (i = 0; i < MAX_PLAYERS; i++)
 			{
-				alliancebits[i] |= 0;
+				alliancebits[i] = 0;
 				for (j = 0; j < MAX_PLAYERS; j++)
 				{
 					alliances[i][j] = saveGameData.alliances[i][j];

src/multiint.cpp
@@ -2311,16 +2311,23 @@ bool recvPositionRequest(NETQUEUE queue)
 	return changePosition(player, position);
 }
 
-// If so, return that team; if not, return -1; if there are no players, return team MAX_PLAYERS.
-int allPlayersOnSameTeam(int except)
+static int getPlayerTeam(int i)
 {
-	int minTeam = MAX_PLAYERS, maxTeam = 0;
+	return game.alliance != ALLIANCES_TEAMS ? i : NetPlay.players[i].team;
+}
+
+// If so, return that team; if not, return -1; if there are no players, return team MAX_PLAYERS.
+static int allPlayersOnSameTeam(int except)
+{
+	int minTeam = MAX_PLAYERS, maxTeam = 0, numPlayers = 0;
 	for (int i = 0; i < game.maxPlayers; ++i)
 	{
 		if (i != except && (NetPlay.players[i].allocated || NetPlay.players[i].ai >= 0))
 		{
-			minTeam = std::min(minTeam, NetPlay.players[i].team);
-			maxTeam = std::max(maxTeam, NetPlay.players[i].team);
+			int team = getPlayerTeam(i);
+			minTeam = std::min(minTeam, team);
+			maxTeam = std::max(maxTeam, team);
+			++numPlayers;
 		}
 	}
 	if (minTeam == MAX_PLAYERS || minTeam == maxTeam)
@@ -2477,7 +2484,7 @@ void addPlayerBox(bool players)
 		{
 			if (game.skDiff[i] || isHumanPlayer(i))
 			{
-				int myTeam = game.alliance != ALLIANCES_TEAMS ? NetPlay.players[i].team : i;
+				int myTeam = getPlayerTeam(i);
 				if (team == -1)
 				{
 					team = myTeam;

Jeanosasco/WarFrontier

commit ade0b351a7229acd5ae302d7288b09a39cbcb64a

Fix visibility issues in loaded games.
Removed «alliancebits[i] = 0;», which broke stuff. Was previously «alliancebits[i] |= 0;» which was a no-op.

Broken in dddf05cbe4bb31b87b8fb69c4b4ad4e634edc151, fixed in 3.1 branch in c5117b0adb7bc1329df91ae170c19af2e2fffb4b.

Fixes ticket:4388. Probably fixes ticket:4368.

src/game.cpp
@@ -1890,7 +1890,6 @@ bool loadGame(const char *pGameToLoad, bool keepObjects, bool freeMem, bool User
 		{
 			for (i = 0; i < MAX_PLAYERS; i++)
 			{
-				alliancebits[i] = 0;
 				for (j = 0; j < MAX_PLAYERS; j++)
 				{
 					alliances[i][j] = saveGameData.alliances[i][j];