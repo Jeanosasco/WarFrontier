Warzone2100/warzone2100

commit bdc51e1590fd536cf0659585539195609006db41

Fix -fsanitize=undefined warning when loading games.
../../src/radar.cpp:179:51: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
../../src/radar.cpp:179:27: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
../../src/radar.cpp:605:20: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
../../src/radar.cpp:607:17: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
../../src/radar.cpp:613:18: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself

src/radar.cpp
@@ -168,7 +168,7 @@ bool resizeRadar()
 	memset(radarBuffer, 0, radarBufferSize);
 	if (rotateRadar)
 	{
-		RadarZoomMultiplier = (float)MAX(RADWIDTH, RADHEIGHT) / (float)MAX(radarTexWidth, radarTexHeight);
+		RadarZoomMultiplier = std::max(RADWIDTH, RADHEIGHT) / std::max({radarTexWidth, radarTexHeight, 1});
 	}
 	else
 	{
@@ -219,8 +219,8 @@ uint8_t GetRadarZoom()
 /** Calculate the radar pixel sizes. Returns pixels per tile. */
 static void CalcRadarPixelSize(float *SizeH, float *SizeV)
 {
-	*SizeH = (float)radarHeight / (float)radarTexHeight;
-	*SizeV = (float)radarWidth / (float)radarTexWidth;
+	*SizeH = radarHeight / std::max(radarTexHeight, 1);
+	*SizeV = radarWidth / std::max(radarTexWidth, 1);
 }
 
 /** Given a position within the radar, return a world coordinate. */

Jeanosasco/WarFrontier

commit c0e152e8220288ba52b8017ea2a25c333012aa70

Reinstate float casts to fix maps not loading.
Broken in bdc51e1590fd536cf0659585539195609006db4.

Fixes ticket:4404. Probably fixes ticket:4405.

src/radar.cpp
@@ -168,7 +168,7 @@ bool resizeRadar()
 	memset(radarBuffer, 0, radarBufferSize);
 	if (rotateRadar)
 	{
-		RadarZoomMultiplier = std::max(RADWIDTH, RADHEIGHT) / std::max({radarTexWidth, radarTexHeight, 1});
+		RadarZoomMultiplier = (float)std::max(RADWIDTH, RADHEIGHT) / std::max({radarTexWidth, radarTexHeight, 1});
 	}
 	else
 	{
@@ -219,8 +219,8 @@ uint8_t GetRadarZoom()
 /** Calculate the radar pixel sizes. Returns pixels per tile. */
 static void CalcRadarPixelSize(float *SizeH, float *SizeV)
 {
-	*SizeH = radarHeight / std::max(radarTexHeight, 1);
-	*SizeV = radarWidth / std::max(radarTexWidth, 1);
+	*SizeH = (float)radarHeight / std::max(radarTexHeight, 1);
+	*SizeV = (float)radarWidth / std::max(radarTexWidth, 1);
 }
 
 /** Given a position within the radar, return a world coordinate. */