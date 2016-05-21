Warzone2100/warzone2100

commit 6a714109276353b6d10dbf1c11d5f181c6511ce1

widget: Replace intOpenPlainForm, intDisplayPlainForm and intClosePlainForm with IntFormAnimated class.
Animation variables are no longer part of W_FORM.

src/ingameop.cpp
@@ -95,32 +95,21 @@ static bool addQuitOptions(void)
 		widgDelete(psWScreen, INTINGAMEPOPUP);		// get rid of the old stuff.
 	}
 
-	W_FORMINIT sFormInit;
-	// add form
-	sFormInit.formID	= 0;
-	sFormInit.id		= INTINGAMEOP;
-	sFormInit.style		= WFORM_PLAIN;
-	sFormInit.width		= INTINGAMEOP3_W;
-	sFormInit.height	= INTINGAMEOP3_H;;
-	sFormInit.x		= (SWORD)INTINGAMEOP3_X;
-	sFormInit.y		= (SWORD)INTINGAMEOP3_Y;
-	sFormInit.pDisplay	= intOpenPlainForm;
-	sFormInit.disableChildren= true;
+	WIDGET *parent = psWScreen->psForm;
 
-	widgAddForm(psWScreen, &sFormInit);
+	// add form
+	IntFormAnimated *inGameOp = new IntFormAnimated(parent);
+	inGameOp->id = INTINGAMEOP;
+	inGameOp->setGeometry(INTINGAMEOP3_W, INTINGAMEOP3_H, INTINGAMEOP3_X, INTINGAMEOP3_Y);
 
 	addIGTextButton(INTINGAMEOP_RESUME, INTINGAMEOP_1_X, INTINGAMEOP_1_Y, INTINGAMEOP_OP_W, _("Resume Game"), OPALIGN);
 	addIGTextButton(INTINGAMEOP_QUIT_CONFIRM, INTINGAMEOP_1_X, INTINGAMEOP_2_Y, INTINGAMEOP_OP_W, _("Quit"), OPALIGN);
 
 	if (NetPlay.isHost && bMultiPlayer && NetPlay.bComms)		// only show for real MP games
 	{
-		sFormInit.id		= INTINGAMEPOPUP;
-		sFormInit.width		= 600;
-		sFormInit.height	= 26;
-		sFormInit.x			= (SWORD)(20+D_W);	// center it
-		sFormInit.y			= (SWORD) 130;
-
-		widgAddForm(psWScreen, &sFormInit);
+		IntFormAnimated *inGamePopup = new IntFormAnimated(parent);
+		inGamePopup->id = INTINGAMEOP;
+		inGamePopup->setGeometry(600, 26, 20 + D_W, 130);
 
 		W_BUTINIT sButInit;
 
@@ -148,21 +137,12 @@ static bool addSlideOptions(void)
 		widgDelete(psWScreen, INTINGAMEOP);		// get rid of the old stuff.
 	}
 
-	W_FORMINIT sFormInit;
+	WIDGET *parent = psWScreen->psForm;
 
 	// add form
-	sFormInit.formID	= 0;
-	sFormInit.id		= INTINGAMEOP;
-	sFormInit.style		= WFORM_PLAIN;
-	sFormInit.x		= (SWORD)INTINGAMEOP2_X;
-	sFormInit.y		= (SWORD)INTINGAMEOP2_Y;
-	sFormInit.width		= INTINGAMEOP2_W;
-	sFormInit.height	= INTINGAMEOP2_H;
-
-	sFormInit.pDisplay	= intOpenPlainForm;
-	sFormInit.disableChildren= true;
-
-	widgAddForm(psWScreen, &sFormInit);
+	IntFormAnimated *ingameOp = new IntFormAnimated(parent);
+	ingameOp->id = INTINGAMEOP;
+	ingameOp->setGeometry(INTINGAMEOP2_X, INTINGAMEOP2_Y, INTINGAMEOP2_W, INTINGAMEOP2_H);
 
 	// fx vol
 	addIGTextButton(INTINGAMEOP_FXVOL, INTINGAMEOP_2_X, INTINGAMEOP_1_Y, INTINGAMEOP_OP_W, _("Voice Volume"), WBUT_PLAIN);
@@ -227,39 +207,17 @@ static bool _intAddInGameOptions(void)
 		kf_TogglePauseMode();
 	}
 
-	W_FORMINIT sFormInit;
-	sFormInit.width		= INTINGAMEOP_W;
+	WIDGET *parent = psWScreen->psForm;
+
+	bool s = (bMultiPlayer && NetPlay.bComms != 0) || bInTutorial;
 
 	// add form
-	sFormInit.formID	= 0;
-	sFormInit.id		= INTINGAMEOP;
-	sFormInit.style		= WFORM_PLAIN;
-	sFormInit.x			= (SWORD)INTINGAMEOP_X;
-	sFormInit.y			= (SWORD)INTINGAMEOP_Y;
-	sFormInit.height	= INTINGAMEOP_H;
-
-	if ((!bMultiPlayer || (NetPlay.bComms == 0)) && !bInTutorial)
-	{
-	}
-	else
-	{
-		sFormInit.height	= INTINGAMEOP_HS;
-	}
-
-	sFormInit.pDisplay	= intOpenPlainForm;
-	sFormInit.disableChildren= true;
-
-	widgAddForm(psWScreen, &sFormInit);
+	IntFormAnimated *ingameOp = new IntFormAnimated(parent);
+	ingameOp->id = INTINGAMEOP;
+	ingameOp->setGeometry(INTINGAMEOP_X, INTINGAMEOP_Y, INTINGAMEOP_W, s? INTINGAMEOP_HS : INTINGAMEOP_H);
 
 	// add 'quit' text
-	if ((!bMultiPlayer || (NetPlay.bComms == 0)) && !bInTutorial)
-	{
-		addIGTextButton(INTINGAMEOP_QUIT, INTINGAMEOP_1_X, INTINGAMEOP_5_Y, INTINGAMEOP_OP_W, _("Quit"), OPALIGN);
-	}
-	else
-	{
-		addIGTextButton(INTINGAMEOP_QUIT, INTINGAMEOP_1_X, INTINGAMEOP_3_Y, INTINGAMEOP_OP_W, _("Quit"), OPALIGN);
-	}
+	addIGTextButton(INTINGAMEOP_QUIT, INTINGAMEOP_1_X, s? INTINGAMEOP_3_Y : INTINGAMEOP_5_Y, INTINGAMEOP_OP_W, _("Quit"), OPALIGN);
 
 	// add 'resume'
 	addIGTextButton(INTINGAMEOP_RESUME, INTINGAMEOP_1_X, INTINGAMEOP_1_Y, INTINGAMEOP_OP_W, _("Resume Game"), OPALIGN);
@@ -267,7 +225,7 @@ static bool _intAddInGameOptions(void)
 	// add 'options'
 	addIGTextButton(INTINGAMEOP_OPTIONS, INTINGAMEOP_1_X, INTINGAMEOP_2_Y, INTINGAMEOP_OP_W, _("Audio Options"), OPALIGN);
 
-	if ((!bMultiPlayer || (NetPlay.bComms == 0)) && !bInTutorial)
+	if (!s)
 	{
 		if (!bMultiPlayer)
 		{
@@ -319,19 +277,11 @@ void intAddInGamePopup(void)
 		kf_TogglePauseMode();	// Pause the game.
 	}
 
-	W_FORMINIT sFormInit;
+	WIDGET *parent = psWScreen->psForm;
 
-	sFormInit.formID	= 0;
-	sFormInit.id		= INTINGAMEPOPUP;
-	sFormInit.style		= WFORM_PLAIN;
-	sFormInit.width		= 600;
-	sFormInit.height	= 160;
-	sFormInit.x			= (SWORD)(20+D_W);
-	sFormInit.y			= (SWORD)((240-(160/2))+D_H);
-	sFormInit.pDisplay	= intOpenPlainForm;
-	sFormInit.disableChildren= true;
-
-	widgAddForm(psWScreen, &sFormInit);
+	IntFormAnimated *ingamePopup = new IntFormAnimated(parent);
+	ingamePopup->id = INTINGAMEPOPUP;
+	ingamePopup->setGeometry(600, 160, 20 + D_W, (240 - 160/2) + D_H);
 
 	// add the text "buttons" now
 	W_BUTINIT sButInit;
@@ -407,9 +357,6 @@ void intCloseInGameOptionsNoAnim(bool bResetMissionWidgets)
 bool intCloseInGameOptions(bool bPutUpLoadSave, bool bResetMissionWidgets)
 {
 
-	W_TABFORM	*Form;
-	WIDGET		*widg;
-
 	if (NetPlay.isHost)
 	{
 		widgDelete(psWScreen, INTINGAMEPOPUP);
@@ -417,7 +364,7 @@ bool intCloseInGameOptions(bool bPutUpLoadSave, bool bResetMissionWidgets)
 
 	if(bPutUpLoadSave)
 	{
-		widg = widgGetFromID(psWScreen,INTINGAMEOP);
+		WIDGET *widg = widgGetFromID(psWScreen, INTINGAMEOP);
 		if(widg)
 		{
 			widgDelete(psWScreen,INTINGAMEOP);
@@ -430,21 +377,20 @@ bool intCloseInGameOptions(bool bPutUpLoadSave, bool bResetMissionWidgets)
 	{
 		// close the form.
 		// Start the window close animation.
+		IntFormAnimated *form;
 		if (isInGamePopupUp)	// FIXME: we hijack this routine for the popup close.
 		{
-			Form = (W_TABFORM*)widgGetFromID(psWScreen,INTINGAMEPOPUP);
+			form = (IntFormAnimated *)widgGetFromID(psWScreen, INTINGAMEPOPUP);
 			isInGamePopupUp = false;
 		}
 		else
 		{
-		Form = (W_TABFORM*)widgGetFromID(psWScreen,INTINGAMEOP);
+			form = (IntFormAnimated *)widgGetFromID(psWScreen, INTINGAMEOP);
 		}
 
-		if(Form)
+		if (form)
 		{
-			Form->displayFunction    = intClosePlainForm;
-			Form->pUserData		 = NULL; // Used to signal when the close anim has finished.
-			Form->disableChildren= true;
+			form->closeAnimate();
 			ClosingInGameOp		 = true;		// like orderup/closingorder
 			InGameOpUp			 = false;
 		}

lib/widget/form.cpp
@@ -57,9 +57,6 @@ W_FORMINIT::W_FORMINIT()
 W_FORM::W_FORM(W_FORMINIT const *init)
 	: WIDGET(init, WIDG_FORM)
 	, disableChildren(init->disableChildren)
-	, Ax0(0), Ay0(0), Ax1(0), Ay1(0)  // These assignments were previously done by a memset.
-	, animCount(0)
-	, startTime(0)                    // This assignment was previously done by a memset.
 {
 	ASSERT((init->style & ~(WFORM_TABBED | WFORM_INVISIBLE | WFORM_CLICKABLE | WFORM_NOCLICKMOVE | WFORM_NOPRIMARY | WFORM_SECONDARY | WIDG_HIDDEN)) == 0, "Unknown style bit");
 	ASSERT((init->style & WFORM_TABBED) == 0 || (init->style & (WFORM_INVISIBLE | WFORM_CLICKABLE)) == 0, "Tabbed form cannot be invisible or clickable");
@@ -70,9 +67,6 @@ W_FORM::W_FORM(W_FORMINIT const *init)
 W_FORM::W_FORM(WIDGET *parent)
 	: WIDGET(parent, WIDG_FORM)
 	, disableChildren(false)
-	, Ax0(0), Ay0(0), Ax1(0), Ay1(0)
-	, animCount(0)
-	, startTime(0)
 {}
 
 W_CLICKFORM::W_CLICKFORM(W_FORMINIT const *init)

lib/widget/form.h
@@ -38,9 +38,6 @@ struct W_FORM : public WIDGET
 	void display(int xOffset, int yOffset);
 
 	bool            disableChildren;        ///< Disable all child widgets if true
-	UWORD           Ax0, Ay0, Ax1, Ay1;     ///< Working coords for animations.
-	UDWORD          animCount;              ///< Animation counter.
-	UDWORD          startTime;              ///< Animation start time
 };
 
 /* The tabbed form data structure */

lib/widget/widgbase.h
@@ -118,6 +118,7 @@ struct WIDGET
 	int height() const { return dim.height(); }
 	void move(int x, int y) { dim.moveTo(x, y); }
 	void setGeometry(int x, int y, int w, int h) { dim.setRect(x, y, w, h); }
+	void setGeometry(QRect const &r) { dim = r; }
 
 	void attach(WIDGET *widget);
 	void detach(WIDGET *widget);

src/challenge.cpp
@@ -169,29 +169,22 @@ bool addChallenges()
 
 	psRequestScreen = new W_SCREEN; // init the screen
 
+	WIDGET *parent = psRequestScreen->psForm;
+
 	/* add a form to place the tabbed form on */
-	W_FORMINIT sFormInit;
-	sFormInit.formID = 0;				//this adds the blue background, and the "box" behind the buttons -Q
-	sFormInit.id = CHALLENGE_FORM;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = (SWORD) CHALLENGE_X;
-	sFormInit.y = (SWORD) CHALLENGE_Y;
-	sFormInit.width = CHALLENGE_W;
-	// we need the form to be long enough for all resolutions, so we take the total number of items * height
-	// and * the gaps, add the banner, and finally, the fudge factor ;)
-	sFormInit.height = (slotsInColumn * CHALLENGE_ENTRY_H + CHALLENGE_HGAP * slotsInColumn) + CHALLENGE_BANNER_DEPTH + 20;
-	sFormInit.disableChildren = true;
-	sFormInit.pDisplay = intOpenPlainForm;
-	widgAddForm(psRequestScreen, &sFormInit);
+	IntFormAnimated *challengeForm = new IntFormAnimated(parent);
+	challengeForm->id = CHALLENGE_FORM;
+	challengeForm->setGeometry(CHALLENGE_X, CHALLENGE_Y, CHALLENGE_W, (slotsInColumn * CHALLENGE_ENTRY_H + CHALLENGE_HGAP * slotsInColumn) + CHALLENGE_BANNER_DEPTH + 20);
 
 	// Add Banner
+	W_FORMINIT sFormInit;
 	sFormInit.formID = CHALLENGE_FORM;
 	sFormInit.id = CHALLENGE_BANNER;
+	sFormInit.style = WFORM_PLAIN;
 	sFormInit.x = CHALLENGE_HGAP;
 	sFormInit.y = CHALLENGE_VGAP;
 	sFormInit.width = CHALLENGE_W - (2 * CHALLENGE_HGAP);
 	sFormInit.height = CHALLENGE_BANNER_DEPTH;
-	sFormInit.disableChildren = false;
 	sFormInit.pDisplay = displayLoadBanner;
 	sFormInit.UserData = 0;
 	widgAddForm(psRequestScreen, &sFormInit);

src/design.cpp
@@ -396,19 +396,12 @@ static bool _intAddDesign(bool bShowCentreScreen)
 		bRender3DOnly = false;
 	}
 
+	WIDGET *parent = psWScreen->psForm;
+
 	/* Add the main design form */
-	sFormInit.formID = 0;
-	sFormInit.id = IDDES_FORM;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = (SWORD)DES_CENTERFORMX;	//0;
-	sFormInit.y = (SWORD)DES_CENTERFORMY;	//0;
-	sFormInit.width = DES_CENTERFORMWIDTH;	//DISP_WIDTH-1;
-	sFormInit.height = DES_CENTERFORMHEIGHT;	//DES_BASEHEIGHT;
-	sFormInit.pDisplay = intDisplayPlainForm;
-	if (!widgAddForm(psWScreen, &sFormInit))
-	{
-		return false;
-	}
+	IntFormAnimated *desForm = new IntFormAnimated(parent, false);
+	desForm->id = IDDES_FORM;
+	desForm->setGeometry(DES_CENTERFORMX, DES_CENTERFORMY, DES_CENTERFORMWIDTH, DES_CENTERFORMHEIGHT);
 
 	/* add the edit name box */
 	sEdInit.formID = IDDES_FORM;
@@ -589,18 +582,9 @@ static bool _intAddDesign(bool bShowCentreScreen)
 	}
 
 	/* add central stats form */
-	sFormInit.formID = 0;
-	sFormInit.id = IDDES_STATSFORM;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = (SWORD)DES_STATSFORMX;
-	sFormInit.y = (SWORD)DES_STATSFORMY;
-	sFormInit.width = DES_STATSFORMWIDTH;
-	sFormInit.height = DES_STATSFORMHEIGHT;
-	sFormInit.pDisplay = intDisplayPlainForm;
-	if (!widgAddForm(psWScreen, &sFormInit))
-	{
-		return false;
-	}
+	IntFormAnimated *statsForm = new IntFormAnimated(parent, false);
+	statsForm->id = IDDES_STATSFORM;
+	statsForm->setGeometry(DES_STATSFORMX, DES_STATSFORMY, DES_STATSFORMWIDTH, DES_STATSFORMHEIGHT);
 
 	/* Add the body form */
 	sFormInit.formID = IDDES_STATSFORM;
@@ -788,11 +772,8 @@ static bool _intAddDesign(bool bShowCentreScreen)
 	intSetDesignMode(IDES_BODY);
 
 	/* hide design and component forms until required */
-	if (bShowCentreScreen == false)
-	{
-		widgHide(psWScreen, IDDES_FORM);
-	}
-	widgHide(psWScreen, IDDES_STATSFORM);
+	desForm->show(bShowCentreScreen);
+	statsForm->hide();
 	widgHide(psWScreen, IDDES_RIGHTBASE);
 
 	return true;
@@ -838,23 +819,15 @@ static bool _intAddTemplateForm(DROID_TEMPLATE *psSelected)
 	             ((DES_LEFTFORMHEIGHT - DES_TABTHICKNESS - DES_TABBUTGAP) /
 	              (DES_TABBUTHEIGHT + DES_TABBUTGAP));
 
+	WIDGET *parent = psWScreen->psForm;
+
 	/* add a form to place the tabbed form on */
-	W_FORMINIT sFormInit;
-	sFormInit.formID = 0;
-	sFormInit.id = IDDES_TEMPLBASE;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = (SWORD)RET_X;
-	sFormInit.y = (SWORD)DESIGN_Y;
-	sFormInit.width = RET_FORMWIDTH;
-	sFormInit.height = DES_LEFTFORMHEIGHT + 4;
-	sFormInit.pDisplay = intDisplayPlainForm;
-	if (!widgAddForm(psWScreen, &sFormInit))
-	{
-		return false;
-	}
+	IntFormAnimated *templbaseForm = new IntFormAnimated(parent, false);
+	templbaseForm->id = IDDES_TEMPLBASE;
+	templbaseForm->setGeometry(RET_X, DESIGN_Y, RET_FORMWIDTH, DES_LEFTFORMHEIGHT + 4);
 
 	/* Add the design templates form */
-	sFormInit = W_FORMINIT();
+	W_FORMINIT sFormInit;
 	sFormInit.formID = IDDES_TEMPLBASE;	//IDDES_FORM;
 	sFormInit.id = IDDES_TEMPLFORM;
 	sFormInit.style = WFORM_TABBED;
@@ -1904,20 +1877,12 @@ static bool intAddComponentForm(UDWORD numButtons)
 {
 	unsigned butPerForm = DES_BUTSPERFORM;
 
+	WIDGET *parent = psWScreen->psForm;
+
 	/* add a form to place the tabbed form on */
-	W_FORMINIT sFormInit;
-	sFormInit.formID = 0;
-	sFormInit.id = IDDES_RIGHTBASE;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = (SWORD)(RADTLX - 2);
-	sFormInit.y = (SWORD)DESIGN_Y;
-	sFormInit.width = RET_FORMWIDTH;
-	sFormInit.height = DES_RIGHTFORMHEIGHT + 4;
-	sFormInit.pDisplay = intDisplayPlainForm;
-	if (!widgAddForm(psWScreen, &sFormInit))
-	{
-		return false;
-	}
+	IntFormAnimated *rightBase = new IntFormAnimated(parent, false);
+	rightBase->id = IDDES_RIGHTBASE;
+	rightBase->setGeometry(RADTLX - 2, DESIGN_Y, RET_FORMWIDTH, DES_RIGHTFORMHEIGHT + 4);
 
 	/* Calculate how many buttons will go on a form */
 	//================== adds L/R Scroll buttons ===================================
@@ -1957,7 +1922,7 @@ static bool intAddComponentForm(UDWORD numButtons)
 	}
 
 	//now a single form
-	sFormInit = W_FORMINIT();
+	W_FORMINIT sFormInit;
 	sFormInit.formID = IDDES_RIGHTBASE;
 	sFormInit.id = IDDES_COMPFORM;
 	sFormInit.style = WFORM_TABBED;

src/frontend.cpp
@@ -1724,36 +1724,26 @@ void addBackdrop(void)
 // ////////////////////////////////////////////////////////////////////////////
 void addTopForm(void)
 {
-	W_FORMINIT sFormInit;
+	WIDGET *parent = widgGetFromID(psWScreen, FRONTEND_BACKDROP);
 
-	sFormInit.formID = FRONTEND_BACKDROP;
-	sFormInit.id = FRONTEND_TOPFORM;
-	sFormInit.style = WFORM_PLAIN;
-
-	if(titleMode == MULTIOPTION)
+	IntFormAnimated *topForm = new IntFormAnimated(parent, false);
+	topForm->id = FRONTEND_TOPFORM;
+	if (titleMode == MULTIOPTION)
 	{
-		sFormInit.x		= FRONTEND_TOPFORM_WIDEX;
-		sFormInit.y		= FRONTEND_TOPFORM_WIDEY;
-		sFormInit.width = FRONTEND_TOPFORM_WIDEW;
-		sFormInit.height= FRONTEND_TOPFORM_WIDEH;
+		topForm->setGeometry(FRONTEND_TOPFORM_WIDEX, FRONTEND_TOPFORM_WIDEY, FRONTEND_TOPFORM_WIDEW, FRONTEND_TOPFORM_WIDEH);
 	}
 	else
-
 	{
-		sFormInit.x		= FRONTEND_TOPFORMX;
-		sFormInit.y		= FRONTEND_TOPFORMY;
-		sFormInit.width = FRONTEND_TOPFORMW;
-		sFormInit.height= FRONTEND_TOPFORMH;
+		topForm->setGeometry(FRONTEND_TOPFORMX, FRONTEND_TOPFORMY, FRONTEND_TOPFORMW, FRONTEND_TOPFORMH);
 	}
-	sFormInit.pDisplay = intDisplayPlainForm;
-	widgAddForm(psWScreen, &sFormInit);
 
+	W_FORMINIT sFormInit;
 	sFormInit.formID= FRONTEND_TOPFORM;
 	sFormInit.id	= FRONTEND_LOGO;
 	int imgW = iV_GetImageWidth(FrontImages, IMAGE_FE_LOGO);
 	int imgH = iV_GetImageHeight(FrontImages, IMAGE_FE_LOGO);
-	int dstW = sFormInit.width;
-	int dstH = sFormInit.height;
+	int dstW = topForm->width();
+	int dstH = topForm->height();
 	if (imgW*dstH < imgH*dstW)  // Want to set aspect ratio dstW/dstH = imgW/imgH.
 	{
 		dstW = imgW * dstH/imgH;  // Too wide.
@@ -1762,8 +1752,8 @@ void addTopForm(void)
 	{
 		dstH = imgH * dstW/imgW;  // Too high.
 	}
-	sFormInit.x = (sFormInit.width  - dstW)/2;
-	sFormInit.y = (sFormInit.height - dstH)/2;
+	sFormInit.x = (topForm->width()  - dstW)/2;
+	sFormInit.y = (topForm->height() - dstH)/2;
 	sFormInit.width  = dstW;
 	sFormInit.height = dstH;
 	sFormInit.pDisplay= displayLogo;
@@ -1773,20 +1763,11 @@ void addTopForm(void)
 // ////////////////////////////////////////////////////////////////////////////
 void addBottomForm(void)
 {
-	W_FORMINIT sFormInit;
+	WIDGET *parent = widgGetFromID(psWScreen, FRONTEND_BACKDROP);
 
-	sFormInit.formID = FRONTEND_BACKDROP;
-	sFormInit.id = FRONTEND_BOTFORM;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = FRONTEND_BOTFORMX;
-	sFormInit.y = FRONTEND_BOTFORMY;
-	sFormInit.width = FRONTEND_BOTFORMW;
-	sFormInit.height = FRONTEND_BOTFORMH;
-
-	sFormInit.pDisplay = intOpenPlainForm;
-	sFormInit.disableChildren = true;
-
-	widgAddForm(psWScreen, &sFormInit);
+	IntFormAnimated *botForm = new IntFormAnimated(parent);
+	botForm->id = FRONTEND_BOTFORM;
+	botForm->setGeometry(FRONTEND_BOTFORMX, FRONTEND_BOTFORMY, FRONTEND_BOTFORMW, FRONTEND_BOTFORMH);
 }
 
 // ////////////////////////////////////////////////////////////////////////////

src/hci.cpp
@@ -2809,30 +2809,22 @@ bool intAddReticule()
 	{
 		return true; // all fine 
 	}
+
+	WIDGET *parent = psWScreen->psForm;
+
 	/* Create the basic form */
-	W_FORMINIT sFormInit;
-	sFormInit.formID = 0;
-	sFormInit.id = IDRET_FORM;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = RET_X;
-	sFormInit.y = RET_Y;
-	sFormInit.width = RET_FORMWIDTH;
-	sFormInit.height = RET_FORMHEIGHT;
-	sFormInit.pDisplay = intDisplayPlainForm;
-	if (!widgAddForm(psWScreen, &sFormInit))
-	{
-		return false;
-	}
-	else // normal reticule
-	{
-		setReticuleBut(RETBUT_COMMAND, _("Commanders (F6)"), IMAGE_COMMANDDROID_UP, WBUT_PLAIN);
-		setReticuleBut(RETBUT_INTELMAP, _("Intelligence Display (F5)"), IMAGE_INTELMAP_UP, WBUT_SECONDARY);
-		setReticuleBut(RETBUT_FACTORY, _("Manufacture (F1)"), IMAGE_MANUFACTURE_UP, WBUT_SECONDARY);
-		setReticuleBut(RETBUT_DESIGN, _("Design (F4)"), IMAGE_DESIGN_UP, WBUT_PLAIN);
-		setReticuleBut(RETBUT_RESEARCH, _("Research (F2)"), IMAGE_RESEARCH_UP, WBUT_PLAIN);
-		setReticuleBut(RETBUT_BUILD, _("Build (F3)"), IMAGE_BUILD_UP, WBUT_PLAIN);
-		setReticuleBut(RETBUT_CANCEL, _("Close"), IMAGE_CANCEL_UP, WBUT_PLAIN);
-	}
+	IntFormAnimated *retForm = new IntFormAnimated(parent, false);
+	retForm->id = IDRET_FORM;
+	retForm->setGeometry(RET_X, RET_Y, RET_FORMWIDTH, RET_FORMHEIGHT);
+
+	// normal reticule
+	setReticuleBut(RETBUT_COMMAND, _("Commanders (F6)"), IMAGE_COMMANDDROID_UP, WBUT_PLAIN);
+	setReticuleBut(RETBUT_INTELMAP, _("Intelligence Display (F5)"), IMAGE_INTELMAP_UP, WBUT_SECONDARY);
+	setReticuleBut(RETBUT_FACTORY, _("Manufacture (F1)"), IMAGE_MANUFACTURE_UP, WBUT_SECONDARY);
+	setReticuleBut(RETBUT_DESIGN, _("Design (F4)"), IMAGE_DESIGN_UP, WBUT_PLAIN);
+	setReticuleBut(RETBUT_RESEARCH, _("Research (F2)"), IMAGE_RESEARCH_UP, WBUT_PLAIN);
+	setReticuleBut(RETBUT_BUILD, _("Build (F3)"), IMAGE_BUILD_UP, WBUT_PLAIN);
+	setReticuleBut(RETBUT_CANCEL, _("Close"), IMAGE_CANCEL_UP, WBUT_PLAIN);
 	ReticuleUp = true;
 	return true;
 }
@@ -3192,21 +3184,12 @@ static bool intAddObjectWindow(BASE_OBJECT *psObjects, BASE_OBJECT *psSelected, 
 	/* Reset the current object and store the current list */
 	psObjSelected = NULL;
 
-	/* Create the basic form */
+	WIDGET *parent = psWScreen->psForm;
 
-	W_FORMINIT sFormInit;
-	sFormInit.formID = 0;
-	sFormInit.id = IDOBJ_FORM;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = (SWORD)OBJ_BACKX;
-	sFormInit.y = (SWORD)OBJ_BACKY;
-	sFormInit.width = OBJ_BACKWIDTH;
-	sFormInit.height = 	OBJ_BACKHEIGHT;
-	sFormInit.pDisplay = intDisplayPlainForm;
-	if (!widgAddForm(psWScreen, &sFormInit))
-	{
-		return false;
-	}
+	/* Create the basic form */
+	IntFormAnimated *objForm = new IntFormAnimated(parent, false);
+	objForm->id = IDOBJ_FORM;
+	objForm->setGeometry(OBJ_BACKX, OBJ_BACKY, OBJ_BACKWIDTH, OBJ_BACKHEIGHT);
 
 	/* Add the close button */
 	W_BUTINIT sButInit;
@@ -3225,7 +3208,7 @@ static bool intAddObjectWindow(BASE_OBJECT *psObjects, BASE_OBJECT *psSelected, 
 	}
 
 	/*add the tabbed form */
-	sFormInit = W_FORMINIT();
+	W_FORMINIT sFormInit;
 	sFormInit.formID = IDOBJ_FORM;
 	sFormInit.id = IDOBJ_TABFORM;
 	sFormInit.style = WFORM_TABBED;
@@ -3731,18 +3714,14 @@ static bool intUpdateObject(BASE_OBJECT *psObjects, BASE_OBJECT *psSelected, boo
 /* Remove the build widgets from the widget screen */
 void intRemoveObject(void)
 {
-	W_TABFORM *Form;
-
 	widgDelete(psWScreen, IDOBJ_TABFORM);
 	widgDelete(psWScreen, IDOBJ_CLOSE);
 
 	// Start the window close animation.
-	Form = (W_TABFORM *)widgGetFromID(psWScreen, IDOBJ_FORM);
+	IntFormAnimated *Form = (IntFormAnimated *)widgGetFromID(psWScreen, IDOBJ_FORM);
 	if (Form)
 	{
-		Form->displayFunction = intClosePlainForm;
-		Form->disableChildren = true;
-		Form->pUserData = NULL; // Used to signal when the close anim has finished.
+		Form->closeAnimate();
 		ClosingObject = true;
 	}
 
@@ -3776,18 +3755,14 @@ static void intRemoveObjectNoAnim(void)
 /* Remove the stats widgets from the widget screen */
 void intRemoveStats(void)
 {
-	W_TABFORM *Form;
-
 	widgDelete(psWScreen, IDSTAT_CLOSE);
 	widgDelete(psWScreen, IDSTAT_TABFORM);
 
 	// Start the window close animation.
-	Form = (W_TABFORM *)widgGetFromID(psWScreen, IDSTAT_FORM);
+	IntFormAnimated *Form = (IntFormAnimated *)widgGetFromID(psWScreen, IDSTAT_FORM);
 	if (Form)
 	{
-		Form->displayFunction = intClosePlainForm;
-		Form->pUserData = NULL; // Used to signal when the close anim has finished.
-		Form->disableChildren = true;
+		Form->closeAnimate();
 		ClosingStats = true;
 	}
 
@@ -4162,22 +4137,12 @@ static bool intAddStats(BASE_STATS **ppsStatsList, UDWORD numStats,
 
 	ClearStatBuffers();
 
-	/* Create the basic form */
+	WIDGET *parent = psWScreen->psForm;
 
-	W_FORMINIT sFormInit;
-	sFormInit.formID = 0;
-	sFormInit.id = IDSTAT_FORM;
-	sFormInit.style = WFORM_PLAIN;
-	sFormInit.x = STAT_X;
-	sFormInit.y = (SWORD)STAT_Y;
-	sFormInit.width = STAT_WIDTH;
-	sFormInit.height = 	STAT_HEIGHT;
-	sFormInit.pDisplay = intDisplayPlainForm;
-	if (!widgAddForm(psWScreen, &sFormInit))
-	{
-		debug(LOG_ERROR, "intAddStats: Failed to add form");
-		return false;
-	}
+	/* Create the basic form */
+	IntFormAnimated *theStatForm = new IntFormAnimated(parent, false);  // Called theStatForm since there's already a statForm parameter to this function
+	theStatForm->id = IDSTAT_FORM;
+	theStatForm->setGeometry(STAT_X, STAT_Y, STAT_WIDTH, STAT_HEIGHT);
 
 	W_LABINIT sLabInit;
 
@@ -4314,7 +4279,7 @@ static bool intAddStats(BASE_STATS **ppsStatsList, UDWORD numStats,
 	}
 	//==============buttons before tabbed form!==========================
 	// Add the tabbed form
-	sFormInit = W_FORMINIT();
+	W_FORMINIT sFormInit;
 	sFormInit.formID = IDSTAT_FORM;
 	sFormInit.id = IDSTAT_TABFORM;
 	sFormInit.style = WFORM_TABBED;

Jeanosasco/WarFrontier

commit 3ce61ba9cf4bb5a25579261c67b6ad2720ea047c

widget: Fix broken warning on host quitting multiplayer games.
Probably broken in 6a714109276353b6d10dbf1c11d5f181c6511ce1.

Fixes ticket:4384.

src/ingameop.cpp
@@ -29,6 +29,7 @@
 #include "lib/framework/wzapp.h"
 #include "lib/framework/strres.h"
 #include "lib/widget/widget.h"
+#include "lib/widget/label.h"
 #include "lib/netplay/netplay.h"
 #include "lib/ivis_opengl/piestate.h"		// for getrendertype
 #include "lib/sound/audio.h"					// for sound.
@@ -84,20 +85,14 @@ static bool addIGTextButton(UDWORD id, UWORD x, UWORD y, UWORD width, const char
 
 static bool addQuitOptions(void)
 {
-	if (widgGetFromID(psWScreen, INTINGAMEOP))
-	{
-		widgDelete(psWScreen, INTINGAMEOP);		// get rid of the old stuff.
-	}
-
-	if (widgGetFromID(psWScreen, INTINGAMEPOPUP))
-	{
-		widgDelete(psWScreen, INTINGAMEPOPUP);		// get rid of the old stuff.
-	}
+	// get rid of the old stuff.
+	delete widgGetFromID(psWScreen, INTINGAMEOP);
+	delete widgGetFromID(psWScreen, INTINGAMEPOPUP);
 
 	WIDGET *parent = psWScreen->psForm;
 
 	// add form
-	IntFormAnimated *inGameOp = new IntFormAnimated(parent);
+	auto inGameOp = new IntFormAnimated(parent);
 	inGameOp->id = INTINGAMEOP;
 	inGameOp->setGeometry(INTINGAMEOP3_X, INTINGAMEOP3_Y, INTINGAMEOP3_W, INTINGAMEOP3_H);
 
@@ -106,23 +101,15 @@ static bool addQuitOptions(void)
 
 	if (NetPlay.isHost && bMultiPlayer && NetPlay.bComms)		// only show for real MP games
 	{
-		IntFormAnimated *inGamePopup = new IntFormAnimated(parent);
-		inGamePopup->id = INTINGAMEOP;
-		inGamePopup->setGeometry(600, 26, 20 + D_W, 130);
+		auto inGamePopup = new IntFormAnimated(parent);
+		inGamePopup->id = INTINGAMEPOPUP;
+		inGamePopup->setGeometry((pie_GetVideoBufferWidth() - 600)/2, inGameOp->y() - 26 - 20, 600, 26);
 
-		W_BUTINIT sButInit;
-
-		sButInit.formID		= INTINGAMEPOPUP;
-		sButInit.style		= OPALIGN;
-		sButInit.width		= 600;
-		sButInit.height		= 10;
-		sButInit.x			= 0;
-		sButInit.y			= 8;
-		sButInit.pDisplay	= displayTextOption;
-		sButInit.id			= INTINGAMEOP_POPUP_MSG3;
-		sButInit.pText		= _("WARNING: You're the host. If you quit, the game ends for everyone!");
-
-		widgAddButton(psWScreen, &sButInit);
+		auto label = new W_LABEL(inGamePopup);
+		label->setGeometry(0, 0, inGamePopup->width(), inGamePopup->height());
+		label->setString(_("WARNING: You're the host. If you quit, the game ends for everyone!"));
+		label->setTextAlignment(WLAB_ALIGNCENTRE);
+		label->setFont(font_medium, WZCOL_YELLOW);
 	}
 
 	return true;
@@ -131,10 +118,7 @@ static bool addQuitOptions(void)
 
 static bool addSlideOptions(void)
 {
-	if (widgGetFromID(psWScreen, INTINGAMEOP))
-	{
-		widgDelete(psWScreen, INTINGAMEOP);		// get rid of the old stuff.
-	}
+	delete widgGetFromID(psWScreen, INTINGAMEOP);  // get rid of the old stuff.
 
 	WIDGET *parent = psWScreen->psForm;
 
@@ -413,8 +397,6 @@ bool intCloseInGameOptions(bool bPutUpLoadSave, bool bResetMissionWidgets)
 // process clicks made by user.
 void intProcessInGameOptions(UDWORD id)
 {
-
-
 	switch (id)
 	{
 	// NORMAL KEYS
@@ -493,8 +475,4 @@ void intProcessInGameOptions(UDWORD id)
 	default:
 		break;
 	}
-
-
 }
-
-

src/ingameop.h
@@ -38,10 +38,6 @@ extern bool isInGamePopupUp;
 // ////////////////////////////////////////////////////////////////////////////
 // defines
 
-// the screen itself.
-#define INTINGAMEOP				10500
-#define INTINGAMEPOPUP			77335
-
 // position info for window.
 
 // game options
@@ -74,7 +70,9 @@ extern bool isInGamePopupUp;
 
 enum
 {
-	INTINGAMEOP_QUIT = INTINGAMEOP + 1,
+	INTINGAMEOP = 10500,
+	INTINGAMEPOPUP,
+	INTINGAMEOP_QUIT,
 	INTINGAMEOP_QUIT_CONFIRM,               ///< The all important quit button
 	INTINGAMEOP_RESUME,
 	INTINGAMEOP_LOAD_MISSION,
@@ -103,8 +101,7 @@ enum
 	INTINGAMEOP_TUI_TARGET_ORIGIN_SW,		///< Switch
 	INTINGAMEOP_POPUP_QUIT,
 	INTINGAMEOP_POPUP_MSG1,
-	INTINGAMEOP_POPUP_MSG2,
-	INTINGAMEOP_POPUP_MSG3
+	INTINGAMEOP_POPUP_MSG2
 };
 
 