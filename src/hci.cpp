Warzone2100/warzone2100

commit 2810c730e1733262b3ff27decfbb93c1db821a6a

Move reticule button enable/disable control into scripts.
New qtscript features:
- setReticuleFlash(id, value) - make reticule button flash
- levelType - read type of level (set in .lev file)

src/hci.cpp
@@ -98,17 +98,6 @@ static bool ChatDialogUp = false;
 #define RETYOFFSET (0)
 #define NUMRETBUTS	7 // Number of reticule buttons.
 
-enum  				  // Reticule button indecies.
-{
-	RETBUT_CANCEL,
-	RETBUT_FACTORY,
-	RETBUT_RESEARCH,
-	RETBUT_BUILD,
-	RETBUT_DESIGN,
-	RETBUT_INTELMAP,
-	RETBUT_COMMAND,
-};
-
 struct BUTSTATE
 {
 	UDWORD id;
@@ -366,8 +355,6 @@ static void processProximityButtons(UDWORD id);
 static DROID *intCheckForDroid(UDWORD droidType);
 static STRUCTURE *intCheckForStructure(UDWORD structType);
 
-static void intCheckReticuleButtons();
-
 // count the number of selected droids of a type
 static SDWORD intNumSelectedDroids(UDWORD droidType);
 
@@ -381,29 +368,43 @@ struct RETBUTSTATS
 	QString filenameDown;
 	QString tip;
 	QString func;
-	int flashing = 0;
+	bool flashing = false;
 	int flashTime = 0;
 	W_BUTTON *button = nullptr;
 	QScriptEngine *engine = nullptr;
 };
 static RETBUTSTATS retbutstats[NUMRETBUTS];
 
+void setReticuleFlash(int ButId, bool flash)
+{
+	if (flash != retbutstats[ButId].flashing)
+	{
+		retbutstats[ButId].flashing = flash;
+		retbutstats[ButId].flashTime = 0;
+	}
+}
+
 void setReticuleStats(int ButId, QString tip, QString filename, QString filenameDown, QString func, QScriptEngine *engine)
 {
-	retbutstats[ButId].tip = std::move(tip);
-	retbutstats[ButId].filename = std::move(filename);
-	retbutstats[ButId].filenameDown = std::move(filenameDown);
+	retbutstats[ButId].tip = tip;
+	retbutstats[ButId].filename = filename;
+	retbutstats[ButId].filenameDown = filenameDown;
 	retbutstats[ButId].downTime = 0;
-	retbutstats[ButId].flashing = 0;
+	retbutstats[ButId].flashing = false;
 	retbutstats[ButId].flashTime = 0;
-	retbutstats[ButId].func = std::move(func);
+	retbutstats[ButId].func = func;
 	retbutstats[ButId].engine = engine;
 
 	if (!retbutstats[ButId].button) // not quite set up yet
 	{
 		return;
 	}
-	retbutstats[ButId].button->setTip(retbutstats[ButId].tip);
+
+	if (!retbutstats[ButId].tip.isEmpty())
+	{
+		retbutstats[ButId].button->setTip(retbutstats[ButId].tip);
+	}
+
 	if (retbutstats[ButId].filename.isEmpty())
 	{
 		retbutstats[ButId].button->setState(WBUT_DISABLE);
@@ -416,13 +417,11 @@ void setReticuleStats(int ButId, QString tip, QString filename, QString filename
 
 static void intDisplayReticuleButton(WIDGET *psWidget, UDWORD xOffset, UDWORD yOffset)
 {
-	int     x = xOffset + psWidget->x();
-	int     y = yOffset + psWidget->y();
-	bool	Hilight = false;
-	bool	Down = false;
-	UBYTE	DownTime = retbutstats[psWidget->UserData].downTime;
-	UBYTE	flashing = retbutstats[psWidget->UserData].flashing;
-	UBYTE	flashTime = retbutstats[psWidget->UserData].flashTime;
+	const int x = xOffset + psWidget->x();
+	const int y = yOffset + psWidget->y();
+	int DownTime = retbutstats[psWidget->UserData].downTime;
+	bool flashing = retbutstats[psWidget->UserData].flashing;
+	int flashTime = retbutstats[psWidget->UserData].flashTime;
 	ASSERT_OR_RETURN(, psWidget->type == WIDG_BUTTON, "Not a button");
 	W_BUTTON *psButton = (W_BUTTON *)psWidget;
 
@@ -432,8 +431,8 @@ static void intDisplayReticuleButton(WIDGET *psWidget, UDWORD xOffset, UDWORD yO
 		return;
 	}
 
-	Down = psButton->state & (WBUT_DOWN | WBUT_CLICKLOCK);
-	Hilight = buttonIsHilite(psButton);
+	bool Down = psButton->state & (WBUT_DOWN | WBUT_CLICKLOCK);
+	bool Hilight = buttonIsHilite(psButton);
 
 	if (Down)
 	{
@@ -502,7 +501,7 @@ static void setReticuleBut(int ButId)
 	sButInit.style = WBUT_SECONDARY;
 	sButInit.UserData = ButId;
 	retbutstats[ButId].downTime = 0;
-	retbutstats[ButId].flashing = 0;
+	retbutstats[ButId].flashing = false;
 	retbutstats[ButId].flashTime = 0;
 	retbutstats[ButId].button = widgAddButton(psWScreen, &sButInit);
 	if (!retbutstats[ButId].button)
@@ -810,15 +809,12 @@ void intResetScreen(bool NoAnim)
 		NoAnim = true;
 	}
 
-	if (ReticuleUp)
+	for (auto& i : retbutstats)
 	{
-		/* Reset the reticule buttons */
-		widgSetButtonState(psWScreen, IDRET_COMMAND, 0);
-		widgSetButtonState(psWScreen, IDRET_BUILD, 0);
-		widgSetButtonState(psWScreen, IDRET_MANUFACTURE, 0);
-		widgSetButtonState(psWScreen, IDRET_INTEL_MAP, 0);
-		widgSetButtonState(psWScreen, IDRET_RESEARCH, 0);
-		widgSetButtonState(psWScreen, IDRET_DESIGN, 0);
+		if (i.button && ReticuleUp)
+		{
+			i.button->unlock();
+		}
 	}
 
 	/* Remove whatever extra screen was displayed */
@@ -2394,11 +2390,6 @@ static void intStopStructPosition()
 /* Display the widgets for the in game interface */
 void intDisplayWidgets()
 {
-	if (ReticuleUp && !bInTutorial)
-	{
-		intCheckReticuleButtons();
-	}
-
 	/*draw the background for the design screen and the Intelligence screen*/
 	if (intMode == INT_DESIGN || intMode == INT_INTELMAP)
 	{
@@ -4356,7 +4347,7 @@ void flashReticuleButton(UDWORD buttonID)
 	WIDGET *psButton = widgGetFromID(psWScreen, buttonID);
 	if (psButton)
 	{
-		retbutstats[psButton->UserData].flashing = 1;
+		retbutstats[psButton->UserData].flashing = true;
 	}
 }
 
@@ -4367,7 +4358,7 @@ void stopReticuleButtonFlash(UDWORD buttonID)
 	if (psButton)
 	{
 		retbutstats[psButton->UserData].flashTime = 0;
-		retbutstats[psButton->UserData].flashing = 0;
+		retbutstats[psButton->UserData].flashing = false;
 	}
 }
 
@@ -4553,94 +4544,6 @@ static SDWORD intNumSelectedDroids(UDWORD droidType)
 	}
 
 	return num;
-}
-
-// Check that each reticule button has the structure or droid required for it and
-// enable/disable accordingly.
-//
-void intCheckReticuleButtons()
-{
-	STRUCTURE	*psStruct;
-	DROID	*psDroid;
-
-	ReticuleEnabled[RETBUT_CANCEL].Enabled = true;
-	ReticuleEnabled[RETBUT_FACTORY].Enabled = false;
-	ReticuleEnabled[RETBUT_RESEARCH].Enabled = false;
-	ReticuleEnabled[RETBUT_BUILD].Enabled = false;
-	ReticuleEnabled[RETBUT_DESIGN].Enabled = allowDesign;
-	ReticuleEnabled[RETBUT_INTELMAP].Enabled = true;
-	ReticuleEnabled[RETBUT_COMMAND].Enabled = false;
-
-	for (psStruct = interfaceStructList(); psStruct != nullptr; psStruct = psStruct->psNext)
-	{
-		if (psStruct->status == SS_BUILT)
-		{
-			switch (psStruct->pStructureType->type)
-			{
-			case REF_RESEARCH:
-				if (!missionLimboExpand())
-				{
-					ReticuleEnabled[RETBUT_RESEARCH].Enabled = true;
-				}
-				break;
-			case REF_FACTORY:
-			case REF_CYBORG_FACTORY:
-			case REF_VTOL_FACTORY:
-				if (!missionLimboExpand())
-				{
-					ReticuleEnabled[RETBUT_FACTORY].Enabled = true;
-				}
-				break;
-			default:
-				break;
-			}
-		}
-	}
-
-	for (psDroid = apsDroidLists[selectedPlayer]; psDroid != nullptr; psDroid = psDroid->psNext)
-	{
-		switch (psDroid->droidType)
-		{
-		case DROID_CONSTRUCT:
-		case DROID_CYBORG_CONSTRUCT:
-			ReticuleEnabled[RETBUT_BUILD].Enabled = true;
-			break;
-		case DROID_COMMAND:
-			ReticuleEnabled[RETBUT_COMMAND].Enabled = true;
-			break;
-		default:
-			break;
-		}
-	}
-
-	for (auto &i : ReticuleEnabled)
-	{
-		WIDGET *psWidget = widgGetFromID(psWScreen, i.id);
-
-		if (psWidget != nullptr)
-		{
-			if (psWidget->type != WIDG_LABEL)
-			{
-				if (i.Enabled)
-				{
-					widgSetButtonState(psWScreen, i.id, 0);
-				}
-				else
-				{
-					widgSetButtonState(psWScreen, i.id, WBUT_DISABLE);
-				}
-
-				if (i.Hidden)
-				{
-					widgHide(psWScreen, i.id);
-				}
-				else
-				{
-					widgReveal(psWScreen, i.id);
-				}
-			}
-		}
-	}
 }
 
 /*Checks to see if there are any research topics to do and flashes the button -

data/base/script/rules.js
@@ -3,6 +3,57 @@
 // * Enable unit design and minimap only when an HQ exists
 receiveAllEvents(true); //Needed to allow enemy research to apply to them
 
+var mainReticule = true;
+var allowDesign = false;
+
+function setMainReticule()
+{
+	setReticuleButton(0, _("Close"), "image_cancel_up.png", "image_cancel_down.png");
+	if (countStruct("A0LightFactory") + countStruct("A0CyborgFactory") + countStruct("A0VTolFactory1") > 0
+	    && levelType != LDS_EXPAND_LIMBO)
+	{
+		setReticuleButton(1, _("Manufacture (F1)"), "image_manufacture_up.png", "image_manufacture_down.png");
+	}
+	else
+	{
+		setReticuleButton(1, _("Manufacture - build factory first"), "", "");
+	}
+	if (countStruct("A0ResearchFacility") > 0 && levelType != LDS_EXPAND_LIMBO)
+	{
+		setReticuleButton(2, _("Research (F2)"), "image_research_up.png", "image_research_down.png");
+	}
+	else
+	{
+		setReticuleButton(2, _("Research - build research facility first"), "", "");
+	}
+	if (countDroid(DROID_CONSTRUCT, selectedPlayer) > 0)
+	{
+		setReticuleButton(3, _("Build (F3)"), "image_build_up.png", "image_build_down.png");
+	}
+	else
+	{
+		setReticuleButton(3, _("Build - manufacture constructor droids first"), "", "");
+	}
+	if (allowDesign == true)
+	{
+		setReticuleButton(4, _("Design (F4)"), "image_design_up.png", "image_design_down.png");
+	}
+	else
+	{
+		setReticuleButton(4, _("Design - construct HQ first"), "", "");
+	}
+	setReticuleButton(5, _("Intelligence Display (F5)"), "image_intelmap_up.png", "image_intelmap_down.png");
+	if (countDroid(DROID_COMMAND, selectedPlayer) > 0)
+	{
+		setReticuleButton(6, _("Commanders (F6)"), "image_commanddroid_up.png", "image_commanddroid_down.png");
+	}
+	else
+	{
+		setReticuleButton(6, _("Commanders - manufacture commanders first"), "", "");
+	}
+	mainReticule = true; // main reticule window is open
+}
+
 function setupGame()
 {
 	if (tilesetType == "URBAN")
@@ -29,6 +80,7 @@ function setupGame()
 	setReticuleButton(4, _("Design (F4)"), "image_design_up.png", "image_design_down.png");
 	setReticuleButton(5, _("Intelligence Display (F5)"), "image_intelmap_up.png", "image_intelmap_down.png");
 	setReticuleButton(6, _("Commanders (F6)"), "image_commanddroid_up.png", "image_commanddroid_down.png");
+	setMainReticule();
 	showInterface();
 }
 
@@ -123,23 +175,75 @@ function eventStartLevel()
 	resetPower();
 }
 
+function eventDroidBuilt(droid, structure)
+{
+	var update_reticule = false;
+
+	if (droid.player == selectedPlayer && droid.type == DROID
+	    && (droid.droidType == DROID_CONSTRUCT || droid.droidType == DROID_COMMAND))
+	{
+		update_reticule = true;
+	}
+
+	if (mainReticule && update_reticule)
+	{
+		setMainReticule();
+	}
+}
+
 function eventStructureBuilt(struct)
 {
+	var update_reticule = false;
+
 	if (struct.player == selectedPlayer && struct.type == STRUCTURE && struct.stattype == HQ)
 	{
-		// Enable unit design and minimap when an HQ gets built
-		setMiniMap(true);
-		setDesign(true);
+		setMiniMap(true); // show minimap
+		setDesign(true); // permit designs
+		allowDesign = true;
+		update_reticule = true;
+	}
+
+	if (struct.player == selectedPlayer && struct.type == STRUCTURE
+	    && (struct.stattype == RESEARCH_LAB || struct.stattype == CYBORG_FACTORY
+	        || struct.stattype == VTOL_FACTORY || struct.stattype == FACTORY))
+	{
+		update_reticule = true;
+	}
+
+	if (mainReticule && update_reticule)
+	{
+		setMainReticule();
 	}
 }
 
 function eventDestroyed(victim)
 {
-	if (victim.player == selectedPlayer && victim.type == STRUCTURE && victim.stattype == HQ)
+	var update_reticule = false;
+
+	if (victim.player == selectedPlayer && victim.type == STRUCTURE && victim.stattype == HQ && !enumStruct(selectedPlayer, HQ).length)
 	{
-		// Disable unit design and minimap when the HQ gets destroyed
-		setMiniMap(false);
-		setDesign(false);
+		setMiniMap(false); // hide minimap if HQ is destroyed and no other HQs are present
+		setDesign(false); // and disallow design
+		allowDesign = false;
+		update_reticule = true;
+	}
+
+	if (victim.player == selectedPlayer && victim.type == STRUCTURE
+	    && (victim.stattype == RESEARCH_LAB || victim.stattype == CYBORG_FACTORY
+	        || victim.stattype == VTOL_FACTORY || victim.stattype == FACTORY))
+	{
+		update_reticule = true;
+	}
+
+	if (victim.player == selectedPlayer && victim.type == DROID
+	    && (victim.droidType == DROID_CONSTRUCT || victim.droidType == DROID_COMMAND))
+	{
+		update_reticule = true;
+	}
+
+	if (mainReticule && update_reticule)
+	{
+		setMainReticule();
 	}
 }
 

data/base/script/tutorial.js
@@ -7,12 +7,6 @@ function eventGameInit()
 	replaceTexture("page-9-player-buildings-bases.png", "page-9-player-buildings-bases-rockies.png");
 	setSky("texpages/page-25-sky-urban.png", 0.5, 10000.0);
 	setReticuleButton(0, _("Close"), "image_cancel_up.png", "image_cancel_down.png");
-	setReticuleButton(1, _("Manufacture (F1)"), "image_manufacture_up.png", "image_manufacture_down.png");
-	setReticuleButton(2, _("Research (F2)"), "image_research_up.png", "image_research_down.png");
-	setReticuleButton(3, _("Build (F3)"), "image_build_up.png", "image_build_down.png");
-	setReticuleButton(4, _("Design (F4)"), "image_design_up.png", "image_design_down.png");
-	setReticuleButton(5, _("Intelligence Display (F5)"), "image_intelmap_up.png", "image_intelmap_down.png");
-	setReticuleButton(6, _("Commanders (F6)"), "image_commanddroid_up.png", "image_commanddroid_down.png");
 }
 
 function eventStartLevel()

data/mp/multiplay/skirmish/rules.js
@@ -11,16 +11,54 @@ receiveAllEvents(true);  // If doing this in eventGameInit, it seems to be too l
 var lastHitTime = 0;
 var cheatmode = false;
 var maxOilDrums = 0;
+var mainReticule = true;
+var allowDesign = false;
 
 function setMainReticule()
 {
 	setReticuleButton(0, _("Close"), "image_cancel_up.png", "image_cancel_down.png");
-	setReticuleButton(1, _("Manufacture (F1)"), "image_manufacture_up.png", "image_manufacture_down.png");
-	setReticuleButton(2, _("Research (F2)"), "image_research_up.png", "image_research_down.png");
-	setReticuleButton(3, _("Build (F3)"), "image_build_up.png", "image_build_down.png");
-	setReticuleButton(4, _("Design (F4)"), "image_design_up.png", "image_design_down.png");
+	if (countStruct("A0LightFactory") + countStruct("A0CyborgFactory") + countStruct("A0VTolFactory1") > 0)
+	{
+		setReticuleButton(1, _("Manufacture (F1)"), "image_manufacture_up.png", "image_manufacture_down.png");
+	}
+	else
+	{
+		setReticuleButton(1, _("Manufacture - build factory first"), "", "");
+	}
+	if (countStruct("A0ResearchFacility") > 0)
+	{
+		setReticuleButton(2, _("Research (F2)"), "image_research_up.png", "image_research_down.png");
+	}
+	else
+	{
+		setReticuleButton(2, _("Research - build research facility first"), "", "");
+	}
+	if (countDroid(DROID_CONSTRUCT, selectedPlayer) > 0)
+	{
+		setReticuleButton(3, _("Build (F3)"), "image_build_up.png", "image_build_down.png");
+	}
+	else
+	{
+		setReticuleButton(3, _("Build - manufacture constructor droids first"), "", "");
+	}
+	if (allowDesign == true)
+	{
+		setReticuleButton(4, _("Design (F4)"), "image_design_up.png", "image_design_down.png");
+	}
+	else
+	{
+		setReticuleButton(4, _("Design - construct HQ first"), "", "");
+	}
 	setReticuleButton(5, _("Intelligence Display (F5)"), "image_intelmap_up.png", "image_intelmap_down.png");
-	setReticuleButton(6, _("Commanders (F6)"), "image_commanddroid_up.png", "image_commanddroid_down.png");
+	if (countDroid(DROID_COMMAND, selectedPlayer) > 0)
+	{
+		setReticuleButton(6, _("Commanders (F6)"), "image_commanddroid_up.png", "image_commanddroid_down.png");
+	}
+	else
+	{
+		setReticuleButton(6, _("Commanders - manufacture commanders first"), "", "");
+	}
+	mainReticule = true; // main reticule window is open
 }
 
 function setupGame()
@@ -223,6 +261,7 @@ function eventGameInit()
 	// Disabled by default
 	setMiniMap(false);
 	setDesign(false);
+	allowDesign = false;
 	// This is the only template that should be enabled before design is allowed
 	enableTemplate("ConstructionDroid");
 
@@ -319,21 +358,75 @@ function eventAttacked(victimObj, attackerObj)
 	}
 }
 
+function eventDroidBuilt(droid, structure)
+{
+	var update_reticule = false;
+
+	if (droid.player == selectedPlayer && droid.type == DROID
+	    && (droid.droidType == DROID_CONSTRUCT || droid.droidType == DROID_COMMAND))
+	{
+		update_reticule = true;
+	}
+
+	if (mainReticule && update_reticule)
+	{
+		setMainReticule();
+	}
+}
+
 function eventStructureBuilt(struct)
 {
+	var update_reticule = false;
+
 	if (struct.player == selectedPlayer && struct.type == STRUCTURE && struct.stattype == HQ)
 	{
 		setMiniMap(true); // show minimap
 		setDesign(true); // permit designs
+		allowDesign = true;
+		update_reticule = true;
+	}
+
+	if (struct.player == selectedPlayer && struct.type == STRUCTURE
+	    && (struct.stattype == RESEARCH_LAB || struct.stattype == CYBORG_FACTORY
+	        || struct.stattype == VTOL_FACTORY || struct.stattype == FACTORY))
+	{
+		update_reticule = true;
+	}
+
+	if (mainReticule && update_reticule)
+	{
+		setMainReticule();
 	}
 }
 
 function eventDestroyed(victim)
 {
+	var update_reticule = false;
+
 	if (victim.player == selectedPlayer && victim.type == STRUCTURE && victim.stattype == HQ && !enumStruct(selectedPlayer, HQ).length)
 	{
 		setMiniMap(false); // hide minimap if HQ is destroyed and no other HQs are present
 		setDesign(false); // and disallow design
+		allowDesign = false;
+		update_reticule = true;
+	}
+
+	if (victim.player == selectedPlayer && victim.type == STRUCTURE
+	    && (victim.stattype == RESEARCH_LAB || victim.stattype == CYBORG_FACTORY
+	        || victim.stattype == VTOL_FACTORY || victim.stattype == FACTORY))
+	{
+		update_reticule = true;
+	}
+
+	if (victim.player == selectedPlayer && victim.type == DROID
+	    && (victim.droidType == DROID_CONSTRUCT || victim.droidType == DROID_COMMAND))
+	{
+		update_reticule = true;
+	}
+
+	if (mainReticule && update_reticule)
+	{
+		setMainReticule();
 	}
 }
 

lib/widget/button.cpp
@@ -76,6 +76,12 @@ void W_BUTTON::setFlash(bool enable)
 	}
 }
 
+void W_BUTTON::unlock()
+{
+	dirty = true;
+	state &= ~(WBUT_LOCK | WBUT_CLICKLOCK);
+}
+
 void W_BUTTON::setState(unsigned newState)
 {
 	ASSERT(!((newState & WBUT_LOCK) && (newState & WBUT_CLICKLOCK)), "Cannot have both WBUT_LOCK and WBUT_CLICKLOCK");

lib/widget/button.h
@@ -63,6 +63,7 @@ public:
 	QString getString() const;
 	void setString(QString string);
 	void setTip(QString string);
+	void unlock();
 
 	void setImages(Images const &images);
 	void setImages(Image image, Image imageDown, Image imageHighlight, Image imageDisabled = Image());

src/hci.h
@@ -40,6 +40,17 @@ struct W_SCREEN;
 struct iIMDShape;
 struct QScriptEngine;
 
+enum  				  // Reticule button indecies.
+{
+	RETBUT_CANCEL,
+	RETBUT_FACTORY,
+	RETBUT_RESEARCH,
+	RETBUT_BUILD,
+	RETBUT_DESIGN,
+	RETBUT_INTELMAP,
+	RETBUT_COMMAND,
+};
+
 #define BASE_COORDS_X	(640)
 #define BASE_COORDS_Y	(460)
 #define E_W (pie_GetVideoBufferWidth() - BASE_COORDS_X)
@@ -285,7 +296,8 @@ void intDisplayWidgets();
 bool intAddReticule();
 bool intAddPower();
 void intRemoveReticule();
-void setReticuleStats(int ButId, QString tip, QString filename, QString filenameDown, QString func, QScriptEngine *engine);
+void setReticuleStats(int ButId, QString tip = QString(), QString filename = QString(), QString filenameDown = QString(), QString func = QString(), QScriptEngine *engine = nullptr);
+void setReticuleFlash(int ButId, bool flash);
 
 /* Set the map view point to the world coordinates x,y */
 void intSetMapPos(UDWORD x, UDWORD y);

src/init.cpp
@@ -1259,6 +1259,7 @@ bool stageThreeInitialise()
 		}
 	}
 
+	countUpdate();
 	if (getLevelLoadType() != GTYPE_SAVE_MIDMISSION)
 	{
 		if (getDebugMappingStatus())

Jeanosasco/WarFrontier

commit 548a71876eb33238b72e618e96e586f2c57898dd

Use the reticule key enable state again.
Keys F1-F6 to open reticule menus were broken with commit 2810c730e1733262b3ff27decfbb93c1db821a6a.
Fixes ticket:4715.

src/hci.cpp
@@ -394,6 +394,7 @@ void setReticuleStats(int ButId, QString tip, QString filename, QString filename
 	retbutstats[ButId].flashTime = 0;
 	retbutstats[ButId].func = func;
 	retbutstats[ButId].engine = engine;
+	ReticuleEnabled[ButId].Enabled = false;
 
 	if (!retbutstats[ButId].button) // not quite set up yet
 	{
@@ -411,6 +412,7 @@ void setReticuleStats(int ButId, QString tip, QString filename, QString filename
 	}
 	else
 	{
+		ReticuleEnabled[ButId].Enabled = true;
 		retbutstats[ButId].button->setState(0);
 	}
 }
@@ -4418,6 +4420,10 @@ switch (buttonID)
 		intMode = INT_DESIGN;
 		reticuleCallback(RETBUT_DESIGN);
 		triggerEvent(TRIGGER_MENU_DESIGN_UP);
+		break;
+	case RETBUT_INTELMAP:
+		addIntelScreen();
+		reticuleCallback(RETBUT_INTELMAP);
 		break;
 	case RETBUT_COMMAND:
 		intResetScreen(false);