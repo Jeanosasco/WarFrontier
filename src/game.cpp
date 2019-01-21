Warzone2100/warzone2100

commit 30aa2a9ddbc9b9bf17bf478b50ef8b35db287cf3

Do not share ally vision when loading campaign saves.
More of a game configuation issue affecting alliancesSharedVision() here, which
has its own ticket.

Closes ticket 4828.

src/game.cpp
@@ -1955,7 +1955,7 @@ bool loadGame(const char *pGameToLoad, bool keepObjects, bool freeMem, bool User
 					{
 						alliances[i][j] = ALLIANCE_FORMED;    // hack to fix old savegames
 					}
-					if (alliancesSharedVision(game.alliance) && alliances[i][j] == ALLIANCE_FORMED)
+					if (bMultiPlayer && alliancesSharedVision(game.alliance) && alliances[i][j] == ALLIANCE_FORMED)
 					{
 						alliancebits[i] |= 1 << j;
 					}

Jeanosasco/WarFrontier

commit 1f7c9845bf0d8fdd0fc8b12e6c79c53eeb1b564e

Fix alliance vision sharing when reloading in a skirmish.
Set bMultiPlayer a little earlier. Bug introduced in 30aa2a9ddbc9b9bf17bf478b50ef8b35db287cf3.

src/game.cpp
@@ -1898,6 +1898,11 @@ bool loadGame(const char *pGameToLoad, bool keepObjects, bool freeMem, bool User
 		// Stuff added after level load to avoid being reset or initialised during load
 		// always !keepObjects
 
+		if (saveGameVersion >= VERSION_33)
+		{
+			bMultiPlayer = saveGameData.multiPlayer;
+		}
+
 		if (saveGameVersion >= VERSION_12)
 		{
 			mission.startTime = saveGameData.missionTime;
@@ -2027,7 +2032,6 @@ bool loadGame(const char *pGameToLoad, bool keepObjects, bool freeMem, bool User
 			game			= saveGameData.sGame;
 			game.scavengers = scav;	// ok, so this is butt ugly. but i'm just getting inspiration from the rest of the code around here. ok? - per
 			productionPlayer = selectedPlayer;
-			bMultiPlayer	= saveGameData.multiPlayer;
 			bMultiMessages	= bMultiPlayer;
 
 			NetPlay.bComms = (saveGameData.sNetPlay).bComms;