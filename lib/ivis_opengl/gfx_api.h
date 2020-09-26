past-due/warzone2100

Refactor SDL and gfx_api::context initialization / integration

lib/ivis_opengl/gfx_api.h
@@ -25,12 +25,24 @@
 #include <vector>
 #include <tuple>
 
-struct SDL_Window; // forward-declare
-
+#include "lib/framework/frame.h"
 #include "screen.h"
 #include "pietypes.h"
 
 #include <glm/glm.hpp>
+
+namespace gfx_api
+{
+	// Must be implemented by backend (ex. SDL)
+	class backend_OpenGL_Impl; // see: gfx_api_gl.h
+	class backend_Impl_Factory
+	{
+	public:
+		virtual ~backend_Impl_Factory() {};
+		virtual std::unique_ptr<backend_OpenGL_Impl> getOpenGLBackendImpl() const = 0;
+	};
+	//
+}
 
 namespace gfx_api
 {
@@ -234,7 +246,7 @@ namespace gfx_api
 		virtual void set_depth_range(const float& min, const float& max) = 0;
 		virtual int32_t get_context_value(const context_value property) = 0;
 		static context& get();
-		virtual bool setSwapchain(struct SDL_Window* window) = 0;
+		virtual bool initGraphics(const backend_Impl_Factory& impl) = 0;
 		virtual void flip() = 0;
 		virtual void debugStringMarker(const char *str) = 0;
 		virtual void debugSceneBegin(const char *descr) = 0;

lib/ivis_opengl/gfx_api_gl.cpp
@@ -1420,6 +1420,34 @@ static void GLAPIENTRY khr_callback(GLenum source, GLenum type, GLuint id, GLenu
 	debug(LOG_ERROR, "GL::%s(%s:%s) : %s", cbsource(source), cbtype(type), cbseverity(severity), message);
 }
 
+bool gl_context::initGraphics(const gfx_api::backend_Impl_Factory& impl)
+{
+	// obtain backend_OpenGL_Impl from impl
+	backend_impl = impl.getOpenGLBackendImpl();
+	if (!backend_impl)
+	{
+		debug(LOG_ERROR, "Failed to get OpenGL backend implementation");
+		return false;
+	}
+
+	backend_impl->createGLContext();
+
+	int width, height = 0;
+	backend_impl->getDrawableSize(&width, &height);
+	debug(LOG_WZ, "Drawable Size: %d x %d", width, height);
+
+	glViewport(0, 0, width, height);
+	glCullFace(GL_FRONT);
+	//	glEnable(GL_CULL_FACE);
+
+	if (!initGLContext())
+	{
+		return false;
+	}
+
+	return true;
+}
+
 bool gl_context::initGLContext()
 {
 	GLint glMaxTUs;
@@ -1616,6 +1644,13 @@ bool gl_context::initGLContext()
 	glGenBuffers(1, &scratchbuffer);
 
 	return true;
+}
+
+void gl_context::flip()
+{
+	backend_impl->swapWindow();
+	glUseProgram(0);
+	current_program = nullptr;
 }
 
 void gl_context::shutdown()

lib/ivis_opengl/gfx_api_gl.h
@@ -26,6 +26,23 @@
 #include <cmath>
 #include <functional>
 
+namespace gfx_api
+{
+	class backend_OpenGL_Impl
+	{
+	public:
+		backend_OpenGL_Impl() {};
+		virtual ~backend_OpenGL_Impl() {};
+
+		// Creates an OpenGL context (double-buffered)
+		virtual bool createGLContext() = 0;
+		virtual void swapWindow() = 0;
+
+		// Use this function to get the size of a window's underlying drawable in pixels (for use with glViewport).
+		virtual void getDrawableSize(int* w, int* h) = 0;
+	};
+}
+
 struct gl_texture final : public gfx_api::texture
 {
 private:
@@ -153,8 +170,9 @@ private:
 
 struct gl_context final : public gfx_api::context
 {
+	std::unique_ptr<gfx_api::backend_OpenGL_Impl> backend_impl;
+
 	gl_pipeline_state_object* current_program = nullptr;
-	SDL_Window* WZwindow = nullptr;
 	GLuint scratchbuffer = 0;
 	size_t scratchbuffer_size = 0;
 	bool khr_debug = false;
@@ -185,7 +203,7 @@ struct gl_context final : public gfx_api::context
 	virtual void set_depth_range(const float& min, const float& max) override;
 	virtual int32_t get_context_value(const context_value property) override;
 
-	virtual bool setSwapchain(struct SDL_Window* window) override;
+	virtual bool initGraphics(const gfx_api::backend_Impl_Factory& impl) override;
 	virtual void flip() override;
 	virtual void debugStringMarker(const char *str) override;
 	virtual void debugSceneBegin(const char *descr) override;

lib/sdl/gfx_api_sdl.cpp
@@ -1,0 +1,33 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2019  Warzone 2100 Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "gfx_api_sdl.h"
+#include "gfx_api_gl_sdl.h"
+
+SDL_gfx_api_Impl_Factory::SDL_gfx_api_Impl_Factory(SDL_Window* _window)
+{
+	ASSERT(_window != nullptr, "Invalid SDL_Window*");
+	window = _window;
+}
+
+std::unique_ptr<gfx_api::backend_OpenGL_Impl> SDL_gfx_api_Impl_Factory::getOpenGLBackendImpl() const
+{
+	return std::unique_ptr<gfx_api::backend_OpenGL_Impl>(new sdl_OpenGL_Impl(window));
+}
+

lib/sdl/gfx_api_sdl.h
@@ -1,0 +1,32 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2019  Warzone 2100 Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "lib/ivis_opengl/gfx_api.h"
+#include <SDL_video.h>
+
+class SDL_gfx_api_Impl_Factory final : public gfx_api::backend_Impl_Factory
+{
+public:
+	SDL_gfx_api_Impl_Factory(SDL_Window* window);
+
+	virtual std::unique_ptr<gfx_api::backend_OpenGL_Impl> getOpenGLBackendImpl() const override;
+
+private:
+	SDL_Window* window;
+};

lib/sdl/main_sdl.cpp
@@ -51,6 +51,7 @@
 #include "lib/gamelib/gtime.h"
 #include "src/warzoneconfig.h"
 #include "src/game.h"
+#include "gfx_api_sdl.h"
 #include <SDL.h>
 #include <SDL_thread.h>
 #include <SDL_clipboard.h>
@@ -1835,9 +1836,9 @@ bool wzMainScreenSetup(int antialiasing, bool fullscreen, bool vsync, bool highD
 	cocoaSetupWZMenus();
 #endif
 
-	if (!gfx_api::context::get().setSwapchain(WZwindow))
+	if (!gfx_api::context::get().initGraphics(SDL_gfx_api_Impl_Factory(WZwindow)))
 	{
-		debug(LOG_FATAL, "gfx_api::context::get().setSwapchain failed");
+		debug(LOG_FATAL, "gfx_api::context::get().initGraphics failed");
 		SDL_Quit();
 		exit(EXIT_FAILURE);
 	}

Jeanosasco/WarFrontier

lib/ivis_opengl/gfx_api.h
@@ -25,12 +25,24 @@
 #include <vector>
 #include <tuple>
 
-struct SDL_Window; // forward-declare
-
+#include "lib/framework/frame.h"
 #include "screen.h"
 #include "pietypes.h"
 
 #include <glm/glm.hpp>
+
+namespace gfx_api
+{
+	// Must be implemented by backend (ex. SDL)
+	class backend_OpenGL_Impl; // see: gfx_api_gl.h
+	class backend_Impl_Factory
+	{
+	public:
+		virtual ~backend_Impl_Factory() {};
+		virtual std::unique_ptr<backend_OpenGL_Impl> createOpenGLBackendImpl() const = 0;
+	};
+	//
+}
 
 namespace gfx_api
 {
@@ -235,7 +247,7 @@ namespace gfx_api
 		virtual void set_depth_range(const float& min, const float& max) = 0;
 		virtual int32_t get_context_value(const context_value property) = 0;
 		static context& get();
-		virtual bool setSwapchain(struct SDL_Window* window) = 0;
+		virtual bool initialize(const backend_Impl_Factory& impl) = 0;
 		virtual void flip() = 0;
 		virtual void debugStringMarker(const char *str) = 0;
 		virtual void debugSceneBegin(const char *descr) = 0;

lib/ivis_opengl/gfx_api_gl.cpp
@@ -1422,6 +1422,34 @@ static void GLAPIENTRY khr_callback(GLenum source, GLenum type, GLuint id, GLenu
 	debug(LOG_ERROR, "GL::%s(%s:%s) : %s", cbsource(source), cbtype(type), cbseverity(severity), message);
 }
 
+bool gl_context::initialize(const gfx_api::backend_Impl_Factory& impl)
+{
+	// obtain backend_OpenGL_Impl from impl
+	backend_impl = impl.createOpenGLBackendImpl();
+	if (!backend_impl)
+	{
+		debug(LOG_ERROR, "Failed to get OpenGL backend implementation");
+		return false;
+	}
+
+	backend_impl->createGLContext();
+
+	int width, height = 0;
+	backend_impl->getDrawableSize(&width, &height);
+	debug(LOG_WZ, "Drawable Size: %d x %d", width, height);
+
+	glViewport(0, 0, width, height);
+	glCullFace(GL_FRONT);
+	//	glEnable(GL_CULL_FACE);
+
+	if (!initGLContext())
+	{
+		return false;
+	}
+
+	return true;
+}
+
 bool gl_context::initGLContext()
 {
 	GLint glMaxTUs;
@@ -1618,6 +1646,13 @@ bool gl_context::initGLContext()
 	glGenBuffers(1, &scratchbuffer);
 
 	return true;
+}
+
+void gl_context::flip()
+{
+	backend_impl->swapWindow();
+	glUseProgram(0);
+	current_program = nullptr;
 }
 
 void gl_context::shutdown()

lib/ivis_opengl/gfx_api_gl.h
@@ -26,6 +26,23 @@
 #include <cmath>
 #include <functional>
 
+namespace gfx_api
+{
+	class backend_OpenGL_Impl
+	{
+	public:
+		backend_OpenGL_Impl() {};
+		virtual ~backend_OpenGL_Impl() {};
+
+		// Creates an OpenGL context (double-buffered)
+		virtual bool createGLContext() = 0;
+		virtual void swapWindow() = 0;
+
+		// Use this function to get the size of a window's underlying drawable in pixels (for use with glViewport).
+		virtual void getDrawableSize(int* w, int* h) = 0;
+	};
+}
+
 struct gl_texture final : public gfx_api::texture
 {
 private:
@@ -153,8 +170,9 @@ private:
 
 struct gl_context final : public gfx_api::context
 {
+	std::unique_ptr<gfx_api::backend_OpenGL_Impl> backend_impl;
+
 	gl_pipeline_state_object* current_program = nullptr;
-	SDL_Window* WZwindow = nullptr;
 	GLuint scratchbuffer = 0;
 	size_t scratchbuffer_size = 0;
 	bool khr_debug = false;
@@ -185,7 +203,7 @@ struct gl_context final : public gfx_api::context
 	virtual void set_depth_range(const float& min, const float& max) override;
 	virtual int32_t get_context_value(const context_value property) override;
 
-	virtual bool setSwapchain(struct SDL_Window* window) override;
+	virtual bool initialize(const gfx_api::backend_Impl_Factory& impl) override;
 	virtual void flip() override;
 	virtual void debugStringMarker(const char *str) override;
 	virtual void debugSceneBegin(const char *descr) override;

lib/sdl/gfx_api_sdl.cpp
@@ -1,0 +1,33 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2019  Warzone 2100 Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "gfx_api_sdl.h"
+#include "gfx_api_gl_sdl.h"
+
+SDL_gfx_api_Impl_Factory::SDL_gfx_api_Impl_Factory(SDL_Window* _window)
+{
+	ASSERT(_window != nullptr, "Invalid SDL_Window*");
+	window = _window;
+}
+
+std::unique_ptr<gfx_api::backend_OpenGL_Impl> SDL_gfx_api_Impl_Factory::createOpenGLBackendImpl() const
+{
+	return std::unique_ptr<gfx_api::backend_OpenGL_Impl>(new sdl_OpenGL_Impl(window));
+}
+

lib/sdl/gfx_api_sdl.h
@@ -1,0 +1,32 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2019  Warzone 2100 Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "lib/ivis_opengl/gfx_api.h"
+#include <SDL_video.h>
+
+class SDL_gfx_api_Impl_Factory final : public gfx_api::backend_Impl_Factory
+{
+public:
+	SDL_gfx_api_Impl_Factory(SDL_Window* window);
+
+	virtual std::unique_ptr<gfx_api::backend_OpenGL_Impl> createOpenGLBackendImpl() const override;
+
+private:
+	SDL_Window* window;
+};

lib/sdl/main_sdl.cpp
@@ -51,6 +51,7 @@
 #include "lib/gamelib/gtime.h"
 #include "src/warzoneconfig.h"
 #include "src/game.h"
+#include "gfx_api_sdl.h"
 #include <SDL.h>
 #include <SDL_thread.h>
 #include <SDL_clipboard.h>
@@ -1856,9 +1857,9 @@ bool wzMainScreenSetup(int antialiasing, bool fullscreen, bool vsync, bool highD
 	cocoaSetupWZMenus();
 #endif
 
-	if (!gfx_api::context::get().setSwapchain(WZwindow))
+	if (!gfx_api::context::get().initialize(SDL_gfx_api_Impl_Factory(WZwindow)))
 	{
-		debug(LOG_FATAL, "gfx_api::context::get().setSwapchain failed");
+		debug(LOG_FATAL, "gfx_api::context::get().initialize failed");
 		SDL_Quit();
 		exit(EXIT_FAILURE);
 	}