KJeff01/warzone2100

Campaign updates
Set remastered to the v1.2.11 tag.

Mainly:
- New Insane logic to better differentiate itself from Hard (new spawns appear on missions Alpha 5 - Gamma 8).
- Various library updates to support the new Insane, some minor cleanups and fixes.
- Various small balance tweaks and fixes.

data/base/script/campaign/cam2-c.js
@@ -78,6 +78,23 @@ function camEnemyBaseEliminated_COAirBase()
 	camCallOnce("videoTrigger");
 }
 
+function insaneReinforcementSpawn()
+{
+	const units = [cTempl.comatt, cTempl.comit, cTempl.cohct, cTempl.commrl, cTempl.comhpv, cTempl.npcybc];
+	const limits = {minimum: 8, maxRandom: 8};
+	const location = camMakePos(getObject("southWestSpawnPos"));
+	camSendGenericSpawn(CAM_REINFORCE_GROUND, CAM_THE_COLLECTIVE, CAM_REINFORCE_CONDITION_UNITS, location, units, limits.minimum, limits.maxRandom);
+}
+
+function insaneTransporterAttack()
+{
+	const DISTANCE_FROM_POS = 40;
+	const units = [cTempl.cohct, cTempl.commrl, cTempl.comhpv, cTempl.comtathh];
+	const limits = {minimum: 10, maxRandom: 0};
+	const location = camGenerateRandomMapCoordinate(getObject("startPosition"), CAM_GENERIC_LAND_STAT, DISTANCE_FROM_POS);
+	camSendGenericSpawn(CAM_REINFORCE_TRANSPORT, CAM_THE_COLLECTIVE, CAM_REINFORCE_CONDITION_UNITS, location, units, limits.minimum, limits.maxRandom);
+}
+
 function enableFactories()
 {
 	camEnableFactory("COHeavyFac-Leopard");
@@ -224,10 +241,11 @@ function civilianOrders()
 //Capture civilans.
 function eventTransporterLanded(transport)
 {
+	const SCAN_RADIUS = 4;
 	const position = getObject("COTransportPos");
-	const civs = enumRange(position.x, position.y, 15, CAM_SCAV_7, false);
+	const civs = enumRange(position.x, position.y, SCAN_RADIUS, CAM_SCAV_7, false);
 
-	if (civs.length)
+	if ((civs.length > 0) && (camDist(transport, position) <= SCAN_RADIUS))
 	{
 		playSound(cam_sounds.enemyEscaping);
 		capturedCivCount += civs.length - 1;
@@ -409,7 +427,7 @@ function eventStartLevel()
 				regroup: false,
 				count: -1,
 			},
-			templates: (!camClassicMode()) ? [cTempl.commorv, cTempl.cohcv, cTempl.commorv, cTempl.colpbv] : [cTempl.commorv, cTempl.colagv]
+			templates: (!camClassicMode()) ? [cTempl.commorv, cTempl.comhcv, cTempl.commorv, cTempl.colpbv] : [cTempl.commorv, cTempl.colagv]
 		},
 		"COVtolFacRight": {
 			order: CAM_ORDER_ATTACK,
@@ -435,6 +453,11 @@ function eventStartLevel()
 
 	queue("activateGroups", camChangeOnDiff(camMinutesToMilliseconds(8)));
 	setTimer("truckDefense", camChangeOnDiff(camMinutesToMilliseconds(3)));
+	if (difficulty >= INSANE)
+	{
+		setTimer("insaneTransporterAttack", camMinutesToMilliseconds(4));
+		setTimer("insaneReinforcementSpawn", camMinutesToMilliseconds(5));
+	}
 
 	truckDefense();
 }

data/base/script/campaign/libcampaign_includes/guide.js
@@ -56,65 +56,56 @@ function __camGuideTopicCheckResearchComplete(targetResearchName, justResearched
 
 function __camProcessResearchGatedGuideTopics(research = null)
 {
-	let showFlags = (research == null) ? 0 : SHOWTOPIC_FIRSTADD;
-
-	// first "different" weapon type'
+	const __SHOW_FLAGS = (research == null) ? 0 : SHOWTOPIC_FIRSTADD;
+	// first "different" weapon type
 	if (__camGuideTopicCheckResearchComplete('R-Wpn-Flamer01Mk1', research))
 	{
-		addGuideTopic("wz2100::units::weaponselection", showFlags);
+		addGuideTopic("wz2100::units::weaponselection", __SHOW_FLAGS);
 	}
-
 	// repair turret researched
 	if (__camGuideTopicCheckResearchComplete('R-Sys-MobileRepairTurret01', research))
 	{
-		addGuideTopic("wz2100::units::repairing", showFlags);
+		addGuideTopic("wz2100::units::repairing", __SHOW_FLAGS);
 	}
-
 	// first module researched
 	if (__camGuideTopicCheckResearchComplete('R-Struc-PowerModuleMk1', research))
 	{
-		addGuideTopic("wz2100::structures::modules", showFlags);
+		addGuideTopic("wz2100::structures::modules", __SHOW_FLAGS);
 	}
-
 	// sensor turret researched
 	if (__camGuideTopicCheckResearchComplete('R-Sys-Sensor-Turret01', research))
 	{
 		// in this case, the category topic must come first (as adding the child topics will also add it, but we want the category topic to be what's displayed)
-		addGuideTopic("wz2100::units::sensors", showFlags);
+		addGuideTopic("wz2100::units::sensors", __SHOW_FLAGS);
 		addGuideTopic("wz2100::units::sensors::unassigning");
 		addGuideTopic("wz2100::units::sensors::using");
 	}
-
 	// command turret researched
 	if (__camGuideTopicCheckResearchComplete('R-Comp-CommandTurret01', research))
 	{
 		// in this case, the category topic must come first (as adding the child topics will also add it, but we want the category topic to be what's displayed)
-		addGuideTopic("wz2100::units::commanders", showFlags);
+		addGuideTopic("wz2100::units::commanders", __SHOW_FLAGS);
 	}
-
 	// repair facility researched
 	if (__camGuideTopicCheckResearchComplete('R-Struc-RepairFacility', research))
 	{
-		addGuideTopic("wz2100::structures::repairfacility", showFlags);
+		addGuideTopic("wz2100::structures::repairfacility", __SHOW_FLAGS);
 	}
-
 	// vtol factory researched
 	if (__camGuideTopicCheckResearchComplete('R-Struc-VTOLFactory', research))
 	{
-		addGuideTopic("wz2100::structures::vtolfactory", showFlags);
+		addGuideTopic("wz2100::structures::vtolfactory", __SHOW_FLAGS);
 	}
-
 	// vtol propulsion researched
 	if (__camGuideTopicCheckResearchComplete('R-Vehicle-Prop-VTOL', research))
 	{
 		addGuideTopic("wz2100::units::vtols::weapons");
-		addGuideTopic("wz2100::units::vtols::designing", showFlags);
+		addGuideTopic("wz2100::units::vtols::designing", __SHOW_FLAGS);
 	}
-
 	// cb turret researched
 	if (__camGuideTopicCheckResearchComplete('R-Sys-CBSensor-Turret01', research))
 	{
-		addGuideTopic("wz2100::units::sensors::cb", showFlags);
+		addGuideTopic("wz2100::units::sensors::cb", __SHOW_FLAGS);
 	}
 }
 
@@ -123,33 +114,28 @@ function __camEnableGuideTopics()
 	// Always enable (most) general topics & unit orders
 	addGuideTopic("wz2100::general::**", 0, ["wz2100::general::artifacts", "wz2100::general::researching", "wz2100::general::power"]);
 	addGuideTopic("wz2100::unitorders::**");
-
 	// Basic base / structure topics
 	addGuideTopic("wz2100::structures::building");
 	addGuideTopic("wz2100::structures::demolishing");
-
-	let isCam1A = (__camNextLevel === cam_levels.alpha2);
-	if (!isCam1A) // If not very first original campaign level (CAM1A), which displays these in response to events, add them
+	const __IS_ALPHA_ONE = (__camNextLevel === cam_levels.alpha2);
+	if (!__IS_ALPHA_ONE) // If not very first original campaign level (CAM1A), which displays these in response to events, add them
 	{
 		addGuideTopic("wz2100::general::artifacts");
 		addGuideTopic("wz2100::general::researching");
 		addGuideTopic("wz2100::general::power");
-
+		// Basic structure topics
 		addGuideTopic("wz2100::structures::hq");
 		addGuideTopic("wz2100::structures::researchfacility");
 		addGuideTopic("wz2100::structures::oilderrick");
 		addGuideTopic("wz2100::structures::powergenerator");
 		addGuideTopic("wz2100::structures::rallypoint");
 		addGuideTopic("wz2100::structures::factory");
-
 		// Basic units topics
 		addGuideTopic("wz2100::units::building");
 		addGuideTopic("wz2100::units::designing");
 	}
-
 	// Handle research-driven topics (for things already researched - i.e. on savegame load or starting a later campaign)
 	__camProcessResearchGatedGuideTopics();
-
 	// Handle built-unit triggered topics
 	if (countDroid(DROID_COMMAND, CAM_HUMAN_PLAYER) > 0)
 	{
@@ -157,7 +143,7 @@ function __camEnableGuideTopics()
 	}
 	let foundDroids_VTOL = false;
 	const droids = enumDroid(CAM_HUMAN_PLAYER);
-	for (let x = 0; x < droids.length; ++x)
+	for (let x = 0, len = droids.length; x < len; ++x)
 	{
 		const droid = droids[x];
 		if (droid.isVTOL)

Jeanosasco/WarFrontier

data/base/script/campaign/cam2-c.js
@@ -78,6 +78,23 @@ function camEnemyBaseEliminated_COAirBase()
 	camCallOnce("videoTrigger");
 }
 
+function insaneReinforcementSpawn()
+{
+	const units = [cTempl.comatt, cTempl.comit, cTempl.cohct, cTempl.commrl, cTempl.comhpv, cTempl.npcybc];
+	const limits = {minimum: 8, maxRandom: 8};
+	const location = camMakePos(getObject("southWestSpawnPos"));
+	camSendGenericSpawn(CAM_REINFORCE_GROUND, CAM_THE_COLLECTIVE, CAM_REINFORCE_CONDITION_UNITS, location, units, limits.minimum, limits.maxRandom);
+}
+
+function insaneTransporterAttack()
+{
+	const DISTANCE_FROM_POS = 40;
+	const units = [cTempl.cohct, cTempl.commrl, cTempl.comhpv, cTempl.comtathh];
+	const limits = {minimum: 10, maxRandom: 0};
+	const location = camGenerateRandomMapCoordinate(getObject("startPosition"), CAM_GENERIC_LAND_STAT, DISTANCE_FROM_POS);
+	camSendGenericSpawn(CAM_REINFORCE_TRANSPORT, CAM_THE_COLLECTIVE, CAM_REINFORCE_CONDITION_ARTIFACTS, location, units, limits.minimum, limits.maxRandom);
+}
+
 function enableFactories()
 {
 	camEnableFactory("COHeavyFac-Leopard");
@@ -224,10 +241,11 @@ function civilianOrders()
 //Capture civilans.
 function eventTransporterLanded(transport)
 {
+	const SCAN_RADIUS = 4;
 	const position = getObject("COTransportPos");
-	const civs = enumRange(position.x, position.y, 15, CAM_SCAV_7, false);
+	const civs = enumRange(position.x, position.y, SCAN_RADIUS, CAM_SCAV_7, false);
 
-	if (civs.length)
+	if ((civs.length > 0) && (camDist(transport, position) <= SCAN_RADIUS))
 	{
 		playSound(cam_sounds.enemyEscaping);
 		capturedCivCount += civs.length - 1;
@@ -409,7 +427,7 @@ function eventStartLevel()
 				regroup: false,
 				count: -1,
 			},
-			templates: (!camClassicMode()) ? [cTempl.commorv, cTempl.cohcv, cTempl.commorv, cTempl.colpbv] : [cTempl.commorv, cTempl.colagv]
+			templates: (!camClassicMode()) ? [cTempl.commorv, cTempl.comhcv, cTempl.commorv, cTempl.colpbv] : [cTempl.commorv, cTempl.colagv]
 		},
 		"COVtolFacRight": {
 			order: CAM_ORDER_ATTACK,
@@ -435,6 +453,11 @@ function eventStartLevel()
 
 	queue("activateGroups", camChangeOnDiff(camMinutesToMilliseconds(8)));
 	setTimer("truckDefense", camChangeOnDiff(camMinutesToMilliseconds(3)));
+	if (difficulty >= INSANE)
+	{
+		setTimer("insaneTransporterAttack", camMinutesToMilliseconds(4));
+		setTimer("insaneReinforcementSpawn", camMinutesToMilliseconds(5));
+	}
 
 	truckDefense();
 }

data/base/script/campaign/libcampaign_includes/guide.js
@@ -57,137 +57,117 @@ function __camGuideTopicCheckResearchComplete(targetResearchName, justResearched
 
 function __camProcessResearchGatedGuideTopics(research = null)
 {
-	let showFlags = (research == null) ? 0 : SHOWTOPIC_FIRSTADD;
-
+	const __SHOW_FLAGS = (research == null) ? 0 : SHOWTOPIC_FIRSTADD;
 	// First "different" weapon type
 	if (__camGuideTopicCheckResearchComplete('R-Wpn-Flamer01Mk1', research))
 	{
 		// in this case, the category topic must come first (as adding the child topics will also add it, but we want the category topic to be what's displayed)
-		addGuideTopic("wz2100::weapons", showFlags);
+		addGuideTopic("wz2100::weapons", __SHOW_FLAGS);
 		addGuideTopic("wz2100::weapons::antipersonnel");
 		addGuideTopic("wz2100::weapons::flamer");
 		addGuideTopic("wz2100::units::thermaldamage");
 	}
-
 	// Repair Turret researched
 	if (__camGuideTopicCheckResearchComplete('R-Sys-MobileRepairTurret01', research))
 	{
-		addGuideTopic("wz2100::units::repairing", showFlags);
+		addGuideTopic("wz2100::units::repairing", __SHOW_FLAGS);
 	}
-
 	// First Module researched
 	if (__camGuideTopicCheckResearchComplete('R-Struc-PowerModuleMk1', research))
 	{
-		addGuideTopic("wz2100::structures::modules", showFlags);
+		addGuideTopic("wz2100::structures::modules", __SHOW_FLAGS);
 	}
-
 	// Sensor Turret researched
 	if (__camGuideTopicCheckResearchComplete('R-Sys-Sensor-Turret01', research))
 	{
 		// in this case, the category topic must come first (as adding the child topics will also add it, but we want the category topic to be what's displayed)
-		addGuideTopic("wz2100::units::sensors", showFlags);
+		addGuideTopic("wz2100::units::sensors", __SHOW_FLAGS);
 		addGuideTopic("wz2100::units::sensors::unassigning");
 		addGuideTopic("wz2100::units::sensors::using");
 	}
-
 	// First "All-Rounder" weapon researched
 	if (__camGuideTopicCheckResearchComplete('R-Wpn-Cannon1Mk1', research))
 	{
-		addGuideTopic("wz2100::weapons::allrounder", showFlags);
+		addGuideTopic("wz2100::weapons::allrounder", __SHOW_FLAGS);
 	}
-
 	// First "Artillery" weapon researched
 	if (__camGuideTopicCheckResearchComplete('R-Wpn-Mortar01Lt', research))
 	{
-		addGuideTopic("wz2100::weapons::artillery", showFlags);
+		addGuideTopic("wz2100::weapons::artillery", __SHOW_FLAGS);
 	}
-
 	// Half-Track Propulsion researched
 	if (__camGuideTopicCheckResearchComplete('R-Vehicle-Prop-Halftracks', research))
 	{
 		addGuideTopic("wz2100::units::propulsions::wheels");
-		addGuideTopic("wz2100::units::propulsions::halftracks", showFlags);
+		addGuideTopic("wz2100::units::propulsions::halftracks", __SHOW_FLAGS);
 	}
-
 	// First "Anti-Tank" weapon researched
 	if (__camGuideTopicCheckResearchComplete('R-Wpn-Rocket05-MiniPod', research))
 	{
-		addGuideTopic("wz2100::weapons::antitank", showFlags);
+		addGuideTopic("wz2100::weapons::antitank", __SHOW_FLAGS);
 	}
-
 	// Tracked Propulsion researched
 	if (__camGuideTopicCheckResearchComplete('R-Vehicle-Prop-Tracks', research))
 	{
-		addGuideTopic("wz2100::units::propulsions::tracks", showFlags);
+		addGuideTopic("wz2100::units::propulsions::tracks", __SHOW_FLAGS);
 	}
-
 	// First (and only) Bunker Buster weapon researched
 	if (__camGuideTopicCheckResearchComplete('R-Wpn-Rocket03-HvAT', research))
 	{
-		addGuideTopic("wz2100::weapons::bunkerbuster", showFlags);
+		addGuideTopic("wz2100::weapons::bunkerbuster", __SHOW_FLAGS);
 	}
-
 	// First "Anti-Air" weapon type
 	if (__camGuideTopicCheckResearchComplete('R-Wpn-AAGun03', research))
 	{
-		addGuideTopic("wz2100::weapons::antiair", showFlags);
+		addGuideTopic("wz2100::weapons::antiair", __SHOW_FLAGS);
 	}
-
 	// Command Turret researched
 	if (__camGuideTopicCheckResearchComplete('R-Comp-CommandTurret01', research))
 	{
 		// in this case, the category topic must come first (as adding the child topics will also add it, but we want the category topic to be what's displayed)
-		addGuideTopic("wz2100::units::commanders", showFlags);
+		addGuideTopic("wz2100::units::commanders", __SHOW_FLAGS);
 	}
-
 	// Repair Facility researched
 	if (__camGuideTopicCheckResearchComplete('R-Struc-RepairFacility', research))
 	{
-		addGuideTopic("wz2100::structures::repairfacility", showFlags);
+		addGuideTopic("wz2100::structures::repairfacility", __SHOW_FLAGS);
 	}
-
 	// VTOL Factory researched
 	if (__camGuideTopicCheckResearchComplete('R-Struc-VTOLFactory', research))
 	{
-		addGuideTopic("wz2100::structures::vtolfactory", showFlags);
+		addGuideTopic("wz2100::structures::vtolfactory", __SHOW_FLAGS);
 	}
-
 	// Cyborgs
 	if (__camGuideTopicCheckResearchComplete('R-Struc-Factory-Cyborg', research))
 	{
 		addGuideTopic("wz2100::units::propulsions::cyborg");
-		addGuideTopic("wz2100::structures::cyborgfactory", showFlags);
+		addGuideTopic("wz2100::structures::cyborgfactory", __SHOW_FLAGS);
 	}
-
 	// Hover Propulsion researched
 	if (__camGuideTopicCheckResearchComplete('R-Vehicle-Prop-Hover', research))
 	{
-		addGuideTopic("wz2100::units::propulsions::hover", showFlags);
+		addGuideTopic("wz2100::units::propulsions::hover", __SHOW_FLAGS);
 	}
-
 	// VTOL Propulsion researched
 	if (__camGuideTopicCheckResearchComplete('R-Vehicle-Prop-VTOL', research))
 	{
 		addGuideTopic("wz2100::units::propulsions::vtols::weapons");
-		addGuideTopic("wz2100::units::propulsions::vtols::using", showFlags);
+		addGuideTopic("wz2100::units::propulsions::vtols::using", __SHOW_FLAGS);
 	}
-
 	// CB Turret researched
 	if (__camGuideTopicCheckResearchComplete('R-Sys-CBSensor-Turret01', research))
 	{
-		addGuideTopic("wz2100::units::sensors::cb", showFlags);
+		addGuideTopic("wz2100::units::sensors::cb", __SHOW_FLAGS);
 	}
-
 	// First (explosive) bomb researched
 	if (__camGuideTopicCheckResearchComplete("R-Wpn-Bomb01", research))
 	{
-		addGuideTopic("wz2100::weapons::bombs::explosive", showFlags);
+		addGuideTopic("wz2100::weapons::bombs::explosive", __SHOW_FLAGS);
 	}
-
 	// First incendiary bomb researched
 	if (__camGuideTopicCheckResearchComplete("R-Wpn-Bomb03", research))
 	{
-		addGuideTopic("wz2100::weapons::bombs::incendiary", showFlags);
+		addGuideTopic("wz2100::weapons::bombs::incendiary", __SHOW_FLAGS);
 	}
 }
 
@@ -196,33 +176,28 @@ function __camEnableGuideTopics()
 	// Always enable (most) general topics & unit orders
 	addGuideTopic("wz2100::general::**", 0, ["wz2100::general::artifacts", "wz2100::general::researching", "wz2100::general::power"]);
 	addGuideTopic("wz2100::unitorders::**");
-
 	// Basic base / structure topics
 	addGuideTopic("wz2100::structures::building");
 	addGuideTopic("wz2100::structures::demolishing");
-
-	let isCam1A = (__camNextLevel === cam_levels.alpha2);
-	if (!isCam1A) // If not very first original campaign level (CAM1A), which displays these in response to events, add them
+	const __IS_ALPHA_ONE = (__camNextLevel === cam_levels.alpha2);
+	if (!__IS_ALPHA_ONE) // If not very first original campaign level (CAM1A), which displays these in response to events, add them
 	{
 		addGuideTopic("wz2100::general::artifacts");
 		addGuideTopic("wz2100::general::researching");
 		addGuideTopic("wz2100::general::power");
-
+		// Basic structure topics
 		addGuideTopic("wz2100::structures::hq");
 		addGuideTopic("wz2100::structures::researchfacility");
 		addGuideTopic("wz2100::structures::oilderrick");
 		addGuideTopic("wz2100::structures::powergenerator");
 		addGuideTopic("wz2100::structures::rallypoint");
 		addGuideTopic("wz2100::structures::factory");
-
 		// Basic units topics
 		addGuideTopic("wz2100::units::building");
 		addGuideTopic("wz2100::units::designing");
 	}
-
 	// Handle research-driven topics (for things already researched - i.e. on savegame load or starting a later campaign)
 	__camProcessResearchGatedGuideTopics();
-
 	// Handle built-unit triggered topics
 	if (countDroid(DROID_COMMAND, CAM_HUMAN_PLAYER) > 0)
 	{
@@ -230,7 +205,7 @@ function __camEnableGuideTopics()
 	}
 	let foundDroids_VTOL = false;
 	const droids = enumDroid(CAM_HUMAN_PLAYER);
-	for (let x = 0; x < droids.length; ++x)
+	for (let x = 0, len = droids.length; x < len; ++x)
 	{
 		const droid = droids[x];
 		if (droid.isVTOL)