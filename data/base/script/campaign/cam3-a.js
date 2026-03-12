KJeff01/warzone2100

Add v1.10 campaign balance mod
Accurate to v1.10, this campaign stat mod is more accurate than Classic in regards to:
Armor, damage, and ROF by the end of every mission. Many research topics tweak rewards to correct off-by-one issues (or greater) per component.

I was not able to see structure or cyborg related stats, so they will have to potentially live with small differences.

The main differences are:
- reloadTime research changes never had any effect since the source itself only considered the base value (in effect, no ROF upgrades for rockets/missiles and anything that uses reloadTime).
- All Rockets (including their artillery variants) are generally much weaker due to damage upgrades having zero to barely any effect. Curiously, Missiles benefit from damage upgrades.
- Any weapon that goes above 120 ROF would randomly fail to fire due to intentional delays, with the max ROF actually being 300 as a result (5 bullets per second).
- As per the above, that means MGs, MRP, AA machineguns, HEAP bomb, and Phosphor bomb fire at an unstable rate past 120. So, an average ROF target was calculated and then increased by an offset to recreate v1.10 in-game behavior more closely while staying under 5 bullets per second.

To further increase the accuracy of this mod the following can be followed up on:
- 3.2.x changed HP calculations so some designs are either accurate enough or hundreds of points different.
- Speed related properties appear to be very different.
- See if previous v1.x releases have differences compared to this.

data/base/script/campaign/cam3-a.js
@@ -326,6 +326,13 @@ function cam3Setup()
 		camCompleteRequiredResearch(mis_playerResBetaClassic, CAM_HUMAN_PLAYER);
 		camCompleteRequiredResearch(mis_playerResGammaClassic, CAM_HUMAN_PLAYER);
 
+		if (tweakOptions.camPumpkin_identifier)
+		{
+			const researchFix = ["CAM2RESEARCH-UNDO-Rockets", "CAM3-v110-Undo"];
+			camCompleteRequiredResearch(researchFix, CAM_HUMAN_PLAYER);
+			camCompleteRequiredResearch(researchFix, CAM_NEXUS);
+		}
+
 		if (tweakOptions.camClassic_balance32)
 		{
 			camClassicResearch(mis_gammaStartingResearchClassic, CAM_HUMAN_PLAYER);

Jeanosasco/WarFrontier

data/base/script/campaign/cam3-a.js
@@ -326,6 +326,13 @@ function cam3Setup()
 		camCompleteRequiredResearch(mis_playerResBetaClassic, CAM_HUMAN_PLAYER);
 		camCompleteRequiredResearch(mis_playerResGammaClassic, CAM_HUMAN_PLAYER);
 
+		if (tweakOptions.camPumpkin_identifier110)
+		{
+			const researchFix = ["CAM2RESEARCH-UNDO-Rockets", "CAM3-UNDO-v110"];
+			camCompleteRequiredResearch(researchFix, CAM_HUMAN_PLAYER);
+			camCompleteRequiredResearch(researchFix, CAM_NEXUS);
+		}
+
 		if (tweakOptions.camClassic_balance32)
 		{
 			camClassicResearch(mis_gammaStartingResearchClassic, CAM_HUMAN_PLAYER);